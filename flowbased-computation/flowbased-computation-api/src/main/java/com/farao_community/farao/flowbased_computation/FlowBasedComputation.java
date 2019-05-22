eacf189782a37eff3cc095547d23a803f6c76a3b
pengbo <wangpengbo.ruidian@gmail.com>
2019-05-22 06:56:59 +0200

    FB computation and FB computationFactory. Stucture.

--- flowbased-computation/flowbased-computation-api/src/main/java/com/farao_community/farao/flowbased_computation/FlowBasedComputation.java
+++ flowbased-computation/flowbased-computation-api/src/main/java/com/farao_community/farao/flowbased_computation/FlowBasedComputation.java
@@ -6,25 +6,59 @@
  */
 package com.farao_community.farao.flowbased_computation;
 
+import com.farao_community.farao.data.crac_file.CracFile;
+import com.farao_community.farao.util.LoadFlowService;
+import com.farao_community.farao.util.SensitivityComputationService;
 import com.powsybl.commons.Versionable;
+import com.powsybl.computation.ComputationManager;
 import com.powsybl.iidm.network.Network;
+import com.powsybl.loadflow.LoadFlowFactory;
+import com.powsybl.sensitivity.SensitivityComputationFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 /**
- * Flowbased computation interface
+ * Flowbased computation
  *
- * @author Luc Di Gallo {@literal <luc.di-gallo at rte-france.com>}
  */
-public interface FlowBasedComputation extends Versionable {
+public class FlowBasedComputation {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlowBasedComputation.class);
 
-    /**
-     * Interface to run the flowbased computation
-     *
-     * @return FlowBasedComputationResults results of the flowbased computation
-     */
-    CompletableFuture<FlowBasedComputationResult> run(Network networkIn);
+    private Network network;
+    private CracFile cracFile;
+    private ComputationManager computationManager;
 
-    CompletableFuture<FlowBasedComputationResult> run(String workingStateId, FlowBasedComputationParameters parameters);
+    public FlowBasedComputation(Network network,
+                                CracFile cracFile,
+                                ComputationManager computationManager,
+                                LoadFlowFactory loadFlowFactory,
+                                SensitivityComputationFactory sensitivityComputationFactory) {
+        this.network = network;
+        this.cracFile = cracFile;
+        this.computationManager = computationManager;
+
+        SensitivityComputationService.init(sensitivityComputationFactory, computationManager);
+        LoadFlowService.init(loadFlowFactory, computationManager);
+    }
+
+    public CompletableFuture<FlowBasedComputationResult> run(String workingVariantId, FlowBasedComputationParameters parameters) {
+        Objects.requireNonNull(workingVariantId);
+        Objects.requireNonNull(parameters);
+
+        // Change working variant
+        network.getVariantManager().setWorkingVariant(workingVariantId);
+        network.getVariantManager().allowVariantMultiThreadAccess(true);
+
+        FlowBasedComputationParameters parametersExtension = Objects.requireNonNull(parameters.getExtension(FlowBasedComputationParameters.class)); // Should not be null, checked previously
+
+        //todo implementation computation details
+        FlowBasedComputationResult result = new FlowBasedComputationResult(FlowBasedComputationResult.Status.SUCCESS);
+
+        network.getVariantManager().allowVariantMultiThreadAccess(false);
+        return CompletableFuture.completedFuture(result);
+    }
 
 }
--- flowbased-computation/flowbased-computation-api/src/main/java/com/farao_community/farao/flowbased_computation/FlowBasedComputationFactory.java
+++ flowbased-computation/flowbased-computation-api/src/main/java/com/farao_community/farao/flowbased_computation/FlowBasedComputationFactory.java
@@ -6,9 +6,12 @@
  */
 package com.farao_community.farao.flowbased_computation;
 
+import com.powsybl.commons.config.ComponentDefaultConfig;
 import com.powsybl.computation.ComputationManager;
 import com.powsybl.iidm.network.Network;
 import com.farao_community.farao.data.crac_file.CracFile;
+import com.powsybl.loadflow.LoadFlowFactory;
+import com.powsybl.sensitivity.SensitivityComputationFactory;
 
 /**
  * FlowBased computation factory
@@ -17,17 +20,17 @@ import com.farao_community.farao.data.crac_file.CracFile;
  * </p>
  *
  * @author Luc Di Gallo {@literal <luc.di-gallo at rte-france.com>}
- * @see FlowBasedComputation
  */
-public interface FlowBasedComputationFactory {
+public class FlowBasedComputationFactory {
 
-    /**
-     * @param criticalBranchesValuesProviderIn provider of critical branches values
-     * @param glskValuesProviderIn             provider of GLSK values
-     * @return an Interface of the Flowbased computation
-     */
-    FlowBasedComputation create(CriticalBranchesValuesProvider criticalBranchesValuesProviderIn,
-                                GlskValuesProvider glskValuesProviderIn);
 
-    FlowBasedComputation create(Network network, CracFile cracFile, ComputationManager computationManager, int i);
+    //todo add LinearGLSK or GlksFile... ? or getLinearGLSK from a File string File Path?
+
+    public FlowBasedComputation create(Network network, CracFile cracFile, ComputationManager computationManager, int priority) {
+        LoadFlowFactory loadFlowFactory = ComponentDefaultConfig.load().newFactoryImpl(LoadFlowFactory.class);
+        SensitivityComputationFactory sensitivityComputationFactory = ComponentDefaultConfig.load().newFactoryImpl(SensitivityComputationFactory.class);
+        return new FlowBasedComputation(network, cracFile, computationManager, loadFlowFactory, sensitivityComputationFactory);
+    }
+
+
 }
