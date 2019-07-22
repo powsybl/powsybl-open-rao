2f35d4a878c86156966a7e97224910d3c94e84cd
Marc Erkol <marc.erkol@rte-france.com>
2019-07-22 09:58:40 +0200

    save

--- ra-optimisation/ra-optimisation-api/src/test/java/com/farao_community/farao/ra_optimisation/RaoComputationTest.java
+++ ra-optimisation/ra-optimisation-api/src/test/java/com/farao_community/farao/ra_optimisation/RaoComputationTest.java
@@ -6,16 +6,25 @@
  */
 package com.farao_community.farao.ra_optimisation;
 
+import com.farao_community.farao.data.crac_file.CracFile;
+import com.farao_community.farao.data.crac_file.xlsx.model.TimesSeries;
+import com.farao_community.farao.data.crac_file.xlsx.service.ImportService;
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
+import com.powsybl.commons.config.ComponentDefaultConfig;
 import com.powsybl.commons.config.InMemoryPlatformConfig;
 import com.powsybl.commons.config.PlatformConfig;
+import com.powsybl.computation.local.LocalComputationManager;
+import com.powsybl.iidm.import_.Importer;
+import com.powsybl.iidm.import_.Importers;
+import com.powsybl.iidm.network.Network;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.FileSystem;
 
 /**
@@ -41,5 +50,18 @@ public class RaoComputationTest {
     @Test
     public void run() {
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void my_test() throws IOException {
+
+        ImportService importService = new ImportService();
+        CracFile cracFile = importService.importContacts(RaoComputationTest.class.getResourceAsStream("/exemple_crac_xlsx.xlsx"), TimesSeries.TIME_1830, "/exemple_crac_xlsx.xlsx");
+        InputStream is = RaoComputationTest.class.getResourceAsStream("/networkTest.uct");
+        Network net = Importers.loadNetwork("/networkTest.uct", is);
+        RaoComputation rao = ComponentDefaultConfig.load().newFactoryImpl(RaoComputationFactory.class).create(net, cracFile, LocalComputationManager.getDefault(), 0);
+
+        RaoComputationParameters parameters = RaoComputationParameters.load(platformConfig);
+        RaoComputationResult result = rao.run(net.getVariantManager().getWorkingVariantId(), parameters).join();
     }
 }
