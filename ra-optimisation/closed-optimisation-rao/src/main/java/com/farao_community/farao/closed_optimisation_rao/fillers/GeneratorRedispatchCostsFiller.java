3623b3b1fe114dc73def73143ca65f334c7a6e1d
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2019-07-26 11:31:28 +0200

    fix on generatorRedispatchCostsFiller

--- ra-optimisation/closed-optimisation-rao/src/main/java/com/farao_community/farao/closed_optimisation_rao/fillers/GeneratorRedispatchCostsFiller.java
+++ ra-optimisation/closed-optimisation-rao/src/main/java/com/farao_community/farao/closed_optimisation_rao/fillers/GeneratorRedispatchCostsFiller.java
@@ -22,32 +22,31 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static com.farao_community.farao.closed_optimisation_rao.fillers.FillersTools.*;
+
 /**
  * @author Sebastien Murgey {@literal <sebastien.murgey at rte-france.com>}
  */
 @AutoService(AbstractOptimisationProblemFiller.class)
 public class GeneratorRedispatchCostsFiller extends AbstractOptimisationProblemFiller {
-    private static final String REDISPATCH_VALUE_POSTFIX = "_redispatch_value";
-    private static final String REDISPATCH_ACTIVATION_POSTFIX = "_redispatch_activation";
-    private static final String REDISPATCH_COST_POSTFIX = "_redispatch_cost";
-    private static final String TOTAL_REDISPATCH_COST = "total_redispatch_cost";
 
-    private List<RedispatchRemedialActionElement> generatorsRedispatch;
 
-    /**
-     * Check if the remedial action is a Redispatch remedial action (i.e. with only
-     * one remedial action element and redispatch)
-     */
-    private boolean isRedispatchRemedialAction(RemedialAction remedialAction) {
-        return remedialAction.getRemedialActionElements().size() == 1 &&
-                remedialAction.getRemedialActionElements().get(0) instanceof RedispatchRemedialActionElement;
-    }
+    private List<RedispatchRemedialActionElement> generatorsRedispatchN;
+    private List<RedispatchRemedialActionElement> generatorsRedispatchCurative;
+
+
 
     @Override
     public void initFiller(Network network, CracFile cracFile, Map<String, Object> data) {
         super.initFiller(network, cracFile, data);
-        this.generatorsRedispatch = cracFile.getRemedialActions().stream()
-                .filter(this::isRedispatchRemedialAction)
+        this.generatorsRedispatchN = cracFile.getRemedialActions().stream()
+                .filter(ra -> isRedispatchRemedialAction(ra)).filter(ra -> isRemedialActionPreventiveFreeToUse(ra))
+                .flatMap(remedialAction -> remedialAction.getRemedialActionElements().stream())
+                .map(remedialActionElement -> (RedispatchRemedialActionElement) remedialActionElement)
+                .collect(Collectors.toList());
+
+        this.generatorsRedispatchCurative = cracFile.getRemedialActions().stream()
+                .filter(ra -> isRedispatchRemedialAction(ra)).filter(ra -> isRemedialActionCurativeFreeToUse(ra))
                 .flatMap(remedialAction -> remedialAction.getRemedialActionElements().stream())
                 .map(remedialActionElement -> (RedispatchRemedialActionElement) remedialActionElement)
                 .collect(Collectors.toList());
@@ -56,18 +55,33 @@ public class GeneratorRedispatchCostsFiller extends AbstractOptimisationProblemF
     @Override
     public List<String> variablesProvided() {
         List<String> variables = new ArrayList<>();
-        variables.addAll(generatorsRedispatch.stream().map(gen -> gen.getId() + REDISPATCH_ACTIVATION_POSTFIX)
+        variables.addAll(generatorsRedispatchN.stream().map(gen -> nameRedispatchActivationVariableN((gen.getId())))
                 .collect(Collectors.toList()));
-        variables.addAll(generatorsRedispatch.stream().map(gen -> gen.getId() + REDISPATCH_COST_POSTFIX)
+        variables.addAll(generatorsRedispatchN.stream().map(gen -> nameRedispatchCostVariableN(gen.getId()))
                 .collect(Collectors.toList()));
+        cracFile.getContingencies().forEach(cont -> {
+            variables.addAll(generatorsRedispatchCurative.stream().map(gen -> nameRedispatchActivationVariableCurative(cont.getId(), gen.getId()))
+                    .collect(Collectors.toList()));
+        });
+        cracFile.getContingencies().forEach(cont -> {
+            variables.addAll(generatorsRedispatchCurative.stream().map(gen -> nameRedispatchCostVariableCurative(cont.getId(), gen.getId()))
+                    .collect(Collectors.toList()));
+        });
+
         variables.add(TOTAL_REDISPATCH_COST);
+
         return variables;
     }
 
     @Override
     public List<String> variablesExpected() {
-        return generatorsRedispatch.stream().map(gen -> gen.getId() + REDISPATCH_VALUE_POSTFIX)
+        List<String> variablesList = generatorsRedispatchN.stream().map(gen -> nameRedispatchValueVariableN(gen.getId()))
                 .collect(Collectors.toList());
+        cracFile.getContingencies().forEach(cont -> {
+            variablesList.addAll(generatorsRedispatchCurative.stream().map(gen -> nameRedispatchValueVariableCurative(cont.getId(), gen.getId()))
+                    .collect(Collectors.toList()));
+        });
+        return variablesList;
     }
 
     @Override
@@ -78,15 +92,15 @@ public class GeneratorRedispatchCostsFiller extends AbstractOptimisationProblemF
         MPConstraint totalRedispatchCostEquation = solver.makeConstraint(0, 0);
         totalRedispatchCostEquation.setCoefficient(totalRedispatchCostVariable, 1);
 
-        generatorsRedispatch.forEach(gen -> {
+        generatorsRedispatchN.forEach(gen -> {
             String genId = gen.getId();
-            MPVariable redispatchValueVariable = Objects.requireNonNull(solver.lookupVariableOrNull(genId + REDISPATCH_VALUE_POSTFIX));
+            MPVariable redispatchValueVariable = Objects.requireNonNull(solver.lookupVariableOrNull(nameRedispatchValueVariableN(genId)));
 
             // Redispatch activation variable
-            MPVariable redispatchActivationVariable = solver.makeBoolVar(genId + REDISPATCH_ACTIVATION_POSTFIX);
+            MPVariable redispatchActivationVariable = solver.makeBoolVar(nameRedispatchActivationVariableN(genId));
 
             // Redispatch cost variable
-            MPVariable redispatchCostVariable = solver.makeNumVar(-infinity, infinity, genId + REDISPATCH_COST_POSTFIX);
+            MPVariable redispatchCostVariable = solver.makeNumVar(-infinity, infinity, nameRedispatchCostVariableN(genId));
 
             // Redispatch cost equation
             MPConstraint costEquation = solver.makeConstraint(0, 0);
@@ -104,6 +118,38 @@ public class GeneratorRedispatchCostsFiller extends AbstractOptimisationProblemF
             MPConstraint constraintUp = solver.makeConstraint(-infinity, 0);
             constraintUp.setCoefficient(redispatchValueVariable, 1);
             constraintUp.setCoefficient(redispatchActivationVariable, -redispatchValueVariable.ub());
+        });
+
+        cracFile.getContingencies().forEach(cont -> {
+            String contId = cont.getId();
+            generatorsRedispatchCurative.forEach( gen -> {
+
+                String genId = gen.getId();
+                MPVariable redispatchValueVariable = Objects.requireNonNull(solver.lookupVariableOrNull(nameRedispatchValueVariableCurative(contId,genId)));
+
+                // Redispatch activation variable
+                MPVariable redispatchActivationVariable = solver.makeBoolVar(nameRedispatchActivationVariableCurative(contId,genId));
+
+                // Redispatch cost variable
+                MPVariable redispatchCostVariable = solver.makeNumVar(-infinity, infinity, nameRedispatchCostVariableCurative(contId,genId));
+
+                // Redispatch cost equation
+                MPConstraint costEquation = solver.makeConstraint(0, 0);
+                costEquation.setCoefficient(redispatchCostVariable, 1);
+                costEquation.setCoefficient(redispatchActivationVariable, -gen.getStartupCost());
+                costEquation.setCoefficient(redispatchValueVariable, -gen.getMarginalCost());
+
+                // Total redispatch cost participation
+                totalRedispatchCostEquation.setCoefficient(redispatchCostVariable, -1);
+
+                // Constraint for enforcing redispatch to be 0 when not activated
+                MPConstraint constraintLow = solver.makeConstraint(0, infinity);
+                constraintLow.setCoefficient(redispatchValueVariable, 1);
+                constraintLow.setCoefficient(redispatchActivationVariable, -redispatchValueVariable.lb());
+                MPConstraint constraintUp = solver.makeConstraint(-infinity, 0);
+                constraintUp.setCoefficient(redispatchValueVariable, 1);
+                constraintUp.setCoefficient(redispatchActivationVariable, -redispatchValueVariable.ub());
+            });
         });
     }
 }
