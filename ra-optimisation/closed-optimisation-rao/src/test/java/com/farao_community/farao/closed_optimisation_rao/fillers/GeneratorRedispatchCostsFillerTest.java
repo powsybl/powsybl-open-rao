b3ac80c296aaf107f7b64e17541f83392d4e7fd1
Marc Erkol <marc.erkol@rte-france.com>
2019-07-30 09:29:08 +0200

    test of data

--- ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/fillers/GeneratorRedispatchCostsFillerTest.java
+++ ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/fillers/GeneratorRedispatchCostsFillerTest.java
@@ -2,6 +2,7 @@ package com.farao_community.farao.closed_optimisation_rao.fillers;
 
 import com.farao_community.farao.closed_optimisation_rao.JsonClosedOptimisationRaoResultTest;
 import com.farao_community.farao.data.crac_file.CracFile;
+import com.farao_community.farao.data.crac_file.RedispatchRemedialActionElement;
 import com.farao_community.farao.data.crac_file.json.JsonCracFile;
 import com.powsybl.iidm.import_.Importers;
 import com.powsybl.iidm.network.Network;
@@ -10,17 +11,24 @@ import org.junit.Test;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 public class GeneratorRedispatchCostsFillerTest {
 
     GeneratorRedispatchCostsFiller generatorRedispatchCostsFiller;
 
+    CracFile cracFile;
+
     @Before
     public void setUp() {
         InputStream is = JsonClosedOptimisationRaoResultTest.class.getResourceAsStream("/1_2nodes_preContingency_RD_N.xiidm");
         Network net = Importers.loadNetwork("1_2nodes_preContingency_RD_N.xiidm", is);
-        CracFile cracFile = JsonCracFile.read(CracFile.class.getResourceAsStream("/1_2nodes_preContingency_RD_N.json"));
+        cracFile = JsonCracFile.read(CracFile.class.getResourceAsStream("/1_2nodes_preContingency_RD_N.json"));
         Map<String, Object> data = new HashMap<>();
 
         generatorRedispatchCostsFiller = new GeneratorRedispatchCostsFiller();
@@ -29,8 +37,16 @@ public class GeneratorRedispatchCostsFillerTest {
 
     @Test
     public void test() {
-        generatorRedispatchCostsFiller.variablesExpected();
-        generatorRedispatchCostsFiller.variablesProvided();
+        List<String> variablesExpected = generatorRedispatchCostsFiller.variablesExpected();
+        assertNotNull(variablesExpected);
+        System.out.println(variablesExpected.toString());
+
+        List<String> variablesProvided = generatorRedispatchCostsFiller.variablesProvided();
+        assertNotNull(variablesExpected);
+
+        System.out.println(cracFile.toString());
+
+        generatorRedispatchCostsFiller.
         //TODO fillProblem()
     }
 }
