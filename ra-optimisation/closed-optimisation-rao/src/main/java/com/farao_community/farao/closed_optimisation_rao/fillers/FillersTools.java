6bc775b1330c48824b219ac300e07fc799d336e6
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2019-07-30 11:27:49 +0200

    update filler PstAngleVariablesFiller with curative RA

--- ra-optimisation/closed-optimisation-rao/src/main/java/com/farao_community/farao/closed_optimisation_rao/fillers/FillersTools.java
+++ ra-optimisation/closed-optimisation-rao/src/main/java/com/farao_community/farao/closed_optimisation_rao/fillers/FillersTools.java
@@ -1,5 +1,6 @@
 package com.farao_community.farao.closed_optimisation_rao.fillers;
 
+import com.farao_community.farao.data.crac_file.PstElement;
 import com.farao_community.farao.data.crac_file.RedispatchRemedialActionElement;
 import com.farao_community.farao.data.crac_file.RemedialAction;
 import com.farao_community.farao.data.crac_file.UsageRule;
@@ -18,7 +19,10 @@ public final class FillersTools {
     private static final String REDISPATCH_ACTIVATION_CURATIVE_POSTFIX = "_redispatch_activation_curative";
     private static final String REDISPATCH_COST_N_POSTFIX = "_redispatch_cost_n";
     private static final String REDISPATCH_COST_CURATIVE_POSTFIX = "_redispatch_cost_curative";
+    private static final String SHIFT_VALUE_N_POSTFIX = "_shift_value_n";
+    private static final String SHIFT_VALUE_CURATIVE_POSTFIX = "_shift_value_curative";
 
+    public static final String PST_SENSITIVITIES_DATA_NAME = "pst_branch_sensitivities";
     public static final String TOTAL_REDISPATCH_COST = "total_redispatch_cost";
     public static final String GEN_SENSITIVITIES_DATA_NAME = "generators_branch_sensitivities";
 
@@ -30,6 +34,15 @@ public final class FillersTools {
     public static boolean isRedispatchRemedialAction(RemedialAction remedialAction) {
         return remedialAction.getRemedialActionElements().size() == 1 &&
                 remedialAction.getRemedialActionElements().get(0) instanceof RedispatchRemedialActionElement;
+    }
+
+    /**
+     * Check if the remedial action is a PST remedial action (i.e. with only
+     * one remedial action element and pst)
+     */
+    public static boolean isPstRemedialAction(RemedialAction remedialAction) {
+        return remedialAction.getRemedialActionElements().size() == 1 &&
+                remedialAction.getRemedialActionElements().get(0) instanceof PstElement;
     }
 
     /**
@@ -102,5 +115,21 @@ public final class FillersTools {
     public static String nameRedispatchCostVariableCurative(String contingencyId, String remedialActionId)
     {
         return contingencyId + BLANK_CHARACTER + remedialActionId + REDISPATCH_COST_CURATIVE_POSTFIX;
+    }
+
+    /**
+     * Get standard name of PST's shift value variables for preventive remedial actions
+     */
+    public static String nameShiftValueVariableN(String remedialActionId)
+    {
+        return remedialActionId + SHIFT_VALUE_N_POSTFIX;
+    }
+
+    /**
+     * Get standard name of PST's shift value variables for curative remedial actions
+     */
+    public static String nameShiftValueVariableCurative(String contingencyId, String remedialActionId)
+    {
+        return contingencyId + BLANK_CHARACTER + remedialActionId + SHIFT_VALUE_CURATIVE_POSTFIX;
     }
 }
--- ra-optimisation/closed-optimisation-rao/src/main/java/com/farao_community/farao/closed_optimisation_rao/fillers/PstAngleVariablesFiller.java
+++ ra-optimisation/closed-optimisation-rao/src/main/java/com/farao_community/farao/closed_optimisation_rao/fillers/PstAngleVariablesFiller.java
@@ -9,7 +9,6 @@ package com.farao_community.farao.closed_optimisation_rao.fillers;
 import com.farao_community.farao.closed_optimisation_rao.AbstractOptimisationProblemFiller;
 import com.farao_community.farao.data.crac_file.CracFile;
 import com.farao_community.farao.data.crac_file.PstElement;
-import com.farao_community.farao.data.crac_file.RemedialAction;
 import com.farao_community.farao.data.crac_file.TypeOfLimit;
 import com.google.auto.service.AutoService;
 import com.google.ortools.linearsolver.MPSolver;
@@ -20,29 +19,46 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static com.farao_community.farao.closed_optimisation_rao.fillers.FillersTools.*;
+
 /**
  * @author Sebastien Murgey {@literal <sebastien.murgey at rte-france.com>}
  */
 @AutoService(AbstractOptimisationProblemFiller.class)
 public class PstAngleVariablesFiller extends AbstractOptimisationProblemFiller {
-    private static final String SHIFT_VALUE_POSTFIX = "_shift_value";
 
-    private List<PstElement> pstElement;
+    private List<PstElement> pstElementN;
+    private List<PstElement> pstElementCurative;
 
     /**
-     * Check if the remedial action is a PST remedial action (i.e. with only
-     * one remedial action element and PST)
+     * add PST shift value variable into MPSolver with appropriate bounds
      */
-    private boolean isPstRemedialAction(RemedialAction remedialAction) {
-        return remedialAction.getRemedialActionElements().size() == 1 &&
-                remedialAction.getRemedialActionElements().get(0) instanceof PstElement;
+    private void buildPstShitValueVariable(String varName, PstElement pst, MPSolver solver){
+        PhaseTapChanger phaseTapChanger = network.getTwoWindingsTransformer(pst.getId()).getPhaseTapChanger();
+        int lowTapPosition = pst.getTypeOfLimit() == TypeOfLimit.ABSOLUTE ? pst.getMinStepRange() : pst.getMinStepRange() + phaseTapChanger.getTapPosition();
+        int highTapPosition = pst.getTypeOfLimit() == TypeOfLimit.ABSOLUTE ? pst.getMaxStepRange() : pst.getMaxStepRange() + phaseTapChanger.getTapPosition();
+        // Considering alpha is always between alpha low and alpha high
+        double alphaLowStep = phaseTapChanger.getStep(lowTapPosition).getAlpha();
+        double alphaHighStep = phaseTapChanger.getStep(highTapPosition).getAlpha();
+        double alphaMin = Math.min(alphaLowStep, alphaHighStep);
+        double alphaMax = Math.max(alphaLowStep, alphaHighStep);
+        double alphaInit = phaseTapChanger.getCurrentStep().getAlpha();
+        solver.makeNumVar(alphaMin - alphaInit, alphaMax - alphaInit, varName );
     }
 
     @Override
     public void initFiller(Network network, CracFile cracFile, Map<String, Object> data) {
         super.initFiller(network, cracFile, data);
-        this.pstElement = cracFile.getRemedialActions().stream()
-                .filter(this::isPstRemedialAction)
+        this.pstElementN = cracFile.getRemedialActions().stream()
+                .filter(ra -> isPstRemedialAction(ra))
+                .filter(ra -> isRemedialActionPreventiveFreeToUse(ra))
+                .flatMap(remedialAction -> remedialAction.getRemedialActionElements().stream())
+                .map(remedialActionElement -> (PstElement) remedialActionElement)
+                .collect(Collectors.toList());
+
+        this.pstElementCurative = cracFile.getRemedialActions().stream()
+                .filter(ra -> isPstRemedialAction(ra))
+                .filter(ra -> isRemedialActionCurativeFreeToUse(ra))
                 .flatMap(remedialAction -> remedialAction.getRemedialActionElements().stream())
                 .map(remedialActionElement -> (PstElement) remedialActionElement)
                 .collect(Collectors.toList());
@@ -50,23 +66,28 @@ public class PstAngleVariablesFiller extends AbstractOptimisationProblemFiller {
 
     @Override
     public void fillProblem(MPSolver solver) {
-        pstElement.forEach(pst -> {
-            PhaseTapChanger phaseTapChanger = network.getTwoWindingsTransformer(pst.getId()).getPhaseTapChanger();
-            int lowTapPosition = pst.getTypeOfLimit() == TypeOfLimit.ABSOLUTE ? pst.getMinStepRange() : pst.getMinStepRange() + phaseTapChanger.getTapPosition();
-            int highTapPosition = pst.getTypeOfLimit() == TypeOfLimit.ABSOLUTE ? pst.getMaxStepRange() : pst.getMaxStepRange() + phaseTapChanger.getTapPosition();
-            // Considering alpha is always between alpha low and alpha high
-            double alphaLowStep = phaseTapChanger.getStep(lowTapPosition).getAlpha();
-            double alphaHighStep = phaseTapChanger.getStep(highTapPosition).getAlpha();
-            double alphaMin = Math.min(alphaLowStep, alphaHighStep);
-            double alphaMax = Math.max(alphaLowStep, alphaHighStep);
-            double alphaInit = phaseTapChanger.getCurrentStep().getAlpha();
-            solver.makeNumVar(alphaMin - alphaInit, alphaMax - alphaInit, pst.getId() + SHIFT_VALUE_POSTFIX);
+        // fill problem with preventive PST variables
+        pstElementN.forEach(pst -> {
+            buildPstShitValueVariable(nameShiftValueVariableN(pst.getId()), pst, solver);
+        });
+
+        // fill problem with curative PST variables
+        cracFile.getContingencies().forEach( cont ->{
+            pstElementCurative.forEach(pst -> {
+                buildPstShitValueVariable(nameShiftValueVariableCurative(cont.getId(), pst.getId()), pst, solver);
+            });
+
         });
     }
 
     @Override
     public List<String> variablesProvided() {
-        return pstElement.stream().map(pst -> pst.getId() + SHIFT_VALUE_POSTFIX)
+        List<String> variablesList = pstElementN.stream().map(gen -> nameShiftValueVariableN(gen.getId()))
                 .collect(Collectors.toList());
+        cracFile.getContingencies().forEach(cont -> {
+            variablesList.addAll(pstElementCurative.stream().map(gen -> nameShiftValueVariableCurative(cont.getId(), gen.getId()))
+                    .collect(Collectors.toList()));
+        });
+        return variablesList;
     }
 }
