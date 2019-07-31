4d6c2b1ebfaa9a1055257f06260c93cb0d05175f
Marc Erkol <marc.erkol@rte-france.com>
2019-07-31 16:03:42 +0200

    editions of tests

---
+++ ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/MPObjectiveMock.java
@@ -0,0 +1,37 @@
+package com.farao_community.farao.closed_optimisation_rao;
+
+import com.google.ortools.linearsolver.MPObjective;
+import com.google.ortools.linearsolver.MPVariable;
+import com.google.ortools.linearsolver.operations_research_linear_solverJNI;
+
+import java.util.HashMap;
+
+public class MPObjectiveMock extends MPObjective {
+
+    private HashMap<String, Double> coefficients;
+    private boolean isMinimization;
+
+    protected MPObjectiveMock() {
+        super(0, true);
+        coefficients = new HashMap<>();
+        isMinimization = true;
+    }
+
+    @Override
+    public void clear() {
+        coefficients.clear();
+    }
+
+    @Override
+    public void setCoefficient(MPVariable var, double coeff) {
+        coefficients.put(var.name(), coeff);
+    }
+
+    @Override
+    public double getCoefficient(MPVariable var) {
+        return coefficients.get(var.name());
+    }
+
+    @Override
+    public void setMinimization(){isMinimization = true;}
+}
