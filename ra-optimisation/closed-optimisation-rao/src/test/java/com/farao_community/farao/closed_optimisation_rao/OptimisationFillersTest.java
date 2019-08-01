91758153dd73664e2db1b6621f6936d839537863
Marc Erkol <marc.erkol@rte-france.com>
2019-08-01 11:24:10 +0200

    pst test for rao filler is ok

--- ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/OptimisationFillersTest.java
+++ ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/OptimisationFillersTest.java
@@ -3,7 +3,6 @@ package com.farao_community.farao.closed_optimisation_rao;
 import com.farao_community.farao.closed_optimisation_rao.fillers.*;
 import com.farao_community.farao.data.crac_file.CracFile;
 import com.farao_community.farao.data.crac_file.json.JsonCracFile;
-import com.google.ortools.linearsolver.MPSolver;
 import com.powsybl.iidm.import_.Importers;
 import com.powsybl.iidm.network.Network;
 import org.apache.commons.lang3.tuple.Pair;
@@ -13,9 +12,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Queue;
-
-import static junit.framework.TestCase.assertTrue;
 
 public class OptimisationFillersTest {
 
@@ -74,10 +70,64 @@ public class OptimisationFillersTest {
 
     }
 
-
     @Test
-    public void test() {
+    public void testcase2() {
+        CracFile cracFile = JsonCracFile.read(CracFile.class.getResourceAsStream("/5_3nodes_preContingency_PSTandRD_N-1.json"));
+        InputStream is = JsonClosedOptimisationRaoResultTest.class.getResourceAsStream("/5_3nodes_preContingency_PSTandRD_N-1.xiidm");
+        Network network = Importers.loadNetwork("/5_3nodes_preContingency_PSTandRD_N-1.xiidm", is);
 
+        HashMap<String, Object> data = new HashMap<>();
+        HashMap<Pair<String, String>, Double> pstSensitivities = new HashMap<>();
+        HashMap<Pair<String, String>, Double> generatorSensitivities = new HashMap<>();
+        HashMap<String, Double> referenceFlows = new HashMap<>();
 
+        pstSensitivities.put(Pair.of("MONITORED_FRANCE_BELGIUM_1", "PST"), -69.81317138671875);
+        pstSensitivities.put(Pair.of("MONITORED_FRANCE_BELGIUM_3", "PST"), -139.6263427734375);
+        pstSensitivities.put(Pair.of("MONITORED_FRANCE_BELGIUM_2", "PST"), -139.6263427734375);
+        pstSensitivities.put(Pair.of("C2_MONITORED_FRANCE_BELGIUM_3", "PST"), 186.16845703125);
+        pstSensitivities.put(Pair.of("C1_MONITORED_FRANCE_BELGIUM_1", "PST"), -139.6263427734375);
+        pstSensitivities.put(Pair.of("C2_MONITORED_FRANCE_BELGIUM_2", "PST"), 186.16845703125);
+        pstSensitivities.put(Pair.of("C1_MONITORED_FRANCE_BELGIUM_3", "PST"), -139.6263427734375);
+        generatorSensitivities.put(Pair.of("C1_MONITORED_FRANCE_BELGIUM_3", "GENERATOR_FR"), 0.21739129722118378);
+        generatorSensitivities.put(Pair.of("MONITORED_FRANCE_BELGIUM_3", "GENERATOR_BE"), 0.28260868787765503);
+        generatorSensitivities.put(Pair.of("MONITORED_FRANCE_BELGIUM_2", "GENERATOR_BE"), 0.28260868787765503);
+        generatorSensitivities.put(Pair.of("C1_MONITORED_FRANCE_BELGIUM_1", "GENERATOR_FR"), -0.21739129722118378);
+        generatorSensitivities.put(Pair.of("C2_MONITORED_FRANCE_BELGIUM_3", "GENERATOR_BE"), 0.18840579688549042);
+        generatorSensitivities.put(Pair.of("C2_MONITORED_FRANCE_BELGIUM_2", "GENERATOR_BE"), 0.37681159377098083);
+        generatorSensitivities.put(Pair.of("MONITORED_FRANCE_BELGIUM_1", "GENERATOR_BE"), -0.14130434393882751);
+        generatorSensitivities.put(Pair.of("C2_MONITORED_FRANCE_BELGIUM_2", "GENERATOR_FR"), -0.28985506296157837);
+        generatorSensitivities.put(Pair.of("C2_MONITORED_FRANCE_BELGIUM_3", "GENERATOR_FR"), -0.14492753148078918);
+        generatorSensitivities.put(Pair.of("MONITORED_FRANCE_BELGIUM_2", "GENERATOR_FR"), -0.21739129722118378);
+        generatorSensitivities.put(Pair.of("C1_MONITORED_FRANCE_BELGIUM_1", "GENERATOR_BE"), 0.28260868787765503);
+        generatorSensitivities.put(Pair.of("MONITORED_FRANCE_BELGIUM_3", "GENERATOR_FR"), -0.21739129722118378);
+        generatorSensitivities.put(Pair.of("C1_MONITORED_FRANCE_BELGIUM_3", "GENERATOR_BE"), 0.28260868787765503);
+        generatorSensitivities.put(Pair.of("MONITORED_FRANCE_BELGIUM_1", "GENERATOR_FR"), -0.10869564861059189);
+
+        referenceFlows.put("MONITORED_FRANCE_BELGIUM_1", -158.11209106445312);
+        referenceFlows.put("MONITORED_FRANCE_BELGIUM_3", -316.22418212890625);
+        referenceFlows.put("MONITORED_FRANCE_BELGIUM_2", -316.22418212890625);
+        referenceFlows.put("C1_MONITORED_FRANCE_BELGIUM_1", -316.22418212890625);
+        referenceFlows.put("C2_MONITORED_FRANCE_BELGIUM_3", -378.36773681640625);
+        referenceFlows.put("C2_MONITORED_FRANCE_BELGIUM_2", -421.63226318359375);
+        referenceFlows.put("C1_MONITORED_FRANCE_BELGIUM_3", -316.22418212890625);
+
+        data.put("pst_branch_sensitivities", pstSensitivities);
+        data.put("generators_branch_sensitivities", generatorSensitivities);
+        data.put("reference_flows", referenceFlows);
+
+        List<String> fillersToTest = new ArrayList<>();
+
+        fillersToTest.add(BranchMarginsPositivityConstraintFiller.class.getName());
+        fillersToTest.add(BranchMarginsVariablesFiller.class.getName());
+        fillersToTest.add(GeneratorRedispatchCostsFiller.class.getName());
+        fillersToTest.add(GeneratorRedispatchVariablesFiller.class.getName());
+        fillersToTest.add(PstAngleImpactOnBranchFlowFiller.class.getName());
+        fillersToTest.add(PstAngleVariablesFiller.class.getName());
+        fillersToTest.add(RedispatchCostMinimizationObjectiveFiller.class.getName());
+        fillersToTest.add(RedispatchEquilibriumConstraintFiller.class.getName());
+        fillersToTest.add(RedispatchImpactOnBranchFlowFiller.class.getName());
+        FillersTestCase fillersTestCase = new FillersTestCase(cracFile, network, data, fillersToTest);
+
+        fillersTestCase.fillersTest();
     }
 }
