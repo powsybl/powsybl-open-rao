be7c598dd17b4f51190056029602d666ae97675a
Marc Erkol <marc.erkol@rte-france.com>
2019-08-01 14:01:57 +0200

    correction for checkstyle

--- ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/OptimisationFillersTest.java
+++ ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/OptimisationFillersTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.closed_optimisation_rao;
 
 import com.farao_community.farao.closed_optimisation_rao.fillers.*;
@@ -12,7 +18,10 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
+/**
+ * @author Marc Erkol {@literal <marc.erkol at rte-france.com>}
+ * @author Baptiste Seguinot {@literal <baptiste.seguinot at rte-france.com>}
+ */
 public class OptimisationFillersTest {
 
     @Test
