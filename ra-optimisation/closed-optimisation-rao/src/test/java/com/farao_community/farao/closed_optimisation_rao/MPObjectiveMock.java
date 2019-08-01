8f831878b89ac851463df1ffaafcfa928e0f4ead
Marc Erkol <marc.erkol@rte-france.com>
2019-08-01 14:59:29 +0200

    add tags name

--- ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/MPObjectiveMock.java
+++ ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/MPObjectiveMock.java
@@ -1,10 +1,18 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.closed_optimisation_rao;
 
 import com.google.ortools.linearsolver.MPObjective;
 import com.google.ortools.linearsolver.MPVariable;
 
 import java.util.HashMap;
-
+/**
+ * @author Marc Erkol {@literal <marc.erkol at rte-france.com>}
+ */
 public class MPObjectiveMock extends MPObjective {
 
     private HashMap<String, Double> coefficients;
