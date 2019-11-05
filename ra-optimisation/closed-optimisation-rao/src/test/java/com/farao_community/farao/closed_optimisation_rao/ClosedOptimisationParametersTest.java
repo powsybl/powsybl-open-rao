1577a9fe7c00e89672059429fc5e85e9d5dfea46
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2019-11-05 17:20:57 +0100

    Add some tests on ClosedOptimisationRaoParameters

---
+++ ra-optimisation/closed-optimisation-rao/src/test/java/com/farao_community/farao/closed_optimisation_rao/ClosedOptimisationParametersTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.closed_optimisation_rao;
+
+import com.farao_community.farao.ra_optimisation.RaoComputationParameters;
+import com.powsybl.commons.config.PlatformConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * @author Baptiste Seguinot {@literal <baptiste.seguinot at rte-france.com>}
+ */
+public class ClosedOptimisationParametersTest {
+
+    private RaoComputationParameters parameters;
+    private ClosedOptimisationRaoParameters parametersExtension;
+
+    @Before
+    public void setUp() {
+        PlatformConfig config = Mockito.mock(PlatformConfig.class);
+        parameters = RaoComputationParameters.load(config);
+        parametersExtension = parameters.getExtension(ClosedOptimisationRaoParameters.class);
+    }
+
+    @Test
+    public void checkRaoConfigurationTest() {
+        List<String> qualityCheckOfDefaultParameters = ConfigurationUtil.checkRaoConfiguration(parameters);
+        assertTrue(qualityCheckOfDefaultParameters.isEmpty());
+    }
+
+    @Test
+    public void getOptimisationConstantsTest() {
+        Map<String, Double> constants = ConfigurationUtil.getOptimisationConstants(parametersExtension);
+        assertTrue(constants.containsKey(ClosedOptimisationRaoNames.OVERLOAD_PENALTY_COST_NAME));
+    }
+}
