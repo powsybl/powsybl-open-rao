f84d137fc7fc9873fb7eb35b8dc77573c4781388
Joris Mancini <joris.mancini@rte-france.com>
2019-11-07 17:53:19 +0100

    Create a new module for greedy optimisation in ra-optimisation with basic structure for a RAO

---
+++ ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/SearchTreeRao.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.search_tree_rao;
+
+import com.farao_community.farao.ra_optimisation.RaoComputation;
+import com.farao_community.farao.ra_optimisation.RaoComputationParameters;
+import com.farao_community.farao.ra_optimisation.RaoComputationResult;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
+ */
+public class SearchTreeRao implements RaoComputation {
+    @Override
+    public CompletableFuture<RaoComputationResult> run(String s, RaoComputationParameters raoComputationParameters) {
+        return null;
+    }
+}
---
+++ ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/SearchTreeRaoResult.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.search_tree_rao;
+
+import com.farao_community.farao.ra_optimisation.RaoComputationResult;
+import com.powsybl.commons.extensions.AbstractExtension;
+
+/**
+ * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
+ */
+public class SearchTreeRaoResult extends AbstractExtension<RaoComputationResult> {
+    @Override
+    public String getName() {
+        return null;
+    }
+}
