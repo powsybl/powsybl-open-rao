1748a1af86eab22c033fd10ab607b2fabb7710c6
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2019-11-26 11:11:22 +0100

    update before checkout
    Signed-off-by: Baptiste Seguinot <baptiste.seguinot@rte-france.com>

--- ra-optimisation/rao-api/src/main/java/com/farao_community/farao/ra_optimisation/RaOptimizerProvider.java
+++ ra-optimisation/rao-api/src/main/java/com/farao_community/farao/ra_optimisation/RaOptimizerProvider.java
@@ -1,6 +1,5 @@
 package com.farao_community.farao.ra_optimisation;
 
-import com.farao_community.farao.data.crac_file.CracFile;
 import com.powsybl.commons.Versionable;
 import com.powsybl.computation.ComputationManager;
 import com.powsybl.iidm.network.Network;
@@ -8,5 +7,5 @@ import com.powsybl.iidm.network.Network;
 import java.util.concurrent.CompletableFuture;
 
 public interface RaOptimizerProvider extends Versionable {
-    CompletableFuture<RaoComputationResult> run(Network network, CracFile cracFile, ComputationManager computationManager, String workingVariantId, RaoComputationParameters parameters);
+    CompletableFuture<RaoComputationResult> run(Network network, Crac crac, ComputationManager computationManager, String workingVariantId, RaoComputationParameters parameters);
 }
