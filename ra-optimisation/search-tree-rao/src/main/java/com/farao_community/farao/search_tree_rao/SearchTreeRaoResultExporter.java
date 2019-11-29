5dea9318d96459680bc77f04c1979c8ce329b927
ruidian <47722616+pengbo-ruidian@users.noreply.github.com>
2019-11-29 14:19:02 +0100

    [Josiris][US 1.4][FAR-154][FAR-155] SearchTreeRaoResult import/export (#88)
    * Create a new module for greedy optimisation in ra-optimisation with basic structure for a RAO
    
    * implement SearchTreeRaoFactory
    
    * add enum: ComputationStatus and StopCriterion
    
    * change according to spec FAR-154
    
    * update farao version for search-tree-rao
    
    * test for search tree rao result
    
    * minor
    
    * minor
    
    * remove empty classes
    
    * correction
    
    * modif after review
    
    * SearchTreeRaoResult exporter
    
    * minor
    
    * enrich searchTreeRaoResult format. see FAR-154 and FAR-155.
    
    * remove duplica
    
    * modif according to review
    
    * comment
    
    * minor
    
    * modif after review

---
+++ ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/SearchTreeRaoResultExporter.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.search_tree_rao;
+
+import java.io.OutputStream;
+
+/**
+ * @author Pengbo Wang {@literal <pengbo.wang at rte-international.com>}
+ */
+public interface SearchTreeRaoResultExporter {
+
+    /**
+     * Get the format of this exporter
+     *
+     * @return the format name of this exporter
+     */
+    String getFormat();
+
+    /**
+     * Export a result of a remedial actions optimisation
+     *
+     * @param result The result of the remedial action optimisation
+     * @param os The output stream used for the export
+     */
+    void export(SearchTreeRaoResult result, OutputStream os);
+
+}
