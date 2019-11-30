198e94fc341bb293ff2bd41d81b1bdf837ff4ea1
pengbo-ruidian <wangpengbo.ruidian@gmail.com>
2019-11-30 20:24:32 +0100

    header

--- ra-optimisation/search-tree-rao/src/test/java/com.farao_community.farao.search_tree_rao/SearchTreeRaoToolTest.java
+++ ra-optimisation/search-tree-rao/src/test/java/com.farao_community.farao.search_tree_rao/SearchTreeRaoToolTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.search_tree_rao;
 
 import com.farao_community.farao.commons.AbstractToolTest;
@@ -9,6 +15,9 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.Collections;
 
+/**
+ * @author Pengbo Wang {@literal <pengbo.wang at rte-international.com>}
+ */
 public class SearchTreeRaoToolTest extends AbstractToolTest {
 
     private static final String COMMAND_NAME = "search-tree-rao";
