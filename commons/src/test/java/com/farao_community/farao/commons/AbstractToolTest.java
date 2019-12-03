b311392eb90053d50f5394984a776b19658e1950
ruidian <47722616+pengbo-ruidian@users.noreply.github.com>
2019-12-03 11:25:17 +0100

    [JOSIRIS][US1.1][FAR-167] SearchTreeRao Tool. move from PR#92 (#93)
    * search tree rao tool structure

--- commons/src/test/java/com/farao_community/farao/commons/AbstractToolTest.java
+++ commons/src/test/java/com/farao_community/farao/commons/AbstractToolTest.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.farao_community.farao.flowbased_computation;
+package com.farao_community.farao.commons;
 
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
