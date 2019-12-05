a5768fe8a1f9ee0ee3d33737177db0d9c539152d
Joris Mancini <joris.mancini@rte-france.com>
2019-12-05 11:04:12 +0100

    Add a test on PstRange

--- data/crac-file/crac-impl/src/test/java/com/farao_community/farao/data/crac_impl/remedial_action/range_action/PstRangeTest.java
+++ data/crac-file/crac-impl/src/test/java/com/farao_community/farao/data/crac_impl/remedial_action/range_action/PstRangeTest.java
@@ -7,7 +7,9 @@
 
 package com.farao_community.farao.data.crac_impl.remedial_action.range_action;
 
+import com.farao_community.farao.commons.FaraoException;
 import com.farao_community.farao.data.crac_api.NetworkElement;
+import com.farao_community.farao.data.crac_impl.remedial_action.network_action.PstSetpoint;
 import com.powsybl.iidm.import_.Importers;
 import com.powsybl.iidm.network.Network;
 import org.junit.Test;
@@ -30,5 +32,20 @@ public class PstRangeTest {
         assertEquals(0, network.getTwoWindingsTransformer("BBE2AA1  BBE3AA1  1").getPhaseTapChanger().getTapPosition());
         pstRange.apply(network, 12);
         assertEquals(12, network.getTwoWindingsTransformer("BBE2AA1  BBE3AA1  1").getPhaseTapChanger().getTapPosition());
+    }
+
+    @Test
+    public void applyOutOfBound() {
+        Network network = Importers.loadNetwork(
+            "TestCase12Nodes.uct",
+            getClass().getResourceAsStream("/TestCase12Nodes.uct")
+        );
+        PstRange pstRange = new PstRange(new NetworkElement("BBE2AA1  BBE3AA1  1", "BBE2AA1  BBE3AA1  1"));
+        try {
+            pstRange.apply(network, 50);
+            fail();
+        } catch (FaraoException ignored) {
+
+        }
     }
 }
