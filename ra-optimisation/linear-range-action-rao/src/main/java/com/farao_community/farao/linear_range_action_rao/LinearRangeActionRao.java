dd3c9c5e3f2876728eca4119149cbb090345ce00
Joris Mancini <joris.mancini@rte-france.com>
2019-12-05 11:01:49 +0100

    create linear-range-action-rao module

---
+++ ra-optimisation/linear-range-action-rao/src/main/java/com/farao_community/farao/linear_range_action_rao/LinearRangeActionRao.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.linear_range_action_rao;
+
+import com.farao_community.farao.data.crac_api.Crac;
+import com.farao_community.farao.ra_optimisation.RaoComputationResult;
+import com.farao_community.farao.rao_api.RaoParameters;
+import com.farao_community.farao.rao_api.RaoProvider;
+import com.powsybl.computation.ComputationManager;
+import com.powsybl.iidm.network.Network;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
+ */
+public class LinearRangeActionRao implements RaoProvider {
+
+    @Override
+    public String getName() {
+        return "Linear Range Action Rao";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+
+    @Override
+    public CompletableFuture<RaoComputationResult> run(Network network, Crac crac, String variantId, ComputationManager computationManager, RaoParameters parameters) {
+        // TODO : implement LinearRangeActionRao
+        return null;
+    }
+}
