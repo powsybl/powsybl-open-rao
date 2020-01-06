03c562fda812199bd28e0a65fa2da72011236584
pengbo-ruidian <wangpengbo.ruidian@gmail.com>
2020-01-06 18:52:41 +0100

    update us test case

--- ra-optimisation/linear-range-action-rao/src/test/java/com/farao_community/farao/linear_range_action_rao/LinearRangeActionRaoTest.java
+++ ra-optimisation/linear-range-action-rao/src/test/java/com/farao_community/farao/linear_range_action_rao/LinearRangeActionRaoTest.java
@@ -12,16 +12,8 @@ import com.farao_community.farao.data.crac_impl.ComplexContingency;
 import com.farao_community.farao.data.crac_impl.SimpleCnec;
 import com.farao_community.farao.data.crac_impl.SimpleCrac;
 import com.farao_community.farao.data.crac_impl.SimpleState;
-import com.farao_community.farao.data.crac_impl.range_domain.AbsoluteFixedRange;
-import com.farao_community.farao.data.crac_impl.range_domain.RelativeDynamicRange;
-import com.farao_community.farao.data.crac_impl.range_domain.RelativeFixedRange;
-import com.farao_community.farao.data.crac_impl.remedial_action.network_action.*;
-import com.farao_community.farao.data.crac_impl.remedial_action.range_action.*;
 import com.farao_community.farao.data.crac_impl.threshold.AbsoluteFlowThreshold;
-import com.farao_community.farao.data.crac_impl.threshold.VoltageThreshold;
-import com.farao_community.farao.data.crac_impl.usage_rule.FreeToUse;
-import com.farao_community.farao.data.crac_impl.usage_rule.OnConstraint;
-import com.farao_community.farao.data.crac_impl.usage_rule.OnContingency;
+import com.farao_community.farao.data.crac_impl.threshold.RelativeFlowThreshold;
 import com.farao_community.farao.rao_api.RaoParameters;
 import com.farao_community.farao.util.LoadFlowService;
 import com.farao_community.farao.util.SensitivityComputationService;
@@ -46,12 +38,6 @@ import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
-import static com.farao_community.farao.data.crac_api.ActionType.CLOSE;
-import static com.farao_community.farao.data.crac_api.ActionType.OPEN;
-import static com.farao_community.farao.data.crac_api.Direction.IN;
-import static com.farao_community.farao.data.crac_api.Direction.OUT;
-import static com.farao_community.farao.data.crac_api.Side.LEFT;
-import static com.farao_community.farao.data.crac_api.Side.RIGHT;
 import static org.junit.Assert.*;
 
 /**
@@ -105,137 +91,54 @@ public class LinearRangeActionRaoTest {
     }
 
     private static Crac create() {
+        Crac crac = new SimpleCrac("idSimpleCracTestUS", "nameSimpleCracTestUS");
 
-        // Redispatching
-        NetworkElement generator = new NetworkElement("idGenerator", "My Generator");
-        Redispatching rd = new Redispatching(10, 20, 18, 1000, 12, generator);
-        rd.setMinimumPower(rd.getMinimumPower() + 1);
-        rd.setMaximumPower(rd.getMaximumPower() + 1);
-        rd.setTargetPower(rd.getTargetPower() + 1);
-        rd.setStartupCost(rd.getStartupCost() + 1);
-        rd.setMarginalCost(rd.getMarginalCost() + 1);
-
-        // Range domain
-        RelativeFixedRange relativeFixedRange = new RelativeFixedRange(0, 1);
-        relativeFixedRange.setMin(1);
-        relativeFixedRange.setMax(10);
-        RelativeDynamicRange relativeDynamicRange = new RelativeDynamicRange(0, 1);
-        relativeDynamicRange.setMin(100);
-        relativeDynamicRange.setMax(1000);
-        AbsoluteFixedRange absoluteFixedRange = new AbsoluteFixedRange(0, 1);
-        absoluteFixedRange.setMin(10);
-        absoluteFixedRange.setMax(1000);
-
-        // PstRange
-        NetworkElement pst1 = new NetworkElement("BBE2AA1  BBE3AA1  1", "BBE2AA1  BBE3AA1  1");
-        PstRange pstRange1 = new PstRange(null);
-        pstRange1.setNetworkElement(pst1);
-
-        // HvdcRange
-        NetworkElement hvdc1 = new NetworkElement("idHvdc1", "My Hvdc 1");
-        HvdcRange hvdcRange1 = new HvdcRange(null);
-        hvdcRange1.setNetworkElement(hvdc1);
-
-        // GeneratorRange
-        NetworkElement generator1 = new NetworkElement("idGen1", "My Generator 1");
-        InjectionRange injectionRange1 = new InjectionRange(null);
-        injectionRange1.setNetworkElement(generator1);
-
-        // Countertrading
-        Countertrading countertrading = new Countertrading();
-
-        // Topology
-        NetworkElement line1 = new NetworkElement("idLine1", "My Line 1");
-        Topology topology1 = new Topology(line1, OPEN);
-        NetworkElement switch1 = new NetworkElement("idSwitch1", "My Switch 1");
-        Topology topology2 = new Topology(null, null);
-        topology2.setNetworkElement(switch1);
-        topology2.setActionType(CLOSE);
-
-        // Hvdc setpoint
-        HvdcSetpoint hvdcSetpoint = new HvdcSetpoint(switch1, 0);
-        hvdcSetpoint.setNetworkElement(line1);
-        hvdcSetpoint.setSetpoint(1000);
-
-        // Pst setpoint
-        PstSetpoint pstSetpoint = new PstSetpoint(switch1, 0);
-        pstSetpoint.setNetworkElement(pst1);
-        pstSetpoint.setSetpoint(5);
-
-        // Injection setpoint
-        InjectionSetpoint injectionSetpoint = new InjectionSetpoint(switch1, 0);
-        injectionSetpoint.setNetworkElement(generator1);
-        injectionSetpoint.setSetpoint(100);
-
-        ComplexContingency contingency = new ComplexContingency("idContingency");
+        ComplexContingency contingency1 = new ComplexContingency("Contingency FR1 FR3", "Trip of FFR1AA1 FFR3AA1 1",
+                new HashSet<>(Arrays.asList(new NetworkElement("FFR1AA1  FFR3AA1  1"))));
+        crac.addContingency(contingency1);
+        ComplexContingency contingency2 = new ComplexContingency("Contingency FR1 FR2", "Trip of FFR1AA1 FFR2AA1 1",
+                new HashSet<>(Arrays.asList(new NetworkElement("FFR1AA1  FFR2AA1  1"))));
+        crac.addContingency(contingency2);
 
         // Instant
         Instant basecase = new Instant("initial", 0);
-        Instant curative = new Instant("curative", -1);
-        curative.setSeconds(200);
+        Instant defaut = new Instant("default", 60);
+        Instant curative = new Instant("curative", 1200);
+
+        //NetworkElement
+        NetworkElement monitoredElement1 = new NetworkElement("BBE2AA1  FFR3AA1  1", "BBE2AA1  FFR3AA1  1 name");
+        NetworkElement monitoredElement2 = new NetworkElement("FFR2AA1  DDE3AA1  1", "FFR2AA1  DDE3AA1  1 name");
 
         // State
         State stateBasecase = new SimpleState(Optional.empty(), basecase);
-        State stateCurative = new SimpleState(Optional.of(contingency), curative);
-
-        NetworkElement monitoredElement = new NetworkElement("BBE2AA1  FFR3AA1  1", "BBE2AA1  FFR3AA1  1 name");
+        State stateCurativeContingency1 = new SimpleState(Optional.of(contingency1), curative);
+        State stateCurativeContingency2 = new SimpleState(Optional.of(contingency2), curative);
 
         // Thresholds
-        AbsoluteFlowThreshold threshold1 = new AbsoluteFlowThreshold(Unit.AMPERE, LEFT, IN, 1000);
-        threshold1.setSide(RIGHT);
-        threshold1.setDirection(OUT);
-        threshold1.setMaxValue(999);
-        VoltageThreshold threshold2 = new VoltageThreshold(280, 300);
-        threshold2.setMinValue(275);
-        threshold2.setMaxValue(305);
+        AbsoluteFlowThreshold thresholdAbsFlow = new AbsoluteFlowThreshold(Unit.AMPERE, Side.LEFT, Direction.IN, 1500);
+        RelativeFlowThreshold thresholdRelativeFlow = new RelativeFlowThreshold(Unit.AMPERE, Side.LEFT, Direction.IN, 30);
 
         // CNECs
-        SimpleCnec cnec1 = new SimpleCnec("idCnec1", "Cnec 1", null, threshold1, stateCurative);
-        cnec1.setCriticalNetworkElement(monitoredElement);
-        SimpleCnec cnec2 = new SimpleCnec("idCnec2", "Cnec 2", monitoredElement, null, null);
-        cnec2.setState(stateBasecase);
-        cnec2.setThreshold(threshold2);
+        SimpleCnec cnec1basecase = new SimpleCnec("cnec1basecase", "", monitoredElement1, null, stateBasecase);
+        SimpleCnec cnec1stateCurativeContingency1 = new SimpleCnec("cnec1stateCurativeContingency1", "", monitoredElement1, null, stateCurativeContingency1);
+        SimpleCnec cnec1stateCurativeContingency2 = new SimpleCnec("cnec1stateCurativeContingency2", "", monitoredElement1, null, stateCurativeContingency2);
+        cnec1basecase.setThreshold(thresholdAbsFlow);
+        cnec1stateCurativeContingency1.setThreshold(thresholdAbsFlow);
+        cnec1stateCurativeContingency2.setThreshold(thresholdAbsFlow);
 
-        // Usage rules
-        FreeToUse freeToUse = new FreeToUse(null, null);
-        freeToUse.setUsageMethod(UsageMethod.AVAILABLE);
-        freeToUse.setState(stateBasecase);
-        OnContingency onContingency = new OnContingency(UsageMethod.FORCED, stateCurative, null);
-        onContingency.setContingency(contingency);
-        OnConstraint onConstraint = new OnConstraint(UsageMethod.FORCED, stateCurative, null);
-        onConstraint.setCnec(cnec1);
+        SimpleCnec cnec2basecase = new SimpleCnec("cnec2basecase", "", monitoredElement2, null, stateBasecase);
+        SimpleCnec cnec2stateCurativeContingency1 = new SimpleCnec("cnec2stateCurativeContingency1", "", monitoredElement2, null, stateCurativeContingency1);
+        SimpleCnec cnec2stateCurativeContingency2 = new SimpleCnec("cnec2stateCurativeContingency2", "", monitoredElement2, null, stateCurativeContingency2);
+        cnec2basecase.setThreshold(thresholdRelativeFlow);
+        cnec2stateCurativeContingency1.setThreshold(thresholdRelativeFlow);
+        cnec2stateCurativeContingency2.setThreshold(thresholdRelativeFlow);
 
-        // NetworkAction
-        ComplexNetworkAction networkAction1 = new ComplexNetworkAction("id1", "name1", "operator1", new ArrayList<>(Arrays.asList(freeToUse)), new ArrayList<>(Arrays.asList(hvdcSetpoint)));
-        networkAction1.addApplicableNetworkAction(topology2);
-        ComplexNetworkAction networkAction2 = new ComplexNetworkAction("id2", "name2", "operator1", new ArrayList<>(Arrays.asList(freeToUse)), new ArrayList<>(Arrays.asList(pstSetpoint)));
-
-        // RangeAction
-        ComplexRangeAction rangeAction1 = new ComplexRangeAction("idRangeAction", "myRangeAction", "operator1", null, null, null);
-        List<Range> ranges = new ArrayList<>(Arrays.asList(absoluteFixedRange, relativeDynamicRange));
-        rangeAction1.setRanges(ranges);
-        rangeAction1.addRange(relativeFixedRange);
-        List<ApplicableRangeAction> elementaryRangeActions = new ArrayList<>(Arrays.asList(pstRange1));
-        rangeAction1.setApplicableRangeActions(elementaryRangeActions);
-        rangeAction1.addApplicableRangeAction(hvdcRange1);
-        List<UsageRule> usageRules =  new ArrayList<>(Arrays.asList(freeToUse, onConstraint));
-        rangeAction1.setUsageRules(usageRules);
-        rangeAction1.addUsageRule(onContingency);
-
-        ComplexRangeAction rangeAction2 = new ComplexRangeAction("idRangeAction2", "myRangeAction2", "operator1", usageRules, ranges, new ArrayList<>(Arrays.asList(pstRange1)));
-
-        Crac crac = new SimpleCrac("idCrac", "name");
-
-        crac.addCnec(cnec1);
-        crac.addCnec(cnec2);
-        crac.addNetworkAction(networkAction1);
-        crac.addNetworkAction(networkAction2);
-
-        crac.addRangeAction(rangeAction2);
-
-        ComplexContingency contingency1 = new ComplexContingency("idContingency1", "My contingency 1",
-                new HashSet<>(Arrays.asList(new NetworkElement("BBE2AA1  BBE3AA1  1"))));
-        crac.addContingency(contingency1);
+        crac.addCnec(cnec1basecase);
+        crac.addCnec(cnec1stateCurativeContingency1);
+        crac.addCnec(cnec1stateCurativeContingency2);
+        crac.addCnec(cnec2basecase);
+        crac.addCnec(cnec2stateCurativeContingency1);
+        crac.addCnec(cnec2stateCurativeContingency2);
 
         return crac;
     }
