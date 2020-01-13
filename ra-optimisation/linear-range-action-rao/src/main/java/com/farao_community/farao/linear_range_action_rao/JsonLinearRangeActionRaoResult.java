efea1b74f9cd96d8c3012676a2927d86b4f738da
ruidian <47722616+pengbo-ruidian@users.noreply.github.com>
2020-01-13 15:38:10 +0100

    Update json linear range rao result import/export (#117)
    * far 158 init
    
    * test
    
    * log
    
    * review modif
    
    * review modif cont.
    
    * remove comments
    
    * modif after refacto
    
    * correction on pom
    
    * rename
    
    * temp solution for security status in linear range
    
    * sonar
    
    * modif after review
    
    * fix double loop without filter
    
    * review modif
    
    * pom order
    
    * Revert "pom order"
    
    This reverts commit 2f58ce91fb3e3f4973b8c5fbf45aed2ae4d07ccf.
    
    * pom order
    
    * fall back for not calculated cnec ref flow
    
    * update fallback
    
    * add filter for contingency state
    
    * update testcase uct
    
    * update us test case
    
    * edge condition for sensi: no sensi result
    
    * add loadflow in system analysis
    
    * use reference flow from loadflow
    
    * warning for maxthreshold
    
    * how to get margin?
    
    * add crac synchonize with network
    
    * change get max threshold for relative margin
    
    * disable test: incorrect default hades config
    
    * sonar test update
    
    * sonar update
    
    * checkstyle
    
    * remove test
    
    * log
    
    * solve conflit
    
    * solve conflit
    
    * sonar test
    
    * sonar test
    
    * remove comment
    
    * review update
    
    * review modif
    
    * review modif
    
    * review modif
    
    * review modif
    
    * ? test json linear rao param ?
    
    * add min margin in rao result extension
    
    * json linear rao result
    
    * update test .json
    
    * checkstyle
    
    * checkstyle
    
    * fix test config
    
    * fix test config
    
    * remove commented information in test file
    
    * order linear rao pom.xml dependency
    
    * order linear rao pom.xml dependency
    
    * review modif
    
    * review modif
    
    Co-authored-by: TheMaskedTurtle <53527338+TheMaskedTurtle@users.noreply.github.com>
    Co-authored-by:  Baptiste Seguinot <baptiste.seguinot@rte-france.com>

--- ra-optimisation/linear-range-action-rao/src/main/java/com/farao_community/farao/linear_range_action_rao/JsonLinearRangeActionRaoResult.java
+++ ra-optimisation/linear-range-action-rao/src/main/java/com/farao_community/farao/linear_range_action_rao/JsonLinearRangeActionRaoResult.java
@@ -6,6 +6,7 @@
  */
 package com.farao_community.farao.linear_range_action_rao;
 
+import com.farao_community.farao.commons.FaraoException;
 import com.farao_community.farao.ra_optimisation.json.JsonRaoComputationResult;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
@@ -24,15 +25,31 @@ public class JsonLinearRangeActionRaoResult implements  JsonRaoComputationResult
     @Override
     public void serialize(LinearRangeActionRaoResult linearRangeActionRaoResult, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         jsonGenerator.writeStartObject();
-        // no specific results in LinearRangeActionRaoResult yet
+        jsonGenerator.writeObjectField("security-status", linearRangeActionRaoResult.getSecurityStatus());
+        jsonGenerator.writeObjectField("minimum-margin", linearRangeActionRaoResult.getMinMargin());
         jsonGenerator.writeEndObject();
 
     }
 
     @Override
     public LinearRangeActionRaoResult deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-        // no specific results in LinearRangeActionRaoResult yet
-        return new LinearRangeActionRaoResult();
+        LinearRangeActionRaoResult ret = new LinearRangeActionRaoResult();
+
+        while (!jsonParser.nextToken().isStructEnd()) {
+            switch (jsonParser.getCurrentName()) {
+                case "security-status":
+                    jsonParser.nextToken();
+                    ret.setSecurityStatus(Enum.valueOf(LinearRangeActionRaoResult.SecurityStatus.class, jsonParser.getValueAsString()));
+                    break;
+                case "minimum-margin":
+                    jsonParser.nextToken();
+                    ret.setMinMargin(jsonParser.getDoubleValue());
+                    break;
+                default:
+                    throw new FaraoException("Unexpected field: " + jsonParser.getCurrentName());
+            }
+        }
+        return ret;
     }
 
     @Override
--- ra-optimisation/linear-range-action-rao/src/test/java/com/farao_community/farao/linear_range_action_rao/JsonLinearRangeActionRaoResultTest.java
+++ ra-optimisation/linear-range-action-rao/src/test/java/com/farao_community/farao/linear_range_action_rao/JsonLinearRangeActionRaoResultTest.java
@@ -10,8 +10,12 @@ import com.farao_community.farao.ra_optimisation.RaoComputationResult;
 import com.farao_community.farao.ra_optimisation.json.JsonRaoComputationResult;
 import com.powsybl.commons.AbstractConverterTest;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import static org.junit.Assert.*;
@@ -20,6 +24,8 @@ import static org.junit.Assert.*;
  * @author Baptiste Seguinot {@literal <baptiste.seguinot at rte-france.com>}
  */
 public class JsonLinearRangeActionRaoResultTest extends AbstractConverterTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonLinearRangeActionRaoResultTest.class);
 
     @Test
     public void roundTripTest() {
@@ -33,5 +39,25 @@ public class JsonLinearRangeActionRaoResultTest extends AbstractConverterTest {
 
         //compare
         compareTxt(getClass().getResourceAsStream("/LinearRangeActionRaoResults.json"), baos.toString());
+    }
+
+    @Test
+    public void anotherRoundTest() {
+        RaoComputationResult outputResult = new RaoComputationResult(RaoComputationResult.Status.SUCCESS, null, null);
+        LinearRangeActionRaoResult outputResultExtension = new LinearRangeActionRaoResult(LinearRangeActionRaoResult.SecurityStatus.UNSECURED);
+        double minMargin = -1;
+        outputResultExtension.setMinMargin(minMargin);
+        outputResult.addExtension(LinearRangeActionRaoResult.class, outputResultExtension);
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        JsonRaoComputationResult.write(outputResult, outputStream);
+        assertTrue(outputStream.toString().contains("minimum-margin"));
+
+        InputStream inputStream = new ByteArrayInputStream(outputStream.toString().getBytes());
+        RaoComputationResult inputResult = JsonRaoComputationResult.read(inputStream);
+        LinearRangeActionRaoResult inputExtension = inputResult.getExtension(LinearRangeActionRaoResult.class);
+        assertEquals(RaoComputationResult.Status.SUCCESS, inputResult.getStatus());
+        assertEquals(LinearRangeActionRaoResult.SecurityStatus.UNSECURED, inputExtension.getSecurityStatus());
+        assertEquals(minMargin, inputExtension.getMinMargin(), 1E-10);
     }
 }
