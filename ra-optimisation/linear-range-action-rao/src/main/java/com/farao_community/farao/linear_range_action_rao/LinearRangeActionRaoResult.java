e1d0b3f53fa401fcbd116b9f2d5a325ed0427977
ruidian <47722616+pengbo-ruidian@users.noreply.github.com>
2020-01-13 14:38:21 +0100

    Fill the export format with CNECs (#105)

--- ra-optimisation/linear-range-action-rao/src/main/java/com/farao_community/farao/linear_range_action_rao/LinearRangeActionRaoResult.java
+++ ra-optimisation/linear-range-action-rao/src/main/java/com/farao_community/farao/linear_range_action_rao/LinearRangeActionRaoResult.java
@@ -9,14 +9,61 @@ package com.farao_community.farao.linear_range_action_rao;
 
 import com.farao_community.farao.ra_optimisation.RaoComputationResult;
 import com.powsybl.commons.extensions.AbstractExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
  */
 public class LinearRangeActionRaoResult extends AbstractExtension<RaoComputationResult> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LinearRangeActionRaoResult.class);
+
+    public enum SecurityStatus {
+        SECURED,
+        UNSECURED
+    }
+
+    private SecurityStatus securityStatus;
+
+    private double minMargin;
 
     @Override
     public String getName() {
         return "LinearRangeActionRaoResult";
     }
+
+    public LinearRangeActionRaoResult() {
+        this.securityStatus = SecurityStatus.SECURED;
+        this.minMargin = Double.MAX_VALUE;
+    }
+
+    public LinearRangeActionRaoResult(SecurityStatus securityStatus) {
+        this.securityStatus = securityStatus;
+        this.minMargin = Double.MAX_VALUE;
+    }
+
+    public SecurityStatus getSecurityStatus() {
+        return securityStatus;
+    }
+
+    public void setSecurityStatus(SecurityStatus securityStatus) {
+        this.securityStatus = securityStatus;
+    }
+
+    public void updateResult(double margin) {
+        if (margin < this.minMargin) {
+            this.minMargin = margin;
+            if (minMargin < 0.0) {
+                this.setSecurityStatus(SecurityStatus.UNSECURED);
+            }
+            LOGGER.info("Updated LinearRangeActionRaoResult to: mininum margin = {}, security status: {}",
+                    this.minMargin,
+                    this.securityStatus);
+        }
+    }
+
+    public double getMinMargin() {
+        return minMargin;
+    }
+
 }
