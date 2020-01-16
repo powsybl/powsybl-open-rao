0526d80df29257fd7ada94ba8e3aadae5f7a0e68
pengbo-ruidian <wangpengbo.ruidian@gmail.com>
2020-01-16 23:55:18 +0100

    skeleton init

---
+++ ra-optimisation/linear-rao/src/main/java/com/farao_community/farao/linear_rao/AbstractProblemFiller.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.linear_rao;
+
+/**
+ * @author Pengbo Wang {@literal <pengbo.wang at rte-international.com>}
+ */
+public abstract class AbstractProblemFiller {
+    private LinearRaoProblem linearRaoProblem;
+    private LinearRaoData linearRaoData;
+
+    public void fill(LinearRaoProblem linearRaoProblem, LinearRaoData linearRaoData) {
+        //todo
+    }
+
+    public void update(LinearRaoProblem linearRaoProblem, LinearRaoData linearRaoData) {
+        //todo
+    }
+}
