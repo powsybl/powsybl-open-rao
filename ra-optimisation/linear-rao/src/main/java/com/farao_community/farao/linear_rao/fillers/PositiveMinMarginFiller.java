5f6409169d6e9843510597118dc7978c642e617b
Viktor Terrier <viktor.terrier@rte-france.com>
2020-01-21 09:43:26 +0100

    Creation of the objective function

--- ra-optimisation/linear-rao/src/main/java/com/farao_community/farao/linear_rao/fillers/PositiveMinMarginFiller.java
+++ ra-optimisation/linear-rao/src/main/java/com/farao_community/farao/linear_rao/fillers/PositiveMinMarginFiller.java
@@ -14,11 +14,15 @@ import com.farao_community.farao.linear_rao.AbstractProblemFiller;
 import com.farao_community.farao.linear_rao.LinearRaoData;
 import com.farao_community.farao.linear_rao.LinearRaoProblem;
 import com.powsybl.iidm.network.Network;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
  */
 public class PositiveMinMarginFiller extends AbstractProblemFiller {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PositiveMinMarginFiller.class);
+
     @Override
     public void fill(LinearRaoProblem linearRaoProblem, LinearRaoData linearRaoData) {
         this.linearRaoData = linearRaoData;
@@ -35,21 +39,21 @@ public class PositiveMinMarginFiller extends AbstractProblemFiller {
         crac.getCnecs().forEach(this::fillConstraintsCnec);
 
         // Define the objective function
-        //TODO
+        fillObjective();
 
         linearRaoData.getCrac().desynchronize();
     }
 
-    private void fillConstraints() {
-
-    }
-
     private void fillConstraintsCnec(Cnec cnec) {
         try {
-            this.linearRaoProblem.addMinPosMargin(cnec.getId(), cnec.getThreshold().getMinThreshold().orElse(-Double.MAX_VALUE), cnec.getThreshold().getMaxThreshold().orElse(Double.MAX_VALUE));
+            linearRaoProblem.addMinPosMargin(cnec.getId(), cnec.getThreshold().getMinThreshold().orElse(-Double.MAX_VALUE), cnec.getThreshold().getMaxThreshold().orElse(Double.MAX_VALUE));
         } catch (SynchronizationException e) {
             e.printStackTrace();
         }
+    }
+
+    private void fillObjective() {
+        linearRaoProblem.getMinPosObjective();
     }
 }
 
