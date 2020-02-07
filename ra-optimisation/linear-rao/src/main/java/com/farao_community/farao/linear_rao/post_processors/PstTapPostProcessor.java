3ecc7fe2230c50114c8f95104ca5b1bca14d8c87
Viktor Terrier <11834022+terriervik@users.noreply.github.com>
2020-02-07 10:45:31 +0100

    Linear optimisation: fill RA result post-processor (#139)
    * Fill RA result post-processor
    
    * export good values
    
    Signed-off-by: Baptiste Seguinot <baptiste.seguinot@rte-france.com>
    
    * import crac
    
    Signed-off-by: Baptiste Seguinot <baptiste.seguinot@rte-france.com>
    
    * correct sign and add tests
    
    Signed-off-by: Baptiste Seguinot <baptiste.seguinot@rte-france.com>
    
    * remove useless dependencies
    
    Signed-off-by: Baptiste Seguinot <baptiste.seguinot@rte-france.com>
    
    * remove useless test data
    
    Signed-off-by: Baptiste Seguinot <baptiste.seguinot@rte-france.com>
    
    * remove code duplication
    
    Signed-off-by: Baptiste Seguinot <baptiste.seguinot@rte-france.com>
    
    * correction after review
    
    Signed-off-by: Baptiste Seguinot <baptiste.seguinot@rte-france.com>
    
    Co-authored-by:  Baptiste Seguinot <baptiste.seguinot@rte-france.com>
    Co-authored-by: phiedw <philippe.edwards@telecom-paristech.fr>

--- ra-optimisation/linear-rao/src/main/java/com/farao_community/farao/linear_rao/post_processors/PstTapPostProcessor.java
+++ ra-optimisation/linear-rao/src/main/java/com/farao_community/farao/linear_rao/post_processors/PstTapPostProcessor.java
@@ -8,9 +8,17 @@
 package com.farao_community.farao.linear_rao.post_processors;
 
 import com.farao_community.farao.linear_rao.AbstractPostProcessor;
+import com.farao_community.farao.linear_rao.LinearRaoData;
+import com.farao_community.farao.linear_rao.LinearRaoProblem;
+import com.farao_community.farao.ra_optimisation.RaoComputationResult;
 
 /**
  * @author Pengbo Wang {@literal <pengbo.wang at rte-international.com>}
  */
 public class PstTapPostProcessor extends AbstractPostProcessor {
+
+    @Override
+    public void process(LinearRaoProblem linearRaoProblem, LinearRaoData linearRaoData, RaoComputationResult raoComputationResult) {
+    }
+
 }
