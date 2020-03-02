9c43613cf000446866fd8ea0db3a844755272460
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2020-03-02 11:39:25 +0100

    add extensions for CnecResult
    Signed-off-by: Baptiste Seguinot <baptiste.seguinot@rte-france.com>

---
+++ data/crac/crac-result-extensions/src/main/java/com/farao_community/farao/data/crac_result_extensions/CnecResult.java
@@ -0,0 +1,8 @@
+package com.farao_community.farao.data.crac_result_extensions;
+
+import com.farao_community.farao.data.crac_api.Cnec;
+import com.powsybl.commons.extensions.Extension;
+
+public interface CnecResult extends Extension<Cnec>
+{
+}
---
+++ data/crac/crac-result-extensions/src/main/java/com/farao_community/farao/data/crac_result_extensions/CnecResultImpl.java
@@ -0,0 +1,4 @@
+package com.farao_community.farao.data.crac_result_extensions;
+
+public class CnecResultImpl {
+}
