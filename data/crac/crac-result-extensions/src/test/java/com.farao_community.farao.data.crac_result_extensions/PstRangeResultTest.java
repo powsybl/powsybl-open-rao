d311dd04e97fc2cf64990c69d5a932e4a84283c0
Joris Mancini <joris.mancini@rte-france.com>
2020-03-05 15:32:11 +0100

    add tests

--- data/crac/crac-result-extensions/src/test/java/com.farao_community.farao.data.crac_result_extensions/PstRangeResultTest.java
+++ data/crac/crac-result-extensions/src/test/java/com.farao_community.farao.data.crac_result_extensions/PstRangeResultTest.java
@@ -5,6 +5,7 @@ import com.farao_community.farao.data.crac_impl.SimpleState;
 import com.farao_community.farao.data.crac_impl.remedial_action.range_action.PstWithRange;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.Collections;
 import java.util.Optional;
@@ -15,31 +16,33 @@ import static org.junit.Assert.*;
  * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
  */
 public class PstRangeResultTest {
+    private static final double EPSILON = 0.1;
+    private PstRange pstRange;
+    private State state;
+    private PstRangeResult pstRangeResult;
 
     @Before
-    public void setUp() throws Exception {
-    }
-
-    @Test
-    public void getTap() {
-    }
-
-    @Test
-    public void setTap() {
+    public void setUp() {
+        pstRange = new PstWithRange("id", new NetworkElement("ne"));
+        state = new SimpleState(Optional.empty(), new Instant("initial", 0));
+        pstRangeResult = new PstRangeResult(Collections.singleton(state));
     }
 
     @Test
     public void setSetPoint() {
+        PstRange pstRangeMock = Mockito.mock(PstRange.class);
+        Mockito.when(pstRangeMock.computeTapPosition(3.2)).thenReturn(5);
+        Mockito.when(pstRangeMock.isSynchronized()).thenReturn(true);
+        pstRangeResult.setExtendable(pstRangeMock);
+        pstRangeResult.setSetPoint(state, 3.2);
+        assertEquals(3.2, pstRangeResult.getSetPoint(state), EPSILON);
+        assertEquals(5, pstRangeResult.getTap(state));
     }
 
     @Test
     public void addExtension() {
-        PstRange pstRange = new PstWithRange("pst", new NetworkElement("ne"));
-        State state = new SimpleState(Optional.empty(), new Instant("initial", 0));
-        PstRangeResult pstRangeResult = new PstRangeResult(Collections.singleton(state));
         pstRange.addExtension(PstRangeResult.class, pstRangeResult);
-
-        pstRange.getExtension(PstRangeResult.class).setTap(state, 15);
+        pstRangeResult.setTap(state, 15);
         assertEquals(15, pstRange.getExtension(PstRangeResult.class).getTap(state));
     }
 }
