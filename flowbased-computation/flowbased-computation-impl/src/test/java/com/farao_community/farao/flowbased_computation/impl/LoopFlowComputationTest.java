cc7089bde8b4d793ccb0c82748e9a99c608a3a99
pengbo-ruidian <wangpengbo.ruidian@gmail.com>
2020-03-16 14:59:23 +0100

    test cont.

--- flowbased-computation/flowbased-computation-impl/src/test/java/com/farao_community/farao/flowbased_computation/impl/LoopFlowComputationTest.java
+++ flowbased-computation/flowbased-computation-impl/src/test/java/com/farao_community/farao/flowbased_computation/impl/LoopFlowComputationTest.java
@@ -8,7 +8,6 @@ package com.farao_community.farao.flowbased_computation.impl;
 
 import com.farao_community.farao.data.crac_api.Cnec;
 import com.farao_community.farao.data.crac_api.Crac;
-import com.farao_community.farao.flowbased_computation.FlowBasedComputationParameters;
 import com.farao_community.farao.flowbased_computation.glsk_provider.GlskProvider;
 import com.farao_community.farao.util.LoadFlowService;
 import com.farao_community.farao.util.SensitivityComputationService;
@@ -40,8 +39,10 @@ public class LoopFlowComputationTest {
     private Crac crac;
     private GlskProvider glskProvider;
     private ComputationManager computationManager;
-    private FlowBasedComputationParameters parameters;
     private List<String> countries;
+    private Map<String, Double> referenceNetPositionByCountry;
+    private Map<Cnec, Map<String, Double>> ptdfs;
+    private Map<Cnec, Double> frefResults;
 
     @Before
     public void setUp() {
@@ -52,7 +53,6 @@ public class LoopFlowComputationTest {
         crac = ExampleGenerator.crac();
         glskProvider = ExampleGenerator.glskProvider();
         computationManager = LocalComputationManager.getDefault();
-        parameters = FlowBasedComputationParameters.load(platformConfig);
         SensitivityComputationFactory sensitivityComputationFactory = ExampleGenerator.sensitivityComputationFactory();
         SensitivityComputationService.init(sensitivityComputationFactory, computationManager);
 
@@ -60,13 +60,31 @@ public class LoopFlowComputationTest {
         Mockito.when(loadFlowRunner.run(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(new LoadFlowResultImpl(true, Collections.emptyMap(), ""));
         LoadFlowService.init(loadFlowRunner, computationManager);
         countries = Arrays.asList("FR", "BE", "DE", "NL");
+
+        ptdfs = new HashMap<>();
+        frefResults = new HashMap<>();
+        frefResults.put(crac.getCnec("FR-BE"), 50.0);
+        frefResults.put(crac.getCnec("FR-DE"), 50.0);
+        frefResults.put(crac.getCnec("BE-NL"), 50.0);
+        frefResults.put(crac.getCnec("DE-NL"), 50.0);
+
+        referenceNetPositionByCountry = new HashMap<>();
+        referenceNetPositionByCountry.put("FR", 100.0);
+        referenceNetPositionByCountry.put("BE", 0.0);
+        referenceNetPositionByCountry.put("DE", 0.0);
+        referenceNetPositionByCountry.put("NL", -100.0);
     }
 
     @Test
     public void testPtdf() {
         LoopFlowComputation loopFlowComputation = new LoopFlowComputation(network, crac, glskProvider, countries);
-        assertTrue(Objects.nonNull(loopFlowComputation));
-        Map<Cnec, Map<String, Double>> ptdfs = loopFlowComputation.computePtdfOnCurrentNetwork();
-        assertEquals(0.375, ptdfs.get(crac.getCnec("FR-BE")).get("FR"), 0.1);
+        ptdfs = loopFlowComputation.computePtdfOnCurrentNetwork();
+        assertEquals(0.375, ptdfs.get(crac.getCnec("FR-BE")).get("FR"), EPSILON);
+
+        Map<String, Double> fzeroNpResults = loopFlowComputation.buildLoopFlowsFromResult(frefResults, ptdfs, referenceNetPositionByCountry);
+        assertEquals(0.0, fzeroNpResults.get("FR-DE"), EPSILON);
+        assertEquals(0.0, fzeroNpResults.get("FR-BE"), EPSILON);
+        assertEquals(0.0, fzeroNpResults.get("DE-NL"), EPSILON);
+        assertEquals(0.0, fzeroNpResults.get("BE-NL"), EPSILON);
     }
 }
