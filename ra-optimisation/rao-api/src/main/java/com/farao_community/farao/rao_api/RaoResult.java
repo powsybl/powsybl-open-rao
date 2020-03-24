b26a1fdc928009d98504c7c0a03f0125304ea64b
phiedw <philippe.edwards@telecom-paristech.fr>
2020-03-24 11:35:21 +0100

    rao result import export (#203)
    * added RaoResult class
    
    * added writer reader
    
    Co-authored-by: EDWARDS Philippe <philippe.edwards@rte-france.com>

---
+++ ra-optimisation/rao-api/src/main/java/com/farao_community/farao/rao_api/RaoResult.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.rao_api;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * RAO result API. This class will contain information about the RAO computation (computation status, logs, etc).
+ *
+ * @author Philippe Edwards {@literal <philippe.edwards at rte-france.com>}
+ */
+
+@JsonIgnoreProperties(value = { "successful" })
+public class RaoResult {
+
+    public enum Status {
+        FAILURE,
+        SUCCESS
+    }
+
+    private Status status;
+
+    private String preOptimVariantId;
+
+    private String postOptimVariantId;
+
+    @JsonCreator
+    public RaoResult(@JsonProperty("status") Status status) {
+        this.status = status;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public boolean isSuccessful() {
+        return status == Status.SUCCESS;
+    }
+
+    public void setPreOptimVariantId(String preOptimVariantId) {
+        this.preOptimVariantId = preOptimVariantId;
+    }
+
+    public String getPreOptimVariantId() {
+        return preOptimVariantId;
+    }
+
+    public void setPostOptimVariantId(String postOptimVariantId) {
+        this.postOptimVariantId = postOptimVariantId;
+    }
+
+    public String getPostOptimVariantId() {
+        return postOptimVariantId;
+    }
+}
