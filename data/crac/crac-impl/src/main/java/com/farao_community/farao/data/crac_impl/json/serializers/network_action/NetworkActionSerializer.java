647558f60e1817bbd54a959d846eb73dc39e276d
EDWARDS Philippe <philippe.edwards@rte-france.com>
2020-03-30 13:42:46 +0200

    fixed RA extensions export

--- data/crac/crac-impl/src/main/java/com/farao_community/farao/data/crac_impl/json/serializers/network_action/NetworkActionSerializer.java
+++ data/crac/crac-impl/src/main/java/com/farao_community/farao/data/crac_impl/json/serializers/network_action/NetworkActionSerializer.java
@@ -1,12 +1,10 @@
 package com.farao_community.farao.data.crac_impl.json.serializers.network_action;
 
-import com.farao_community.farao.data.crac_api.ExtensionsHandler;
 import com.farao_community.farao.data.crac_api.NetworkAction;
 import com.farao_community.farao.data.crac_impl.json.serializers.AbstractRemedialActionSerializer;
 import com.farao_community.farao.data.crac_impl.remedial_action.network_action.AbstractNetworkAction;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.powsybl.commons.json.JsonUtil;
 
 import java.io.IOException;
 
@@ -18,6 +16,5 @@ public class NetworkActionSerializer<E extends AbstractNetworkAction> extends Ab
     @Override
     public void serialize(E networkAction, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         super.serialize(networkAction, jsonGenerator, serializerProvider);
-        JsonUtil.writeExtensions(networkAction, jsonGenerator, serializerProvider, ExtensionsHandler.getExtensionsSerializers());
     }
 }
