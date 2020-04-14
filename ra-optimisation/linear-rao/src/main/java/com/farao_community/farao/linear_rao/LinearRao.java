e63694619ed69012d0f2c69bba014df838d1adee
EDWARDS Philippe <philippe.edwards@rte-france.com>
2020-04-14 11:13:30 +0200

    put jniortools back

--- ra-optimisation/linear-rao/src/main/java/com/farao_community/farao/linear_rao/LinearRao.java
+++ ra-optimisation/linear-rao/src/main/java/com/farao_community/farao/linear_rao/LinearRao.java
@@ -15,6 +15,7 @@ import com.farao_community.farao.linear_rao.config.LinearRaoParameters;
 import com.farao_community.farao.linear_rao.optimisation.LinearOptimisationException;
 import com.farao_community.farao.rao_api.RaoParameters;
 import com.farao_community.farao.rao_api.RaoProvider;
+import com.farao_community.farao.util.NativeLibraryLoader;
 import com.farao_community.farao.rao_api.RaoResult;
 import com.farao_community.farao.util.SensitivityComputationException;
 import com.google.auto.service.AutoService;
@@ -31,6 +32,10 @@ import java.util.concurrent.CompletableFuture;
  */
 @AutoService(RaoProvider.class)
 public class LinearRao implements RaoProvider {
+
+    static {
+        NativeLibraryLoader.loadNativeLibrary("jniortools");
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LinearRao.class);
 
