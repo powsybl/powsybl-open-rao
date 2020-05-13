5d6647753ea3570064b7b564ed41e6dd87b7a429
Viktor Terrier <viktor.terrier@rte-france.com>
2020-05-13 15:30:24 +0200

    Fill header

--- data/crac-io/crac-io-cne/src/main/java/com/farao_community/farao/data/crac_io_cne/CneFiller.java
+++ data/crac-io/crac-io-cne/src/main/java/com/farao_community/farao/data/crac_io_cne/CneFiller.java
@@ -7,10 +7,83 @@
 
 package com.farao_community.farao.data.crac_io_cne;
 
+import com.farao_community.farao.commons.FaraoException;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.util.GregorianCalendar;
+import java.util.Random;
+
 /**
  * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
  */
-public class CneFiller {
+public final class CneFiller {
 
-    CriticalNetworkElementMarketDocument coucou;
+    private static CriticalNetworkElementMarketDocument cne = new CriticalNetworkElementMarketDocument();
+
+    private CneFiller() { }
+
+    public static CriticalNetworkElementMarketDocument getCne() {
+        return cne;
+    }
+
+    public static void generate() {
+        fillHeader();
+    }
+
+    private static void fillHeader() {
+        cne.setMRID(generateRandomMRID());
+        cne.setRevisionNumber("1");
+        cne.setType("B06");
+        cne.setProcessProcessType("A43");
+        cne.setSenderMarketParticipantMRID(createPartyIDString("A01", "22XCORESO------S"));
+        cne.setSenderMarketParticipantMarketRoleType("A44");
+        cne.setReceiverMarketParticipantMRID(createPartyIDString("A01", "17XTSO-CS------W"));
+        cne.setReceiverMarketParticipantMarketRoleType("A36");
+        cne.setCreatedDateTime(createXMLGregorianCalendarNow());
+        cne.setTimePeriodTimeInterval(createEsmpDateTimeInterval());
+        cne.setDomainMRID(createAreaIDString("A01", "10YDOM-REGION-1V"));
+    }
+
+    // TODO: get real date
+    private static ESMPDateTimeInterval createEsmpDateTimeInterval() {
+        ESMPDateTimeInterval timeInterval = new ESMPDateTimeInterval();
+        timeInterval.setStart(createXMLGregorianCalendarNow().toString());
+        timeInterval.setEnd(createXMLGregorianCalendarNow().toString());
+        return timeInterval;
+    }
+
+    private static XMLGregorianCalendar createXMLGregorianCalendarNow() {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        try {
+            DatatypeFactory datatypeFactory = DatatypeFactory.newInstance();
+            return datatypeFactory.newXMLGregorianCalendar(gregorianCalendar);
+        } catch (DatatypeConfigurationException e) {
+            throw new FaraoException();
+        }
+
+    }
+
+    private static AreaIDString createAreaIDString(String codingScheme, String value) {
+        AreaIDString domainMRID = new AreaIDString();
+        domainMRID.setCodingScheme(codingScheme);
+        domainMRID.setValue(value);
+        return domainMRID;
+    }
+
+    private static PartyIDString createPartyIDString(String codingScheme, String value) {
+        PartyIDString marketParticipantMRID = new PartyIDString();
+        marketParticipantMRID.setCodingScheme(codingScheme);
+        marketParticipantMRID.setValue(value);
+        return marketParticipantMRID;
+    }
+
+    /*
+    *  Generates a random code with 35 characters
+    * */
+    private static String generateRandomMRID() {
+        Random random = new Random(System.nanoTime());
+        return String.format("%s-%s-%s-%s", Integer.toHexString(random.nextInt()), Integer.toHexString(random.nextInt()), Integer.toHexString(random.nextInt()), Integer.toHexString(random.nextInt()));
+    }
 }
---
+++ data/crac-io/crac-io-cne/src/test/java/com/farao_community/farao/data/crac_io_cne/CneFillerTest.java
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (c) 2020, RTE (http://www.rte-france.com)
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.data.crac_io_cne;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
+ */
+public class CneFillerTest {
+
+    @Test
+    public void generateTest() {
+        CneFiller.generate();
+        CriticalNetworkElementMarketDocument cne = CneFiller.getCne();
+        int i = 1;
+    }
+}
