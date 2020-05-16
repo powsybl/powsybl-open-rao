992b87d17aee5c45bc0c0f1458f5b8c9fbc1d98d
pengbo-ruidian <wangpengbo.ruidian@gmail.com>
2020-05-16 08:44:58 +0200

    Revert "save variant and switch back to it after root eval"
    This reverts commit 1b6b4176165007d1a5ee8aec9e6ff9cc30a5a021.

--- ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/process/search_tree/Tree.java
+++ ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/process/search_tree/Tree.java
@@ -7,7 +7,6 @@
 package com.farao_community.farao.search_tree_rao.process.search_tree;
 
 import com.farao_community.farao.commons.FaraoException;
-import com.farao_community.farao.commons.RandomizedString;
 import com.farao_community.farao.data.crac_api.Crac;
 import com.farao_community.farao.data.crac_api.NetworkAction;
 import com.farao_community.farao.data.crac_api.UsageMethod;
@@ -18,7 +17,6 @@ import com.farao_community.farao.search_tree_rao.config.SearchTreeRaoParameters;
 import com.farao_community.farao.util.FaraoNetworkPool;
 import com.powsybl.iidm.network.Network;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -55,11 +53,7 @@ public final class Tree {
         SearchTreeRaoParameters searchTreeRaoParameters = parameters.getExtensionByName("SearchTreeRaoParameters");
 
         Leaf rootLeaf = new Leaf();
-        String initialNetworkVariant = network.getVariantManager().getWorkingVariantId();
-        String newNetworkVariant = RandomizedString.getRandomizedString(Collections.singleton(initialNetworkVariant));
-        network.getVariantManager().cloneVariant(initialNetworkVariant, newNetworkVariant);
-        rootLeaf.evaluate(network, crac, newNetworkVariant, parameters);
-        network.getVariantManager().setWorkingVariant(initialNetworkVariant);
+        rootLeaf.evaluate(network, crac, referenceNetworkVariant, parameters);
         int depth = 0;
 
         if (rootLeaf.getStatus() == Leaf.Status.EVALUATION_ERROR) {
