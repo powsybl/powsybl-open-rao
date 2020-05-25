5fc89bbaba898f46421745c65b159770577cbeba
pet-mit <47452063+pet-mit@users.noreply.github.com>
2020-05-25 15:03:05 +0200

    Robust Crac construction (#235)
    * Creation of CracFactory and CracFactoryService
    
    * Corrected some java doc. Added an abstract class. Removed StateAdder (use CnecAdder instead).
    
    * Added control on network element in crac when adding pst using adder. Added setOperator to PST adder and free-to-use default usage rule.
    
    Co-authored-by: MITRI Peter <mitripet@BUREAU.SI.INTERNE>
    Co-authored-by: phiedw <philippe.edwards@telecom-paristech.fr>
    Co-authored-by: Sébastien Murgey <sebastien.murgey@rte-france.com>

---
+++ data/crac/crac-api/src/test/java/com/farao_community/farao/data/crac_api/CracFactoryTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.data.crac_api;
+
+import com.farao_community.farao.commons.FaraoException;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Peter Mitri {@literal <peter.mitri at rte-france.com>}
+ */
+public class CracFactoryTest {
+
+    @Test
+    public void testGetFactory() {
+        CracFactory cf1 = CracFactory.find("MockCracFactory1");
+        assertNotNull(cf1);
+        assertEquals(cf1.getClass(), MockCracFactory1.class);
+        CracFactory cf2 = CracFactory.find("MockCracFactory2");
+        assertNotNull(cf2);
+        assertEquals(cf2.getClass(), MockCracFactory2.class);
+    }
+
+    @Test(expected = FaraoException.class)
+    public void mustThrowIfImplemNotFound() {
+        CracFactory.find("SimpleCracFactory");
+    }
+
+    @Test(expected = FaraoException.class)
+    public void mustThrowIfNameNullAndMultipleImplem() {
+        CracFactory.find(null);
+    }
+
+    @Test
+    public void testDefaultConfig() {
+        CracFactory cf = CracFactory.findDefault();
+        assertNotNull(cf);
+        assertEquals(cf.getClass(), MockCracFactory1.class);
+    }
+
+    // TO DO : test different default configs : empty (should throw),
+    // with MockCracFactory2, with wrong implem name (should throw)
+    // (this is not yet possible with PowSyBl's TestPlatformConfigProvider)
+}
