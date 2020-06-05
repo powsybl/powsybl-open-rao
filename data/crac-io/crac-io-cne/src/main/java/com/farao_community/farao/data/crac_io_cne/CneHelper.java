7abc6010c48d805bc5edb968d2c72fdca6a73f92
Viktor Terrier <11834022+terriervik@users.noreply.github.com>
2020-06-05 14:38:20 +0200

    Cne header (#265)
    * Cne header
    
    * Tests
    
    * Create ConstraintSeries with contingency
    
    * Cleaning tests
    
    * Start with monitoredSeries
    
    * Measurements for new objects
    
    * Keep just header
    
    * Checkstyle
    
    * Modif
    
    * Correction after review
    
    * Remove parameters of some methods (#268)
    
    Co-authored-by: alexandrem-rte <57182740+alexandrem-rte@users.noreply.github.com>

---
+++ data/crac-io/crac-io-cne/src/main/java/com/farao_community/farao/data/crac_io_cne/CneHelper.java
@@ -0,0 +1,113 @@
+/*
+ *  Copyright (c) 2020, RTE (http://www.rte-france.com)
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.data.crac_io_cne;
+
+import com.farao_community.farao.commons.FaraoException;
+import com.farao_community.farao.data.crac_api.Contingency;
+import com.farao_community.farao.data.crac_api.Crac;
+import com.farao_community.farao.data.crac_api.Instant;
+import com.farao_community.farao.data.crac_impl.ComplexContingency;
+import com.farao_community.farao.data.crac_result_extensions.CracResultExtension;
+import com.farao_community.farao.data.crac_result_extensions.ResultVariantManager;
+import com.powsybl.iidm.network.Network;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
+ */
+public class CneHelper {
+
+    private Crac crac;
+    private Network network;
+    private List<Instant> instants;
+    private String preOptimVariantId;
+    private String postOptimVariantId;
+    private Map<Contingency, ConstraintSeries> constraintSeriesMap;
+    private Contingency basecase;
+
+    public CneHelper(Crac crac, Network network) {
+
+        instants = new ArrayList<>();
+        preOptimVariantId = "";
+        postOptimVariantId = "";
+        constraintSeriesMap = new HashMap<>();
+        basecase = new ComplexContingency("BASECASE");
+
+        this.crac = crac;
+        this.network = network;
+        checkSynchronize();
+
+        if (crac.getExtension(CracResultExtension.class) == null || crac.getExtension(ResultVariantManager.class).getVariants() == null) { // Computation ended
+            throw new FaraoException("Computation failed: no output CNE document available.");
+        }
+    }
+
+    public Contingency getBasecase() {
+        return basecase;
+    }
+
+    public Network getNetwork() {
+        return network;
+    }
+
+    public Map<Contingency, ConstraintSeries> getConstraintSeriesMap() {
+        return constraintSeriesMap;
+    }
+
+    public void addToConstraintSeriesMap(Contingency contingency, ConstraintSeries constraintSeries) {
+        constraintSeriesMap.put(contingency, constraintSeries);
+    }
+
+    public List<Instant> getInstants() {
+        return instants;
+    }
+
+    public String getPreOptimVariantId() {
+        return preOptimVariantId;
+    }
+
+    public String getPostOptimVariantId() {
+        return postOptimVariantId;
+    }
+
+    public Crac getCrac() {
+        return crac;
+    }
+
+    public void initializeAttributes() {
+        CracResultExtension cracExtension = crac.getExtension(CracResultExtension.class);
+        List<String> variants = new ArrayList<>(crac.getExtension(ResultVariantManager.class).getVariants());
+
+        // sort the instants in order to determine which one is preventive, after outage, after auto RA and after CRA
+        instants = crac.getInstants().stream().sorted(Comparator.comparing(Instant::getSeconds)).collect(Collectors.toList());
+
+        // TODO: store the information on preOptim/postOptim Variant in the ResultVariantManager
+        preOptimVariantId = variants.get(0);
+        postOptimVariantId = variants.get(0);
+
+        double minCost = cracExtension.getVariant(variants.get(0)).getCost();
+        double maxCost = cracExtension.getVariant(variants.get(0)).getCost();
+        for (String variant : variants) {
+            if (cracExtension.getVariant(variant).getCost() < minCost) {
+                minCost = cracExtension.getVariant(variant).getCost();
+                postOptimVariantId = variant;
+            } else if (cracExtension.getVariant(variant).getCost() > maxCost) {
+                maxCost = cracExtension.getVariant(variant).getCost();
+                preOptimVariantId = variant;
+            }
+        }
+    }
+
+    public void checkSynchronize() {
+        if (!crac.isSynchronized()) {
+            crac.synchronize(network);
+        }
+    }
+}
