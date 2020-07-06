b6da36bbc06365251eaa64cd0f987f963c096e6a
Montigny <alexandre.montigny@rte-france.com>
2020-07-06 17:54:57 +0200

    The most limiting elements margins can be logged in the right unit

--- ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/SearchTreeRaoLogger.java
+++ ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/SearchTreeRaoLogger.java
@@ -7,9 +7,11 @@
 
 package com.farao_community.farao.search_tree_rao;
 
+import com.farao_community.farao.commons.PhysicalParameter;
 import com.farao_community.farao.commons.Unit;
 import com.farao_community.farao.data.crac_api.Cnec;
 import com.farao_community.farao.data.crac_api.Crac;
+import com.farao_community.farao.data.crac_api.RangeAction;
 import com.farao_community.farao.data.crac_result_extensions.CnecResult;
 import com.farao_community.farao.data.crac_result_extensions.CnecResultExtension;
 import com.farao_community.farao.data.crac_result_extensions.PstRangeResult;
@@ -18,7 +20,6 @@ import com.farao_community.farao.data.crac_result_extensions.RangeActionResultEx
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
@@ -40,33 +41,41 @@ final class SearchTreeRaoLogger {
             rangeActionMsg.append(prefix).append(" - ");
         }
         rangeActionMsg.append("Range action(s): ");
-        rangeActionMsg.append(leaf.getRaoData().getCrac().getRangeActions().stream()
-            .map(rangeAction -> format("%s: %d", rangeAction.getName(),
-                ((PstRangeResult) rangeAction.getExtension(RangeActionResultExtension.class)
+        for (RangeAction rangeAction : leaf.getRaoData().getCrac().getRangeActions()) {
+            String rangeActionName = rangeAction.getName();
+            int rangeActionTap = ((PstRangeResult) rangeAction.getExtension(RangeActionResultExtension.class)
                     .getVariant(leaf.getRaoData().getWorkingVariantId()))
-                    .getTap(leaf.getRaoData().getCrac().getPreventiveState().getId())))
-            .collect(Collectors.joining(", ")));
+                    .getTap(leaf.getRaoData().getCrac().getPreventiveState().getId());
+            rangeActionMsg
+                    .append(format("%s: %d", rangeActionName, rangeActionTap))
+                    .append(" , ");
+        }
         SearchTreeRao.LOGGER.info(rangeActionMsg.toString());
     }
 
     static void logMostLimitingElementsResults(Leaf leaf, Crac crac, Unit unit) {
         List<Cnec> sortedCnecs = new ArrayList<>(crac.getCnecs());
-        sortedCnecs.sort(Comparator.comparingDouble(cnec -> computeMarginInMW(cnec, leaf.getBestVariantId())));
+        sortedCnecs.sort(Comparator.comparingDouble(cnec -> computeCnecMargin(cnec, leaf.getBestVariantId(), unit)));
 
         for (int i = 0; i < Math.min(MAX_LOGS_LIMITING_ELEMENTS, sortedCnecs.size()); i++) {
             Cnec cnec = sortedCnecs.get(i);
+            String cnecNetworkElementName = cnec.getNetworkElement().getName();
+            String cnecStateId = cnec.getState().getId();
+            leaf.getRaoData().setWorkingVariant(leaf.getBestVariantId());
+            double margin = computeCnecMargin(cnec, leaf.getBestVariantId(), unit);
             SearchTreeRao.LOGGER.info(format("Limiting element #%d: element %s at state %s with a margin of %.2f %s",
                 i + 1,
-                cnec.getNetworkElement().getName(),
-                cnec.getState().getId(),
-                computeMarginInMW(cnec, leaf.getBestVariantId()),
+                cnecNetworkElementName,
+                cnecStateId,
+                margin,
                 unit));
         }
     }
 
-    private static double computeMarginInMW(Cnec cnec, String variantId) {
+    private static double computeCnecMargin(Cnec cnec, String variantId, Unit unit) {
         CnecResult cnecResult = cnec.getExtension(CnecResultExtension.class).getVariant(variantId);
-        return Math.min(cnecResult.getMaxThresholdInMW() - cnecResult.getFlowInMW(),
-            cnecResult.getFlowInMW() - cnecResult.getMinThresholdInMW());
+        unit.checkPhysicalParameter(PhysicalParameter.FLOW);
+        double actualValue = unit.equals(Unit.MEGAWATT) ? cnecResult.getFlowInMW() : cnecResult.getFlowInA();
+        return cnec.computeMargin(actualValue, unit);
     }
 }
