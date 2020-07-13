449712b239af0660acc9f0198d5ba46eaa429b6f
Montigny <alexandre.montigny@rte-france.com>
2020-07-13 12:40:25 +0200

    Improve code coverage

--- data/crac/crac-impl/src/test/java/com/farao_community/farao/data/crac_impl/threshold/RelativeFlowThresholdTest.java
+++ data/crac/crac-impl/src/test/java/com/farao_community/farao/data/crac_impl/threshold/RelativeFlowThresholdTest.java
@@ -12,30 +12,46 @@ import com.farao_community.farao.commons.Unit;
 import com.farao_community.farao.data.crac_api.Direction;
 import com.farao_community.farao.data.crac_api.Side;
 import com.farao_community.farao.data.crac_impl.NotSynchronizedException;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 /**
  * @author Alexandre Montigny {@literal <alexandre.montigny at rte-france.com>}
  */
 public class RelativeFlowThresholdTest {
 
-    private RelativeFlowThreshold relativeFlowThreshold;
-    private double branchLimit;
-    private double percentageOfMax;
+    private double branchLimit = 20;
+    private double percentageOfMax = 80;
 
-    @Before
-    public void setUp() {
-        branchLimit = 20;
-        percentageOfMax = 80;
-        relativeFlowThreshold = new RelativeFlowThreshold(Side.LEFT, Direction.DIRECT, percentageOfMax);
+    @Test
+    public void getMinMaxValueDirect() {
+        RelativeFlowThreshold relativeFlowThreshold = new RelativeFlowThreshold(Side.LEFT, Direction.DIRECT, percentageOfMax);
+        assertTrue(relativeFlowThreshold.getMaxValue().isPresent());
+        assertEquals(percentageOfMax, relativeFlowThreshold.getMaxValue().getAsDouble(), 0.1);
+        assertFalse(relativeFlowThreshold.getMinValue().isPresent());
+    }
+
+    @Test
+    public void getMinMaxValueOpposite() {
+        RelativeFlowThreshold relativeFlowThreshold = new RelativeFlowThreshold(Side.LEFT, Direction.OPPOSITE, percentageOfMax);
+        assertTrue(relativeFlowThreshold.getMinValue().isPresent());
+        assertEquals(-percentageOfMax, relativeFlowThreshold.getMinValue().getAsDouble(), 0.1);
+        assertFalse(relativeFlowThreshold.getMaxValue().isPresent());
+    }
+
+    @Test
+    public void getMinMaxValueBoth() {
+        RelativeFlowThreshold relativeFlowThreshold = new RelativeFlowThreshold(Side.LEFT, Direction.BOTH, percentageOfMax);
+        assertTrue(relativeFlowThreshold.getMaxValue().isPresent());
+        assertEquals(percentageOfMax, relativeFlowThreshold.getMaxValue().getAsDouble(), 0.1);
+        assertTrue(relativeFlowThreshold.getMinValue().isPresent());
+        assertEquals(-percentageOfMax, relativeFlowThreshold.getMinValue().getAsDouble(), 0.1);
     }
 
     @Test
     public void convertPercentToAmpere() {
+        RelativeFlowThreshold relativeFlowThreshold = new RelativeFlowThreshold(Side.LEFT, Direction.DIRECT, percentageOfMax);
         relativeFlowThreshold.isSynchronized = true;
         relativeFlowThreshold.setBranchLimit(branchLimit);
         double expectedValue = branchLimit * percentageOfMax / 100;
@@ -44,6 +60,7 @@ public class RelativeFlowThresholdTest {
 
     @Test
     public void convertPercentToMegawatt() {
+        RelativeFlowThreshold relativeFlowThreshold = new RelativeFlowThreshold(Side.LEFT, Direction.DIRECT, percentageOfMax);
         try {
             relativeFlowThreshold.convert(100, Unit.PERCENT_IMAX, Unit.MEGAWATT);
             fail();
@@ -53,7 +70,6 @@ public class RelativeFlowThresholdTest {
             relativeFlowThreshold.voltageLevel = 400;
             double expectedValue = branchLimit * percentageOfMax / 100 * relativeFlowThreshold.voltageLevel * Math.sqrt(3) / 1000;
             assertEquals(expectedValue, relativeFlowThreshold.convert(100, Unit.PERCENT_IMAX, Unit.MEGAWATT), 0.1);
-            // do nothing
         }
 
     }
