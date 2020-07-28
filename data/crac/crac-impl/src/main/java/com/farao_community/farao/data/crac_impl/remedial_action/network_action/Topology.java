46adf3574da9be6757047617417e65646b151088
Montigny <alexandre.montigny@rte-france.com>
2020-07-28 11:00:58 +0200

    implement switch topological actions

--- data/crac/crac-impl/src/main/java/com/farao_community/farao/data/crac_impl/remedial_action/network_action/Topology.java
+++ data/crac/crac-impl/src/main/java/com/farao_community/farao/data/crac_impl/remedial_action/network_action/Topology.java
@@ -18,6 +18,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.powsybl.iidm.network.Branch;
 import com.powsybl.iidm.network.Identifiable;
 import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.Switch;
 import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.List;
@@ -73,6 +74,13 @@ public final class Topology extends AbstractElementaryNetworkAction {
             } else {
                 branch.getTerminal1().connect();
                 branch.getTerminal2().connect();
+            }
+        } else if (element instanceof Switch) {
+            Switch aSwitch = (Switch) element;
+            if (actionType == ActionType.OPEN) {
+                aSwitch.setOpen(true);
+            } else {
+                aSwitch.setOpen(false);
             }
         } else {
             throw new NotImplementedException("Topological actions are only on branches for now");
