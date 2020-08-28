fad2f078c940f5204e8d59d89fd3f20bf35dc245
MITRI Peter <mitripet@BUREAU.SI.INTERNE>
2020-08-28 11:03:29 +0200

    added headers

--- loopflow-computation/src/main/java/com/farao_community/farao/loopflow_computation/CountryNetPositionComputation.java
+++ loopflow-computation/src/main/java/com/farao_community/farao/loopflow_computation/CountryNetPositionComputation.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.loopflow_computation;
 
 import com.powsybl.iidm.network.*;
@@ -6,6 +12,9 @@ import java.util.EnumMap;
 import java.util.Map;
 import java.util.Objects;
 
+/**
+ * @author Peter Mitri {@literal <peter.mitri at rte-france.com>}
+ */
 public class CountryNetPositionComputation {
 
     private Network network;
