17f9f4abab28dad288abfd58ce19f06ab41b4c8c
TheMaskedTurtle <53527338+TheMaskedTurtle@users.noreply.github.com>
2020-09-09 15:30:31 +0200

    Rao input API change (#323)
    * isolated api changes
    
    * fixing tests
    
    * isolated api changes
    
    * fixing tests
    
    * added optional ReferenceProgram to RaoInput
    
    * Put RaoInput in farao-rao-api and adapt code + add tests
    
    * remove relative margin related parameters
    
    * remove useless method in RAO API
    
    * Remove useless RAO API method
    
    Co-authored-by: EDWARDS Philippe <philippe.edwards@rte-france.com>
    Co-authored-by: MITRI Peter <mitripet@BUREAU.SI.INTERNE>
    Co-authored-by: pet-mit <47452063+pet-mit@users.noreply.github.com>

---
+++ ra-optimisation/rao-api/src/main/java/com/farao_community/farao/rao_api/RaoInput.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.rao_api;
+
+import com.farao_community.farao.data.crac_api.Crac;
+import com.farao_community.farao.data.refprog.reference_program.ReferenceProgram;
+import com.powsybl.iidm.network.Country;
+import com.powsybl.iidm.network.Network;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * @author Philippe Edwards {@literal <philippe.edwards at rte-france.com>}
+ */
+public final class RaoInput {
+
+    public static final class RaoInputBuilder {
+
+        private Crac crac;
+        private Network network;
+        private String variantId;
+        private List<Pair<Country, Country> > boundaries;
+        private ReferenceProgram referenceProgram;
+
+        private RaoInputBuilder() {
+
+        }
+
+        public RaoInputBuilder withCrac(Crac crac) {
+            this.crac = crac;
+            return this;
+        }
+
+        public RaoInputBuilder withNetwork(Network network) {
+            this.network = network;
+            return this;
+        }
+
+        public RaoInputBuilder withVariantId(String variantId) {
+            this.variantId = variantId;
+            return this;
+        }
+
+        public RaoInputBuilder withBoundaries(List<Pair<Country, Country> > boundaries) {
+            this.boundaries = boundaries;
+            return this;
+        }
+
+        public RaoInputBuilder withRefProg(ReferenceProgram referenceProgram) {
+            this.referenceProgram = referenceProgram;
+            return this;
+        }
+
+        public RaoInput build() {
+            if (Objects.isNull(network)) {
+                throw new RaoInputException("Network is mandatory when building RAO input.");
+            }
+            if (Objects.isNull(crac)) {
+                throw new RaoInputException("CRAC is mandatory when building RAO input.");
+            }
+
+            RaoInput raoInput = new RaoInput();
+            raoInput.crac = crac;
+            raoInput.network = network;
+            if (Objects.isNull(variantId)) {
+                raoInput.variantId = network.getVariantManager().getWorkingVariantId();
+            } else {
+                raoInput.variantId = variantId;
+            }
+            raoInput.boundaries = boundaries;
+            raoInput.referenceProgram = Objects.isNull(referenceProgram) ? Optional.empty() : Optional.of(referenceProgram);
+
+            return raoInput;
+        }
+    }
+
+    //TODO: add an optional GLSK provider argument
+    private Crac crac;
+    private Network network;
+    private String variantId;
+    private List<Pair<Country, Country> > boundaries;
+    private Optional<ReferenceProgram> referenceProgram;
+
+    private RaoInput() {
+    }
+
+    public static RaoInputBuilder builder() {
+        return new RaoInputBuilder();
+    }
+
+    public Crac getCrac() {
+        return crac;
+    }
+
+    public Network getNetwork() {
+        return network;
+    }
+
+    public String getVariantId() {
+        return variantId;
+    }
+
+    public void setVariantId(String variantId) {
+        this.variantId = variantId;
+    }
+
+    public List<Pair<Country, Country>> getBoundaries() {
+        return boundaries;
+    }
+
+    public Optional<ReferenceProgram> getReferenceProgram() {
+        return referenceProgram;
+    }
+}
