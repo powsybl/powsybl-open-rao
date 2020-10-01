d30a0c2226fe854b7a2778d151ff3f05a0bf44a2
Joris Mancini <joris.mancini@rte-france.com>
2020-10-01 14:25:06 +0200

    Implement simple dichotomy, first iplementation of DichotomyProvider
    Signed-off-by: Joris Mancini <joris.mancini@rte-france.com>

---
+++ dichotomy/simple-dichotomy/src/main/java/com/farao_community/farao/dichotomy/SimpleDichotomy.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.dichotomy;
+
+import com.farao_community.farao.data.crac_api.Crac;
+import com.google.auto.service.AutoService;
+import com.powsybl.computation.ComputationManager;
+import com.powsybl.iidm.network.Country;
+import com.powsybl.iidm.network.Network;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
+ */
+@AutoService(DichotomyProvider.class)
+public class SimpleDichotomy implements DichotomyProvider {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDichotomy.class);
+
+    @Override
+    public CompletableFuture<DichotomyResult> run(Network network, Crac crac, Set<Country> region, DichotomyParameters parameters, ComputationManager computationManager) {
+        LOGGER.info("For simple dichotomy, computation manager will be ignored");
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "simple-dichotomy";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+}
