f59ea0d0575184911c4f8d39875c75eed65d3a8c
Viktor Terrier <viktor.terrier@rte-france.com>
2020-11-20 12:29:19 +0100

    Test

--- flowbased-computation/flowbased-computation-api/src/test/java/com/farao_community/farao/flowbased_computation/FlowbasedComputationToolTest.java
+++ flowbased-computation/flowbased-computation-api/src/test/java/com/farao_community/farao/flowbased_computation/FlowbasedComputationToolTest.java
@@ -53,11 +53,12 @@ public class FlowbasedComputationToolTest extends AbstractToolTest {
 
     @Override
     public void assertCommand() {
-        assertCommand(tool.getCommand(), COMMAND_NAME, 6, 4);
+        assertCommand(tool.getCommand(), COMMAND_NAME, 7, 4);
         assertOption(tool.getCommand().getOptions(), "case-file", true, true);
         assertOption(tool.getCommand().getOptions(), "crac-file", true, true);
         assertOption(tool.getCommand().getOptions(), "glsk-file", true, true);
         assertOption(tool.getCommand().getOptions(), "parameters-file", false, true);
+        assertOption(tool.getCommand().getOptions(), "define-aliases", false, false);
         assertOption(tool.getCommand().getOptions(), "instant", true, true);
         assertOption(tool.getCommand().getOptions(), "output-file", false, true);
 
