a1fba3b7f2c1d8c021b683b147002a299f3cd0e6
Joris Mancini <joris.mancini@rte-france.com>
2020-11-20 16:05:12 +0100

    Fix mergeRaoResults method - If the post-optim variant in curative in the same as the post-optim variant in preventive, it should not be deleted

--- ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/SearchTreeRaoProvider.java
+++ ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/SearchTreeRaoProvider.java
@@ -184,7 +184,7 @@ public class SearchTreeRaoProvider implements RaoProvider {
         mergeRaoResultStatus(preventiveRaoResult, curativeRaoResults);
         mergeCnecResults(crac, preventiveRaoResult, curativeRaoResults);
         mergeRemedialActionsResults(crac, preventiveRaoResult, curativeRaoResults);
-        deleteCurativeVariants(crac, curativeRaoResults);
+        deleteCurativeVariants(crac, preventiveRaoResult.getPostOptimVariantId(), curativeRaoResults);
         return preventiveRaoResult;
     }
 
@@ -240,8 +240,11 @@ public class SearchTreeRaoProvider implements RaoProvider {
         });
     }
 
-    private void deleteCurativeVariants(Crac crac, Map<State, RaoResult> curativeRaoResults) {
-        curativeRaoResults.values().stream().map(RaoResult::getPostOptimVariantId).forEach(variantId ->
-            crac.getExtension(ResultVariantManager.class).deleteVariant(variantId));
+    private void deleteCurativeVariants(Crac crac, String preventivePostOptimVariantId, Map<State, RaoResult> curativeRaoResults) {
+        curativeRaoResults.values().stream().map(RaoResult::getPostOptimVariantId).forEach(variantId -> {
+            if (!variantId.equals(preventivePostOptimVariantId)) {
+                crac.getExtension(ResultVariantManager.class).deleteVariant(variantId);
+            }
+        });
     }
 }
