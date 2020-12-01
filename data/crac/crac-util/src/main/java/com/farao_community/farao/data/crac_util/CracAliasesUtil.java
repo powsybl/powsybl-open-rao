03591175f0a93083b5d5e9085f5046e32b92be7b
Viktor Terrier <viktor.terrier@rte-france.com>
2020-12-01 11:52:19 +0100

    Refactor

--- data/crac/crac-util/src/main/java/com/farao_community/farao/data/crac_util/CracAliasesUtil.java
+++ data/crac/crac-util/src/main/java/com/farao_community/farao/data/crac_util/CracAliasesUtil.java
@@ -14,11 +14,16 @@ import com.powsybl.iidm.network.*;
 import java.util.*;
 import java.util.regex.Pattern;
 
+import static com.farao_community.farao.data.crac_util.UcteNodeMatchingRule.BEGINNING_OF_ELEMENT_NAME;
+import static com.farao_community.farao.data.crac_util.UcteNodeMatchingRule.FIRST_7_CHARACTER_EQUAL;
+
 /**
  * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
  */
 
 public final class CracAliasesUtil {
+
+    static final int NB_CHARACTER_IN_UCTE_NODE = 9;
 
     private CracAliasesUtil() {
     }
@@ -36,6 +41,9 @@ public final class CracAliasesUtil {
         });
     }
 
+    /* It only works correctly for Cnec with direction = both. This corrupts the other Cnec.
+    TODO : only look for the elements with reverse ID if all the thresholds of the cnec have a BOTH direction.
+     */
     private static boolean anyMatch(Identifiable<?> identifiable, String cnecId) {
         return nameMatches(identifiable, cnecId, false) ||
             aliasMatches(identifiable, cnecId, false) ||
@@ -52,8 +60,8 @@ public final class CracAliasesUtil {
     }
 
     private static String checkWithPattern(String string, boolean reverse) {
-        int first = reverse ? 9 : 0;
-        int second = reverse ? 0 : 9;
-        return Pattern.quote(string.substring(first, first + 7)) + ".*" + " " + Pattern.quote(string.substring(second, second + 7)) + ".*" + Pattern.quote(string.substring(17)).trim();
+        int first = reverse ? NB_CHARACTER_IN_UCTE_NODE : 0;
+        int second = reverse ? 0 : NB_CHARACTER_IN_UCTE_NODE;
+        return Pattern.quote(string.substring(first, first + FIRST_7_CHARACTER_EQUAL.getNbCharacter())) + ".*" + " " + Pattern.quote(string.substring(second, second + FIRST_7_CHARACTER_EQUAL.getNbCharacter())) + ".*" + Pattern.quote(string.substring(BEGINNING_OF_ELEMENT_NAME.getNbCharacter())).trim();
     }
 }
