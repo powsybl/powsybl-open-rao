12b65149ee9d70a7b469f99bdbf1b0f7732a4e53
Viktor Terrier <viktor.terrier@rte-france.com>
2020-12-01 10:51:53 +0100

    Copy cleanCrac

--- data/crac/crac-util/src/main/java/com/farao_community/farao/data/crac_util/CracCleaner.java
+++ data/crac/crac-util/src/main/java/com/farao_community/farao/data/crac_util/CracCleaner.java
@@ -7,13 +7,13 @@
 
 package com.farao_community.farao.data.crac_util;
 
-import com.farao_community.farao.data.crac_api.Crac;
+import com.farao_community.farao.data.crac_api.*;
+import com.powsybl.iidm.network.*;
 import com.powsybl.iidm.network.Identifiable;
-import com.powsybl.iidm.network.Network;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
 
 /**
@@ -21,6 +21,8 @@ import java.util.regex.Pattern;
  */
 
 public final class CracCleaner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CracCleaner.class);
 
     private CracCleaner() {
     }
@@ -59,4 +61,88 @@ public final class CracCleaner {
         return Pattern.quote(string.substring(first, first + 7)) + ".*" + " " + Pattern.quote(string.substring(second, second + 7)) + ".*" + Pattern.quote(string.substring(17)).trim();
     }
 
+    public static List<String> cleanCrac(Crac crac, Network network) {
+        List<String> report = new ArrayList<>();
+
+        // remove Cnec whose NetworkElement is absent from the network
+        ArrayList<Cnec> absentFromNetworkCnecs = new ArrayList<>();
+        crac.getCnecs().forEach(cnec -> {
+            if (network.getBranch(cnec.getNetworkElement().getId()) == null) {
+                absentFromNetworkCnecs.add(cnec);
+                report.add(String.format("[REMOVED] Cnec %s with network element [%s] is not present in the network. It is removed from the Crac", cnec.getId(), cnec.getNetworkElement().getId()));
+            }
+        });
+        absentFromNetworkCnecs.forEach(cnec -> crac.removeCnec(cnec.getId()));
+
+        // remove Cnecs that are neither optimized nor monitored
+        ArrayList<Cnec> unmonitoredCnecs = new ArrayList<>();
+        crac.getCnecs().forEach(cnec -> {
+            if (!cnec.isOptimized() && !cnec.isMonitored()) {
+                unmonitoredCnecs.add(cnec);
+                report.add(String.format("[REMOVED] Cnec %s with network element [%s] is neither optimized nor monitored. It is removed from the Crac", cnec.getId(), cnec.getNetworkElement().getId()));
+            }
+        });
+        unmonitoredCnecs.forEach(cnec -> crac.removeCnec(cnec.getId()));
+
+        // remove RangeAction whose NetworkElement is absent from the network
+        ArrayList<RangeAction> absentFromNetworkRangeActions = new ArrayList<>();
+        for (RangeAction rangeAction: crac.getRangeActions()) {
+            rangeAction.getNetworkElements().forEach(networkElement -> {
+                if (network.getIdentifiable(networkElement.getId()) == null) {
+                    absentFromNetworkRangeActions.add(rangeAction);
+                    report.add(String.format("[REMOVED] Remedial Action %s with network element [%s] is not present in the network. It is removed from the Crac", rangeAction.getId(), networkElement.getId()));
+                }
+            });
+        }
+        absentFromNetworkRangeActions.forEach(rangeAction -> crac.removeRangeAction(rangeAction.getId()));
+
+        // remove NetworkAction whose NetworkElement is absent from the network
+        ArrayList<NetworkAction> absentFromNetworkNetworkActions = new ArrayList<>();
+        for (NetworkAction networkAction: crac.getNetworkActions()) {
+            networkAction.getNetworkElements().forEach(networkElement -> {
+                if (network.getIdentifiable(networkElement.getId()) == null) {
+                    absentFromNetworkNetworkActions.add(networkAction);
+                    report.add(String.format("[REMOVED] Remedial Action %s with network element [%s] is not present in the network. It is removed from the Crac", networkAction.getId(), networkElement.getId()));
+                }
+            });
+        }
+        absentFromNetworkNetworkActions.forEach(networkAction -> crac.removeNetworkAction(networkAction.getId()));
+
+        // remove Contingencies whose NetworkElement is absent from the network or does not fit a valid Powsybl Contingency
+        Set<Contingency> absentFromNetworkContingencies = new HashSet<>();
+        for (Contingency contingency : crac.getContingencies()) {
+            contingency.getNetworkElements().forEach(networkElement -> {
+                Identifiable<?> identifiable = network.getIdentifiable(networkElement.getId());
+                if (identifiable == null) {
+                    absentFromNetworkContingencies.add(contingency);
+                    report.add(String.format("[REMOVED] Contingency %s with network element [%s] is not present in the network. It is removed from the Crac", contingency.getId(), networkElement.getId()));
+                } else if (!(identifiable instanceof Branch || identifiable instanceof Generator || identifiable instanceof HvdcLine || identifiable instanceof BusbarSection || identifiable instanceof DanglingLine)) {
+                    report.add(String.format("[WARNING] Contingency %s has a network element [%s] of unhandled type [%s]. This may result in unexpected behavior.", contingency.getId(), networkElement.getId(), identifiable.getClass().toString()));
+                }
+            });
+        }
+
+        absentFromNetworkContingencies.forEach(contingency ->  {
+            crac.getStatesFromContingency(contingency.getId()).forEach(state -> {
+                crac.getCnecs(state).forEach(cnec -> {
+                    crac.removeCnec(cnec.getId());
+                    report.add(String.format("[REMOVED] Cnec %s is removed because its associated contingency [%s] has been removed", cnec.getId(), contingency.getId()));
+                });
+                crac.removeState(state.getId());
+            });
+            crac.removeContingency(contingency.getId());
+        });
+
+        // remove Remedial Action with an empty list of NetworkElement
+        ArrayList<NetworkAction> noValidAction = new ArrayList<>();
+        crac.getNetworkActions().stream().filter(na -> na.getNetworkElements().isEmpty()).forEach(na -> {
+            report.add(String.format("[REMOVED] Remedial Action %s has no associated action. It is removed from the Crac", na.getId()));
+            noValidAction.add(na);
+        });
+        noValidAction.forEach(networkAction -> crac.removeNetworkAction(networkAction.getId()));
+
+        report.forEach(LOGGER::warn);
+
+        return report;
+    }
 }
--- data/crac/crac-util/src/test/java/com/farao_community/farao/data/crac_util/CracCleanerTest.java
+++ data/crac/crac-util/src/test/java/com/farao_community/farao/data/crac_util/CracCleanerTest.java
@@ -7,19 +7,44 @@
 
 package com.farao_community.farao.data.crac_util;
 
-import com.farao_community.farao.data.crac_api.Crac;
+import com.farao_community.farao.commons.Unit;
+import com.farao_community.farao.data.crac_api.*;
+import com.farao_community.farao.data.crac_impl.SimpleCrac;
+import com.farao_community.farao.data.crac_impl.range_domain.Range;
+import com.farao_community.farao.data.crac_impl.range_domain.RangeType;
+import com.farao_community.farao.data.crac_impl.remedial_action.network_action.ComplexNetworkAction;
+import com.farao_community.farao.data.crac_impl.remedial_action.network_action.Topology;
+import com.farao_community.farao.data.crac_impl.remedial_action.range_action.PstWithRange;
+import com.farao_community.farao.data.crac_impl.threshold.AbsoluteFlowThreshold;
+import com.farao_community.farao.data.crac_impl.threshold.RelativeFlowThreshold;
+import com.farao_community.farao.data.crac_impl.usage_rule.FreeToUse;
 import com.farao_community.farao.data.crac_io_api.CracImporters;
 import com.powsybl.iidm.import_.Importers;
 import com.powsybl.iidm.network.Network;
+import com.farao_community.farao.data.crac_impl.utils.NetworkImportsUtil;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
  */
 
 public class CracCleanerTest {
+
+    private Network network;
+
+    @Before
+    public void setUp() {
+        network = NetworkImportsUtil.import12NodesNetwork();
+    }
 
     @Test
     public void testCracAliasesUtil() {
@@ -34,5 +59,114 @@ public class CracCleanerTest {
         assertEquals(1, network.getBranch("FFR1AA1G FFR3AA1I 1").getAliases().size());
         assertEquals(2, network.getBranch("DDE1AA1D DDE2AA1E 1").getAliases().size());
         assertEquals(3, network.getBranch("FFR2AA1H DDE3AA1F 1").getAliases().size());
+    }
+
+    @Test
+    public void testCleanCrac() {
+        SimpleCrac simpleCrac = new SimpleCrac("cracId");
+
+        Contingency contingency = simpleCrac.addContingency("contingencyId", "FFR1AA1  FFR2AA1  1");
+        simpleCrac.addContingency("contingency2Id", "BBE1AA1  BBE2AA1  1", "BBE1AA1  BBE3AA1  1");
+        simpleCrac.addContingency("contThatShouldBeRemoved", "element that does not exist");
+
+        Instant initialInstant = simpleCrac.addInstant("N", 0);
+        Instant outageInstant = simpleCrac.addInstant("postContingencyId", 5);
+
+        State preventiveState = simpleCrac.addState(null, initialInstant);
+        State postContingencyState = simpleCrac.addState(contingency, outageInstant);
+        State stateThatShouldBeRemoved = simpleCrac.addState("contThatShouldBeRemoved", "postContingencyId");
+
+        simpleCrac.addNetworkElement("neId1");
+        simpleCrac.addNetworkElement("neId2");
+        simpleCrac.addNetworkElement(new NetworkElement("pst"));
+
+        simpleCrac.addCnec("cnec1prev", "FFR1AA1  FFR2AA1  1", Collections.singleton(new AbsoluteFlowThreshold(Unit.AMPERE, Side.LEFT, Direction.OPPOSITE, 500)), preventiveState.getId());
+        simpleCrac.addCnec("cnec2prev", "neId2", Collections.singleton(new RelativeFlowThreshold(Side.LEFT, Direction.OPPOSITE, 30)), preventiveState.getId());
+        simpleCrac.addCnec("cnec1cur", "neId1", Collections.singleton(new AbsoluteFlowThreshold(Unit.AMPERE, Side.LEFT, Direction.OPPOSITE, 800)), postContingencyState.getId());
+        simpleCrac.addCnec("cnec3cur", "BBE1AA1  BBE2AA1  1", Collections.singleton(new AbsoluteFlowThreshold(Unit.AMPERE, Side.LEFT, Direction.OPPOSITE, 500)), stateThatShouldBeRemoved.getId());
+
+        Topology topology1 = new Topology(
+            "topologyId1",
+            "topologyName",
+            "RTE",
+            new ArrayList<>(),
+            simpleCrac.getNetworkElement("neId1"),
+            ActionType.CLOSE
+        );
+        Topology topology2 = new Topology(
+            "topologyId2",
+            "topologyName",
+            "RTE",
+            new ArrayList<>(),
+            simpleCrac.getNetworkElement("FFR1AA1  FFR2AA1  1"),
+            ActionType.CLOSE
+        );
+        ComplexNetworkAction complexNetworkAction = new ComplexNetworkAction("complexNextworkActionId", "RTE");
+        PstWithRange pstWithRange = new PstWithRange(
+            "pstRangeId",
+            "pstRangeName",
+            "RTE",
+            Collections.singletonList(new FreeToUse(UsageMethod.AVAILABLE, preventiveState)),
+            Arrays.asList(new Range(0, 16, RangeType.ABSOLUTE_FIXED, RangeDefinition.STARTS_AT_ONE)),
+            simpleCrac.getNetworkElement("pst")
+        );
+
+        simpleCrac.addNetworkAction(topology1);
+        simpleCrac.addNetworkAction(topology2);
+        simpleCrac.addNetworkAction(complexNetworkAction);
+        simpleCrac.addRangeAction(pstWithRange);
+        assertEquals(4, simpleCrac.getCnecs().size());
+        assertEquals(3, simpleCrac.getNetworkActions().size());
+        assertEquals(1, simpleCrac.getRangeActions().size());
+        assertEquals(3, simpleCrac.getContingencies().size());
+        assertEquals(3, simpleCrac.getStates().size());
+
+        List<String> qualityReport = CracCleaner.cleanCrac(simpleCrac, network);
+
+        assertEquals(1, simpleCrac.getCnecs().size());
+        assertEquals(1, simpleCrac.getNetworkActions().size());
+        assertEquals(0, simpleCrac.getRangeActions().size());
+        assertEquals(2, simpleCrac.getContingencies().size());
+        assertEquals(2, simpleCrac.getStates().size());
+
+        assertEquals(7, qualityReport.size());
+        int removedCount = 0;
+        for (String line : qualityReport) {
+            if (line.contains("[REMOVED]")) {
+                removedCount++;
+            }
+        }
+        assertEquals(7, removedCount);
+    }
+
+    @Test
+    public void testRemoveUnmonitoredCnecs() {
+        CracFactory factory = CracFactory.find("SimpleCracFactory");
+        Crac crac = factory.create("test-crac");
+        Instant inst = crac.newInstant().setId("inst1").setSeconds(10).add();
+        crac.newCnec().setId("BBE1AA1  BBE2AA1  1").setOptimized(true).setMonitored(true)
+            .newNetworkElement().setId("BBE1AA1  BBE2AA1  1").add()
+            .newThreshold().setUnit(Unit.MEGAWATT).setMaxValue(0.0).setDirection(Direction.BOTH).setSide(Side.LEFT).add()
+            .setInstant(inst)
+            .add();
+        crac.newCnec().setId("BBE1AA1  BBE3AA1  1").setOptimized(true).setMonitored(false)
+            .newNetworkElement().setId("BBE1AA1  BBE3AA1  1").add()
+            .newThreshold().setUnit(Unit.MEGAWATT).setMaxValue(0.0).setDirection(Direction.BOTH).setSide(Side.LEFT).add()
+            .setInstant(inst)
+            .add();
+        crac.newCnec().setId("FFR1AA1  FFR2AA1  1").setOptimized(false).setMonitored(true)
+            .newNetworkElement().setId("FFR1AA1  FFR2AA1  1").add()
+            .newThreshold().setUnit(Unit.MEGAWATT).setMaxValue(0.0).setDirection(Direction.BOTH).setSide(Side.LEFT).add()
+            .setInstant(inst)
+            .add();
+        crac.newCnec().setId("FFR1AA1  FFR3AA1  1").setOptimized(false).setMonitored(false)
+            .newNetworkElement().setId("FFR1AA1  FFR3AA1  1").add()
+            .newThreshold().setUnit(Unit.MEGAWATT).setMaxValue(0.0).setDirection(Direction.BOTH).setSide(Side.LEFT).add()
+            .setInstant(inst)
+            .add();
+        List<String> qualityReport = CracCleaner.cleanCrac(crac, network);
+        assertEquals(1, qualityReport.size());
+        assertEquals(3, crac.getCnecs().size());
+        assertNull(crac.getCnec("FFR1AA1  FFR3AA1  1"));
     }
 }
