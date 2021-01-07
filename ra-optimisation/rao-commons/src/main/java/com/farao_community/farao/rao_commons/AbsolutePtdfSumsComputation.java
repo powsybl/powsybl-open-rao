a6a0817afc757aa54b3c94427ed6e7464818bdcf
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2021-01-07 16:06:40 +0100

    change PTDF sum computation to handle GLSKs which are not on countries

--- ra-optimisation/rao-commons/src/main/java/com/farao_community/farao/rao_commons/AbsolutePtdfSumsComputation.java
+++ ra-optimisation/rao-commons/src/main/java/com/farao_community/farao/rao_commons/AbsolutePtdfSumsComputation.java
@@ -6,6 +6,7 @@
  */
 package com.farao_community.farao.rao_commons;
 
+import com.farao_community.farao.commons.FaraoException;
 import com.farao_community.farao.commons.ZonalData;
 import com.farao_community.farao.data.crac_api.cnec.BranchCnec;
 import com.farao_community.farao.sensitivity_analysis.SystematicSensitivityResult;
@@ -14,10 +15,9 @@ import com.powsybl.iidm.network.Country;
 import com.powsybl.sensitivity.factors.variables.LinearGlsk;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  *  This class computes the absolute PTDF sums on a given set of CNECs
@@ -30,7 +30,7 @@ public final class AbsolutePtdfSumsComputation {
 
     public static Map<BranchCnec, Double> computeAbsolutePtdfSums(Set<BranchCnec> cnecs, ZonalData<LinearGlsk> glsk, List<Pair<Country, Country>> boundaries, SystematicSensitivityResult sensitivityResult) {
         Map<BranchCnec, Double> ptdfSums = new HashMap<>();
-        Map<String, Map<Country, Double>> ptdfMap = computePtdf(cnecs, glsk, sensitivityResult);
+        Map<String, Map<Country, Double>> ptdfMap = computePtdf(cnecs, glsk, getCountriesInBoundaries(boundaries), sensitivityResult);
         cnecs.forEach(cnec -> {
             double ptdfSum = 0;
             for (Pair<Country, Country> countryPair : boundaries) {
@@ -43,13 +43,20 @@ public final class AbsolutePtdfSumsComputation {
         return ptdfSums;
     }
 
-    private static Map<String, Map<Country, Double>> computePtdf(Set<BranchCnec> cnecs, ZonalData<LinearGlsk> glsk, SystematicSensitivityResult sensitivityResult) {
+    private static Map<String, Map<Country, Double>> computePtdf(Set<BranchCnec> cnecs, ZonalData<LinearGlsk> glsk, List<Country> countriesInBoundaries, SystematicSensitivityResult sensitivityResult) {
+
         Map<String, Map<Country, Double>> ptdfs = new HashMap<>();
         Map<String, LinearGlsk> mapCountryLinearGlsk = glsk.getDataPerZone();
-        for (BranchCnec cnec : cnecs) {
-            for (LinearGlsk linearGlsk: mapCountryLinearGlsk.values()) {
+
+        for (LinearGlsk linearGlsk: mapCountryLinearGlsk.values()) {
+
+            if (!isGlskInBoundaries(linearGlsk.getId(), countriesInBoundaries)) {
+                continue;
+            }
+            Country country = glskIdToCountry(linearGlsk.getId());
+
+            for (BranchCnec cnec : cnecs) {
                 double ptdfValue = sensitivityResult.getSensitivityOnFlow(linearGlsk, cnec);
-                Country country = glskIdToCountry(linearGlsk.getId());
                 if (!ptdfs.containsKey(cnec.getId())) {
                     ptdfs.put(cnec.getId(), new HashMap<>());
                 }
@@ -59,11 +66,28 @@ public final class AbsolutePtdfSumsComputation {
         return ptdfs;
     }
 
+    private static boolean isGlskInBoundaries(String glskId, List<Country> countriesInBoundaries) {
+        try {
+            Country glskCountry = glskIdToCountry(glskId);
+            return countriesInBoundaries.contains(glskCountry);
+        } catch (IllegalArgumentException | FaraoException e) {
+            return false;
+        }
+
+    }
+
     private static Country glskIdToCountry(String glskId) {
         if (glskId.length() < EICode.LENGTH) {
             throw new IllegalArgumentException(String.format("GlskId [%s] should starts with an EI Code", glskId));
         }
         EICode eiCode = new EICode(glskId.substring(0, EICode.LENGTH));
         return eiCode.getCountry();
+    }
+
+    private static List<Country> getCountriesInBoundaries(List<Pair<Country, Country>> boundaries) {
+        return boundaries.stream()
+            .flatMap(countryPair -> Stream.of(countryPair.getLeft(), countryPair.getRight()))
+            .distinct()
+            .collect(Collectors.toList());
     }
 }
