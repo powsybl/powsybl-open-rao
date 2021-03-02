e3e5dfd7cc0b0662e7d2d01a46dfeadfc329f4d9
Peter <47452063+pet-mit@users.noreply.github.com>
2021-03-02 09:17:25 +0100

    Fix behaviour when a LinearGlsk is missing (#441)

--- ra-optimisation/rao-commons/src/test/java/com/farao_community/farao/rao_commons/AbsolutePtdfSumsComputationTest.java
+++ ra-optimisation/rao-commons/src/test/java/com/farao_community/farao/rao_commons/AbsolutePtdfSumsComputationTest.java
@@ -107,4 +107,24 @@ public class AbsolutePtdfSumsComputationTest {
         assertEquals(0, ptdfSums.get(crac.getBranchCnec("cnec2stateCurativeContingency1")), DOUBLE_TOLERANCE);
         assertEquals(0, ptdfSums.get(crac.getBranchCnec("cnec2stateCurativeContingency2")), DOUBLE_TOLERANCE);
     }
+
+    @Test
+    public void testIgnoreAbsentGlsk() {
+        boundaries = Arrays.asList(
+                new ZoneToZonePtdfDefinition("{FR}-{BE}"),
+                new ZoneToZonePtdfDefinition("{FR}-{DE}"),
+                new ZoneToZonePtdfDefinition("{DE}-{BE}"),
+                new ZoneToZonePtdfDefinition("{BE}-{22Y201903144---9}-{DE}+{22Y201903145---4}"),
+                new ZoneToZonePtdfDefinition("{FR}-{ES}"), // ES doesn't exist in GLSK map
+                new ZoneToZonePtdfDefinition("{ES}-{DE}"), // ES doesn't exist in GLSK map
+                new ZoneToZonePtdfDefinition("{22Y201903144---0}-{22Y201903144---1}")); // EICodes that don't exist in GLSK map
+        Map<BranchCnec, Double> ptdfSums = AbsolutePtdfSumsComputation.computeAbsolutePtdfSums(crac.getBranchCnecs(), glskProvider, boundaries, systematicSensitivityResult);
+        // Test that these 3 new boundaries are ignored (results should be the same as previous test)
+        assertEquals(0.6, ptdfSums.get(crac.getBranchCnec("cnec1basecase")), DOUBLE_TOLERANCE);
+        assertEquals(0.9, ptdfSums.get(crac.getBranchCnec("cnec2basecase")), DOUBLE_TOLERANCE);
+        assertEquals(0, ptdfSums.get(crac.getBranchCnec("cnec1stateCurativeContingency1")), DOUBLE_TOLERANCE);
+        assertEquals(0, ptdfSums.get(crac.getBranchCnec("cnec1stateCurativeContingency2")), DOUBLE_TOLERANCE);
+        assertEquals(0, ptdfSums.get(crac.getBranchCnec("cnec2stateCurativeContingency1")), DOUBLE_TOLERANCE);
+        assertEquals(0, ptdfSums.get(crac.getBranchCnec("cnec2stateCurativeContingency2")), DOUBLE_TOLERANCE);
+    }
 }
