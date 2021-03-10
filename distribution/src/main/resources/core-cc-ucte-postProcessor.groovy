d3f7b976e1af61f86fd6ac53e3017db8e5d20d98
pet-mit <peter.mitri@outlook.com>
2021-03-10 12:28:44 +0100

    Import all loads and generators in network post-processing

--- distribution/src/main/resources/core-cc-ucte-postProcessor.groovy
+++ distribution/src/main/resources/core-cc-ucte-postProcessor.groovy
@@ -3,7 +3,6 @@ import com.powsybl.iidm.network.DanglingLine
 import com.powsybl.iidm.network.EnergySource
 import com.powsybl.iidm.network.LoadType
 import com.powsybl.iidm.network.Network
-import com.powsybl.iidm.network.Network
 import com.powsybl.iidm.network.VoltageLevel
 
 /*
@@ -32,6 +31,59 @@ def updateVoltageLevelNominalV(VoltageLevel voltageLevel) {
         voltageLevel.nominalV = 225
     } else {
         // Should not be changed cause is not equal to the default nominal voltage of voltage levels 6 or 7
+    }
+}
+
+/*
+    When importing an UCTE network file, powsybl ignores generators and loads that do not have an initial power flow.
+
+    It can cause an error if a GLSK file associated to this network includes some factors on
+    these nodes. The GLSK importers looks for a Generator (GSK) or Load (LSK) associated to this
+    node. If the Generator/Load does not exist, the GLSK cannot be created.
+
+    This script fix this problem, by creating for all missing generators a generator (P, Q = 0),
+    and all missing loads a load (P, Q = 0).
+ */
+
+createMissingGeneratorsAndLoads()
+
+void createMissingGeneratorsAndLoads() {
+    network.getVoltageLevelStream().forEach({voltageLevel -> createMissingGeneratorsAndLoads(voltageLevel)})
+}
+
+void createMissingGeneratorsAndLoads(VoltageLevel voltageLevel) {
+    voltageLevel.getBusBreakerView().getBuses().forEach({bus -> createMissingGenerator(voltageLevel, bus.getId())})
+    voltageLevel.getBusBreakerView().getBuses().forEach({bus -> createMissingLoad(voltageLevel, bus.getId())})
+}
+
+void createMissingGenerator(VoltageLevel voltageLevel, String busId) {
+    String generatorId = busId + "_generator"
+    if (network.getGenerator(generatorId) == null) {
+        voltageLevel.newGenerator()
+                .setBus(busId)
+                .setEnsureIdUnicity(true)
+                .setId(generatorId)
+                .setMaxP(9999)
+                .setMinP(0)
+                .setTargetP(0)
+                .setTargetQ(0)
+                .setTargetV(voltageLevel.getNominalV())
+                .setVoltageRegulatorOn(false)
+                .add()
+    }
+}
+
+void createMissingLoad(VoltageLevel voltageLevel, String busId) {
+    String loadId = busId + "_load"
+    if (network.getLoad(loadId) == null) {
+        voltageLevel.newLoad()
+                .setBus(busId)
+                .setEnsureIdUnicity(true)
+                .setId(loadId)
+                .setP0(0)
+                .setQ0(0)
+                .setLoadType(LoadType.FICTITIOUS)
+                .add()
     }
 }
 
