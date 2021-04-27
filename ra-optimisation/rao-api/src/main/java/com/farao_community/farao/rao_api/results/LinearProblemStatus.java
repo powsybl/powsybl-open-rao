3732dd7f687e679877d9da12584df4ba4ced7008
Joris Mancini <joris.mancini@rte-france.com>
2021-04-27 16:43:52 +0200

    - Small changes in interfaces : implement getRelativeMargin by default + LinearProblemStatus created from MPSolver status image  - Add an adapter from SystematicSensitivityResult that implements result interfaces

--- ra-optimisation/rao-api/src/main/java/com/farao_community/farao/rao_api/results/LinearProblemStatus.java
+++ ra-optimisation/rao-api/src/main/java/com/farao_community/farao/rao_api/results/LinearProblemStatus.java
@@ -10,7 +10,11 @@ package com.farao_community.farao.rao_api.results;
 /**
  * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
  */
-public interface LinearOptimizationResult extends BranchResult, RangeActionResult, ObjectiveFunctionResult {
-
-    LinearProblemStatus getStatus();
+public enum LinearProblemStatus {
+    OPTIMAL,
+    FEASIBLE,
+    INFEASIBLE,
+    UNBOUNDED,
+    ABNORMAL,
+    NOT_SOLVED
 }
