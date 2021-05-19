f4ab3df58b5e6f8b6e226dfd3feebd7cd2c50f1e
Montigny <alexandre.montigny@rte-france.com>
2021-05-19 08:21:54 +0200

    SearchTreeRaoProviderTest adaptation

--- ra-optimisation/search-tree-rao/src/test/java/com.farao_community.farao.search_tree_rao/SearchTreeRaoProviderTest.java
+++ ra-optimisation/search-tree-rao/src/test/java/com.farao_community.farao.search_tree_rao/SearchTreeRaoProviderTest.java
@@ -18,7 +18,7 @@ import com.farao_community.farao.data.crac_impl.remedial_action.network_action.T
 import com.farao_community.farao.data.crac_impl.usage_rule.OnStateImpl;
 import com.farao_community.farao.data.crac_impl.utils.CommonCracCreation;
 import com.farao_community.farao.data.crac_result_extensions.*;
-import com.farao_community.farao.rao_api.RaoResult;
+import com.farao_community.farao.rao_api.results.RaoResult;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
