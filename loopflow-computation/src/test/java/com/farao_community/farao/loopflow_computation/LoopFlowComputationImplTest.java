817a3d606a289e6944390af4ecc37cbd7a651c6f
pet-mit <peter.mitri@outlook.com>
2021-06-18 15:44:04 +0200

    unit tests

--- loopflow-computation/src/test/java/com/farao_community/farao/loopflow_computation/LoopFlowComputationImplTest.java
+++ loopflow-computation/src/test/java/com/farao_community/farao/loopflow_computation/LoopFlowComputationImplTest.java
@@ -15,22 +15,17 @@ import com.powsybl.iidm.network.*;
 import com.powsybl.sensitivity.factors.variables.LinearGlsk;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Map;
 
 import static junit.framework.TestCase.*;
+import static org.mockito.ArgumentMatchers.any;
 
 /**
  * @author Pengbo Wang {@literal <pengbo.wang at rte-international.com>}
  * @author Baptiste Seguinot {@literal <baptiste.seguinot at rte-france.com>}
  */
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({LoopFlowComputationImpl.class})
 public class LoopFlowComputationImplTest {
 
     private static final double DOUBLE_TOLERANCE = 0.1;
@@ -48,17 +43,31 @@ public class LoopFlowComputationImplTest {
         crac.getFlowCnec("DE-NL").addExtension(LoopFlowThresholdImpl.class, loopFlowExtensionMock);
     }
 
+    private Terminal mockInjection(boolean inMainComponent) {
+        Bus bus = Mockito.mock(Bus.class);
+        Mockito.doReturn(inMainComponent).when(bus).isInMainConnectedComponent();
+        Terminal.BusView busView = Mockito.mock(Terminal.BusView.class);
+        Mockito.doReturn(bus).when(busView).getBus();
+        Terminal terminal = Mockito.mock(Terminal.class);
+        Mockito.doReturn(busView).when(terminal).getBusView();
+        return terminal;
+    }
+
     @Test
     public void calculateLoopFlowTest() {
-        PowerMockito.mockStatic(LoopFlowComputationImpl.class);
-        Mockito.when(LoopFlowComputationImpl.isInMainComponent(Mockito.any(), Mockito.any()))
-                .thenAnswer(invocationOnMock -> true);
-
         ZonalData<LinearGlsk> glsk = ExampleGenerator.glskProvider();
         ReferenceProgram referenceProgram = ExampleGenerator.referenceProgram();
         SystematicSensitivityResult ptdfsAndFlows = ExampleGenerator.systematicSensitivityResult(crac, glsk);
 
-        LoopFlowComputation loopFlowComputation = new LoopFlowComputationImpl(glsk, referenceProgram, null);
+        Network network = Mockito.mock(Network.class);
+        Generator gen = Mockito.mock(Generator.class);
+        Load load = Mockito.mock(Load.class);
+        Mockito.when(network.getGenerator(any())).thenReturn(gen);
+        Mockito.when(network.getLoad(any())).thenReturn(load);
+        Mockito.doReturn(mockInjection(true)).when(gen).getTerminal();
+        Mockito.doReturn(mockInjection(true)).when(load).getTerminal();
+
+        LoopFlowComputation loopFlowComputation = new LoopFlowComputationImpl(glsk, referenceProgram, network);
         LoopFlowResult loopFlowResult = loopFlowComputation.buildLoopFlowsFromReferenceFlowAndPtdf(ptdfsAndFlows, crac.getFlowCnecs());
 
         assertEquals(-50., loopFlowResult.getLoopFlow(crac.getFlowCnec("FR-BE1")), DOUBLE_TOLERANCE);
@@ -131,13 +140,52 @@ public class LoopFlowComputationImplTest {
         assertTrue(LoopFlowComputationImpl.isInMainComponent(linearGlsk, network));
     }
 
-    private Terminal mockInjection(boolean inMainComponent) {
-        Bus bus = Mockito.mock(Bus.class);
-        Mockito.doReturn(inMainComponent).when(bus).isInMainConnectedComponent();
-        Terminal.BusView busView = Mockito.mock(Terminal.BusView.class);
-        Mockito.doReturn(bus).when(busView).getBus();
-        Terminal terminal = Mockito.mock(Terminal.class);
-        Mockito.doReturn(busView).when(terminal).getBusView();
-        return terminal;
+    @Test
+    public void testComputeLoopFlowsWithIsolatedGlsk() {
+        ZonalData<LinearGlsk> glsk = ExampleGenerator.glskProvider();
+        ReferenceProgram referenceProgram = ExampleGenerator.referenceProgram();
+        SystematicSensitivityResult ptdfsAndFlows = ExampleGenerator.systematicSensitivityResult(crac, glsk);
+
+        Network network = Mockito.mock(Network.class);
+
+        Generator genDe = Mockito.mock(Generator.class);
+        Generator genNl = Mockito.mock(Generator.class);
+        Load loadFr = Mockito.mock(Load.class);
+        Load loadBe = Mockito.mock(Load.class);
+
+        Mockito.when(network.getGenerator("Generator DE")).thenReturn(genDe);
+        Mockito.when(network.getGenerator("Generator NL")).thenReturn(genNl);
+        Mockito.when(network.getGenerator("Generator FR")).thenReturn(null);
+        Mockito.when(network.getGenerator("Generator BE 1")).thenReturn(null);
+        Mockito.when(network.getLoad("Generator DE")).thenReturn(null);
+        Mockito.when(network.getLoad("Generator NL")).thenReturn(null);
+        Mockito.when(network.getLoad("Generator FR")).thenReturn(loadFr);
+        Mockito.when(network.getLoad("Generator BE 1")).thenReturn(loadBe);
+
+        Mockito.doReturn(mockInjection(true)).when(genDe).getTerminal();
+        Mockito.doReturn(mockInjection(false)).when(genNl).getTerminal();
+        Mockito.doReturn(mockInjection(true)).when(loadFr).getTerminal();
+        Mockito.doReturn(mockInjection(false)).when(loadBe).getTerminal();
+
+        LoopFlowComputation loopFlowComputation = new LoopFlowComputationImpl(glsk, referenceProgram, network);
+        LoopFlowResult loopFlowResult = loopFlowComputation.buildLoopFlowsFromReferenceFlowAndPtdf(ptdfsAndFlows, crac.getFlowCnecs());
+
+        assertEquals(30., loopFlowResult.getLoopFlow(crac.getFlowCnec("FR-BE1")), DOUBLE_TOLERANCE);
+        assertEquals(280., loopFlowResult.getLoopFlow(crac.getFlowCnec("BE1-BE2")), DOUBLE_TOLERANCE);
+        assertEquals(30., loopFlowResult.getLoopFlow(crac.getFlowCnec("BE2-NL")), DOUBLE_TOLERANCE);
+        assertEquals(170., loopFlowResult.getLoopFlow(crac.getFlowCnec("FR-DE")), DOUBLE_TOLERANCE);
+        assertEquals(170., loopFlowResult.getLoopFlow(crac.getFlowCnec("DE-NL")), DOUBLE_TOLERANCE);
+
+        assertEquals(0, loopFlowResult.getCommercialFlow(crac.getFlowCnec("FR-BE1")), DOUBLE_TOLERANCE);
+        assertEquals(0., loopFlowResult.getCommercialFlow(crac.getFlowCnec("BE1-BE2")), DOUBLE_TOLERANCE);
+        assertEquals(0., loopFlowResult.getCommercialFlow(crac.getFlowCnec("BE2-NL")), DOUBLE_TOLERANCE);
+        assertEquals(0., loopFlowResult.getCommercialFlow(crac.getFlowCnec("FR-DE")), DOUBLE_TOLERANCE);
+        assertEquals(0., loopFlowResult.getCommercialFlow(crac.getFlowCnec("DE-NL")), DOUBLE_TOLERANCE);
+
+        assertEquals(30., loopFlowResult.getReferenceFlow(crac.getFlowCnec("FR-BE1")), DOUBLE_TOLERANCE);
+        assertEquals(280., loopFlowResult.getReferenceFlow(crac.getFlowCnec("BE1-BE2")), DOUBLE_TOLERANCE);
+        assertEquals(30., loopFlowResult.getReferenceFlow(crac.getFlowCnec("BE2-NL")), DOUBLE_TOLERANCE);
+        assertEquals(170., loopFlowResult.getReferenceFlow(crac.getFlowCnec("FR-DE")), DOUBLE_TOLERANCE);
+        assertEquals(170., loopFlowResult.getReferenceFlow(crac.getFlowCnec("DE-NL")), DOUBLE_TOLERANCE);
     }
 }
