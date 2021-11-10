cbc7f398e11fc801d28c6053fdd60e9c1615520a
EDWARDS Philippe <edwardsphi@gm0winl197.bureau.si.interne>
2021-11-10 18:17:21 +0100

    replaced comparator lambda by stocking some values in a map

--- ra-optimisation/rao-commons/src/main/java/com/farao_community/farao/rao_commons/objective_function_evaluator/MinMarginEvaluator.java
+++ ra-optimisation/rao-commons/src/main/java/com/farao_community/farao/rao_commons/objective_function_evaluator/MinMarginEvaluator.java
@@ -42,10 +42,14 @@ public class MinMarginEvaluator implements CostEvaluator {
 
     @Override
     public List<FlowCnec> getCostlyElements(FlowResult flowResult, int numberOfElements) {
+        Map<FlowCnec, Double> margins = new HashMap<>();
+        flowCnecs.stream().filter(Cnec::isOptimized).forEach(flowCnec -> {
+            margins.put(flowCnec, marginEvaluator.getMargin(flowResult, flowCnec, unit));
+        });
         List<FlowCnec> sortedElements = flowCnecs.stream()
-                .filter(Cnec::isOptimized)
-                .sorted(Comparator.comparing(flowCnec -> marginEvaluator.getMargin(flowResult, flowCnec, unit)))
-                .collect(Collectors.toList());
+            .filter(Cnec::isOptimized)
+            .sorted(Comparator.comparing(margins::get))
+            .collect(Collectors.toList());
 
         return sortedElements.subList(0, Math.min(sortedElements.size(), numberOfElements));
     }
