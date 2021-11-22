82ab36ff9eb67ac89bbbcaa72af9a43ad11a2cf6
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2021-11-22 12:04:35 +0100

    Virtual hubs 0.0.7 + allow partial computation of zToz PTDF when some hubs are missing (#549)
    * update virtual hubs version
    
    * Allow the partial computation of zToz PTDF, typically the computation of PTDF BE-DE when alegro nodes are disconnected
    
    * add TU

--- ra-optimisation/rao-commons/src/test/java/com/farao_community/farao/rao_commons/AbsolutePtdfSumsComputationTest.java
+++ ra-optimisation/rao-commons/src/test/java/com/farao_community/farao/rao_commons/AbsolutePtdfSumsComputationTest.java
@@ -115,7 +115,7 @@ public class AbsolutePtdfSumsComputationTest {
     }
 
     @Test
-    public void testIgnoreAbsentGlsk() {
+    public void testIgnoreZtoZWithLessThan2ZtoS() {
 
         // prepare data
         Network network = NetworkImportsUtil.import12NodesNetwork();
@@ -126,18 +126,17 @@ public class AbsolutePtdfSumsComputationTest {
                 new ZoneToZonePtdfDefinition("{FR}-{BE}"),
                 new ZoneToZonePtdfDefinition("{FR}-{DE}"),
                 new ZoneToZonePtdfDefinition("{DE}-{BE}"),
-                new ZoneToZonePtdfDefinition("{BE}-{22Y201903144---9}-{DE}+{22Y201903145---4}"),
-                new ZoneToZonePtdfDefinition("{FR}-{ES}"), // ES doesn't exist in GLSK map
-                new ZoneToZonePtdfDefinition("{ES}-{DE}"), // ES doesn't exist in GLSK map
-                new ZoneToZonePtdfDefinition("{22Y201903144---0}-{22Y201903144---1}")); // EICodes that don't exist in GLSK map
+                new ZoneToZonePtdfDefinition("{BE}-{22Y201903144---0}-{DE}+{22Y201903144---1}"), // wrong EIC for Alegro, only {BE}-{DE} will be taken into account
+                new ZoneToZonePtdfDefinition("{FR}-{ES}"), // ES doesn't exist in GLSK map, must be filtered
+                new ZoneToZonePtdfDefinition("{ES}-{DE}")); // ES doesn't exist in GLSK map, muste be filtered
 
         // compute zToz PTDF sum
         AbsolutePtdfSumsComputation absolutePtdfSumsComputation = new AbsolutePtdfSumsComputation(glskProvider, boundaries, network);
         Map<FlowCnec, Double> ptdfSums = absolutePtdfSumsComputation.computeAbsolutePtdfSums(crac.getFlowCnecs(), systematicSensitivityResult);
 
         // Test that these 3 new boundaries are ignored (results should be the same as previous test)
-        assertEquals(0.6, ptdfSums.get(crac.getFlowCnec("cnec1basecase")), DOUBLE_TOLERANCE);
-        assertEquals(0.9, ptdfSums.get(crac.getFlowCnec("cnec2basecase")), DOUBLE_TOLERANCE);
+        assertEquals(0.5, ptdfSums.get(crac.getFlowCnec("cnec1basecase")), DOUBLE_TOLERANCE); // abs(0.1 - 0.2) + abs(0.1 - 0.3) + abs(0.3 - 0.2) + abs(0.2 - 0.3) = 0.1 + 0.2 + 0.1 + 0.1
+        assertEquals(0.3, ptdfSums.get(crac.getFlowCnec("cnec2basecase")), DOUBLE_TOLERANCE); // abs(0.3 - 0.3) + abs(0.3 - 0.2) + abs(0.2 - 0.3) + abs(0.3 - 0.2) = 0 + 0.1 + 0.1 + 0.1
     }
 
     @Test
