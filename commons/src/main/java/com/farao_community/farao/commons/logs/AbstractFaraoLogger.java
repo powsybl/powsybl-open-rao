b59cbe984b47d2a2bf6c105d60dc92e25a29be7d
Peter Mitri <peter.mitri@rte-france.com>
2022-01-06 18:06:34 +0100

    header

--- commons/src/main/java/com/farao_community/farao/commons/logs/AbstractFaraoLogger.java
+++ commons/src/main/java/com/farao_community/farao/commons/logs/AbstractFaraoLogger.java
@@ -1,8 +1,18 @@
+/*
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package com.farao_community.farao.commons.logs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * @author Peter Mitri {@literal <peter.mitri at rte-france.com>}
+ */
 public abstract class AbstractFaraoLogger implements FaraoLogger {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
