5d4e0cfbcf6a5d626b7b5783b1db9c107da3f63a
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2022-01-18 16:49:17 +0100

    comment after review

--- ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/RangeActionFilter.java
+++ ra-optimisation/search-tree-rao/src/main/java/com/farao_community/farao/search_tree_rao/RangeActionFilter.java
@@ -262,7 +262,7 @@ class RangeActionFilter {
         }
     }
 
-    private int comparePotentialGain(RangeAction<?> ra1, RangeAction ra2, FlowCnec cnec, OptimizationResult optimizationResult) {
+    private int comparePotentialGain(RangeAction<?> ra1, RangeAction<?> ra2, FlowCnec cnec, OptimizationResult optimizationResult) {
         if (cnec == null) {
             return 0;
         }
@@ -272,7 +272,7 @@ class RangeActionFilter {
         return comparison != 0 ? comparison : orderRangeActionsRandomly(ra1, ra2);
     }
 
-    private Double computePotentialGain(RangeAction<?> rangeAction, FlowCnec cnec, OptimizationResult optimizationResult) {
+    private Double computePotentialGain(RangeAction<?> rangeAction, FlowCnec cnec, Optim?izationResult optimizationResult) {
         double directMargin = cnec.getUpperBound(Side.LEFT, Unit.MEGAWATT).orElse(Double.POSITIVE_INFINITY) - optimizationResult.getFlow(cnec, Unit.MEGAWATT);
         double oppositeMargin = optimizationResult.getFlow(cnec, Unit.MEGAWATT) - cnec.getLowerBound(Side.LEFT, Unit.MEGAWATT).orElse(Double.NEGATIVE_INFINITY);
         double sensi = optimizationResult.getSensitivityValue(cnec, rangeAction, Unit.MEGAWATT);
