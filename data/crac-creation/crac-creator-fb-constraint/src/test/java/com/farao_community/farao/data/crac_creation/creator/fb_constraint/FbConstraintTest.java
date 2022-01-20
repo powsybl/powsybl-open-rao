b6e93b78a330dfa61b05de0897b71b54a2d93e79
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2022-01-20 16:31:47 +0100

    fb-constraint CRAC creator (#573)

---
+++ data/crac-creation/crac-creator-fb-constraint/src/test/java/com/farao_community/farao/data/crac_creation/creator/fb_constraint/FbConstraintTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.data.crac_creation.creator.fb_constraint;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Baptiste Seguinot{@literal <baptiste.seguinot at rte-france.com>}
+ */
+public class FbConstraintTest {
+
+    @Test
+    public void testConstructor() {
+        FlowBasedConstraintDocument flowBasedConstraintDocument = Mockito.mock(FlowBasedConstraintDocument.class);
+        FbConstraint fbConstraint = new FbConstraint(flowBasedConstraintDocument, 20);
+
+        assertEquals("FlowBasedConstraintDocument", fbConstraint.getFormat());
+        assertEquals(20, fbConstraint.getFlowBasedDocumentVersion());
+        assertNotNull(fbConstraint.getDocument());
+    }
+}
