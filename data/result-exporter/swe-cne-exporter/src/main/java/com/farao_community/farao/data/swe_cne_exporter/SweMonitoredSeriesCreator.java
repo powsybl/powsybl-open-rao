5aabea4f59ecf2a335dc7c976471f2c6cacaeda7
EDWARDS Philippe <edwardsphi@gm0winl197.bureau.si.interne>
2022-05-24 09:20:10 +0200

    created shells for SWE CNE ConstraintSeries creators

---
+++ data/result-exporter/swe-cne-exporter/src/main/java/com/farao_community/farao/data/swe_cne_exporter/SweMonitoredSeriesCreator.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.data.swe_cne_exporter;
+
+import com.farao_community.farao.data.swe_cne_exporter.xsd.AreaIDString;
+import com.farao_community.farao.data.swe_cne_exporter.xsd.ESMPDateTimeInterval;
+import com.farao_community.farao.data.swe_cne_exporter.xsd.PartyIDString;
+import com.farao_community.farao.data.swe_cne_exporter.xsd.ResourceIDString;
+import org.threeten.extra.Interval;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+import static com.farao_community.farao.data.cne_exporter_commons.CneUtil.cutString;
+
+/**
+ * Generates MonitoredSeries for SWE CNE format
+ *
+ * @author Philippe Edwards {@literal <philippe.edwards at rte-france.com>}
+ */
+public class SweMonitoredSeriesCreator {
+}
