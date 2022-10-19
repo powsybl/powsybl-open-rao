90b5069baf58617b88f33f8e36b3b08ef58014fc
Peter <47452063+pet-mit@users.noreply.github.com>
2022-10-19 09:30:39 +0200

    Allow creating CIM twice (#678)

--- data/crac-creation/crac-creator-cim/src/main/java/com/farao_community/farao/data/crac_creation/creator/cim/crac_creator/CimCracCreator.java
+++ data/crac-creation/crac-creator-cim/src/main/java/com/farao_community/farao/data/crac_creation/creator/cim/crac_creator/CimCracCreator.java
@@ -22,6 +22,7 @@ import com.google.auto.service.AutoService;
 import com.powsybl.iidm.network.Network;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -45,7 +46,7 @@ public class CimCracCreator implements CracCreator<CimCrac, CimCracCreationConte
         // Set attributes
         this.crac = parameters.getCracFactory().create(cimCrac.getCracDocument().getMRID());
         this.network = network;
-        this.cimTimeSeries = cimCrac.getCracDocument().getTimeSeries();
+        this.cimTimeSeries = new ArrayList<>(cimCrac.getCracDocument().getTimeSeries());
         this.creationContext = new CimCracCreationContext(crac, offsetDateTime);
 
         // Get warning messages from parameters parsing
