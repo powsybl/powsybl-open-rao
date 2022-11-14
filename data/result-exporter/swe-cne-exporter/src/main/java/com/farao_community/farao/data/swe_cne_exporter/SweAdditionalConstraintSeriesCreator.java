a7946093a9c6ec0e5884758800f64bf3fc43817f
Godelaine <87479798+Godelaine@users.noreply.github.com>
2022-11-14 12:00:24 +0100

    Cne add angle monitoring (#684)
    * wip
    
    * wip
    
    * checkstyle
    
    * checkstyle bis
    
    * adapt exporter ut
    
    * add business warns
    
    * add UT for wrong instants
    
    * do not write null additionalConstraintSeries
    
    * handle AngleMonitoring divergence
    
    * post cne header possibilities merge
    
    * MR comments
    
    * add getCreatedAngleCnecId
    
    * add getCreatedAngleCnecId
    
    * significant digits

---
+++ data/result-exporter/swe-cne-exporter/src/main/java/com/farao_community/farao/data/swe_cne_exporter/SweAdditionalConstraintSeriesCreator.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.farao_community.farao.data.swe_cne_exporter;
+
+import com.farao_community.farao.commons.Unit;
+import com.farao_community.farao.data.crac_api.Contingency;
+import com.farao_community.farao.data.crac_api.Crac;
+import com.farao_community.farao.data.crac_api.Instant;
+import com.farao_community.farao.data.crac_api.cnec.AngleCnec;
+import com.farao_community.farao.data.crac_creation.creator.cim.crac_creator.CimCracCreationContext;
+import com.farao_community.farao.data.crac_creation.creator.cim.crac_creator.cnec.AngleCnecCreationContext;
+import com.farao_community.farao.data.swe_cne_exporter.xsd.AdditionalConstraintSeries;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.farao_community.farao.commons.logs.FaraoLoggerProvider.BUSINESS_WARNS;
+
+/**
+ * Generates AdditionalConstraintSeries for SWE CNE format
+ *
+ * @author Godelaine de Montmorillon {@literal <godelaine.demontmorillon at rte-france.com>}
+ */
+public class SweAdditionalConstraintSeriesCreator {
+    public static final String ANGLE_CNEC_BUSINESS_TYPE = "B87";
+    private final SweCneHelper sweCneHelper;
+    private final CimCracCreationContext cracCreationContext;
+
+    public SweAdditionalConstraintSeriesCreator(SweCneHelper sweCneHelper, CimCracCreationContext cracCreationContext) {
+        this.sweCneHelper = sweCneHelper;
+        this.cracCreationContext = cracCreationContext;
+    }
+
+    public List<AdditionalConstraintSeries> generateAdditionalConstraintSeries(Contingency contingency) {
+        List<AdditionalConstraintSeries> additionalConstraintSeriesList = new ArrayList<>();
+        if (Objects.isNull(sweCneHelper.getAngleMonitoringResult())) {
+            return additionalConstraintSeriesList;
+        }
+        List<AngleCnecCreationContext> sortedAngleCnecs = cracCreationContext.getAngleCnecCreationContexts().stream()
+                .filter(AngleCnecCreationContext::isImported)
+                .sorted(Comparator.comparing(AngleCnecCreationContext::getCreatedAngleCnecId))
+                .collect(Collectors.toList());
+        if (Objects.isNull(contingency)) {
+            sortedAngleCnecs.stream().filter(angleCnecCreationContext -> Objects.isNull(angleCnecCreationContext.getContingencyId()))
+                    .forEach(angleCnecCreationContext ->
+                BUSINESS_WARNS.warn("Preventive angle cnec {} will not be added to CNE file", angleCnecCreationContext.getNativeId()));
+        } else {
+            sortedAngleCnecs.stream()
+                    .filter(angleCnecCreationContext -> angleCnecCreationContext.getContingencyId().equals(contingency.getId()))
+                            .map(this::generateAdditionalConstraintSeries)
+                                    .filter(Objects::nonNull)
+                                            .forEach(additionalConstraintSeriesList::add);
+        }
+        return additionalConstraintSeriesList;
+    }
+
+    private AdditionalConstraintSeries generateAdditionalConstraintSeries(AngleCnecCreationContext angleCnecCreationContext) {
+        Crac crac = sweCneHelper.getCrac();
+        AngleCnec angleCnec = crac.getAngleCnec(angleCnecCreationContext.getCreatedAngleCnecId());
+        if (!angleCnec.getState().getInstant().equals(Instant.CURATIVE)) {
+            BUSINESS_WARNS.warn("{} angle cnec {} will not be added to CNE file", angleCnec.getState().getInstant(), angleCnecCreationContext.getNativeId());
+            return null;
+        }
+        AdditionalConstraintSeries additionalConstraintSeries = new AdditionalConstraintSeries();
+        additionalConstraintSeries.setMRID(angleCnecCreationContext.getCreatedAngleCnecId());
+        additionalConstraintSeries.setBusinessType(ANGLE_CNEC_BUSINESS_TYPE);
+        additionalConstraintSeries.setName(angleCnec.getName());
+        if (!sweCneHelper.getAngleMonitoringResult().isDivergent()) {
+            additionalConstraintSeries.setQuantityQuantity(BigDecimal.valueOf(sweCneHelper.getAngleMonitoringResult().getAngle(angleCnec, Unit.DEGREE)).setScale(1, RoundingMode.HALF_UP));
+        }
+        return additionalConstraintSeries;
+    }
+}
