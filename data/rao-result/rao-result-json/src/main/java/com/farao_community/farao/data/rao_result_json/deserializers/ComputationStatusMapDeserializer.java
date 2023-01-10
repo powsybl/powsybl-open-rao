95b43aa6a9dc75f200cc0aefa206c2695c792c08
demontmorillongod <godelaine.demontmorillon@rte-france.com>
2023-01-10 08:41:01 +0100

    new files copyright + author

--- data/rao-result/rao-result-json/src/main/java/com/farao_community/farao/data/rao_result_json/deserializers/ComputationStatusMapDeserializer.java
+++ data/rao-result/rao-result-json/src/main/java/com/farao_community/farao/data/rao_result_json/deserializers/ComputationStatusMapDeserializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -19,7 +19,8 @@ import java.io.IOException;
 import static com.farao_community.farao.data.rao_result_json.RaoResultJsonConstants.*;
 
 /**
- * @author Baptiste Seguinot {@literal <baptiste.seguinot at rte-france.com>}
+ * @author Philippe Edwards {@literal <philippe.edwards at rte-france.com>}
+ * @author Godelaine De-Montmorillon {@literal <godelaine.demontmorillon at rte-france.com>}
  */
 final class ComputationStatusMapDeserializer {
 
--- data/rao-result/rao-result-json/src/main/java/com/farao_community/farao/data/rao_result_json/serializers/ComputationStatusMapSerializer.java
+++ data/rao-result/rao-result-json/src/main/java/com/farao_community/farao/data/rao_result_json/serializers/ComputationStatusMapSerializer.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2021, RTE (http://www.rte-france.com)
+ *  Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  *  License, v. 2.0. If a copy of the MPL was not distributed with this
  *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -21,6 +21,7 @@ import static com.farao_community.farao.data.rao_result_json.RaoResultJsonConsta
 
 /**
  * @author Philippe Edwards {@literal <philippe.edwards at rte-france.com>}
+ * @author Godelaine De-Montmorillon {@literal <godelaine.demontmorillon at rte-france.com>}
  */
 final class ComputationStatusMapSerializer {
 
