72047fe8881b54a1009ea78767406d0473c41d64
GabrielPlante <43040976+GabrielPlante@users.noreply.github.com>
2023-09-13 08:53:04 +0200

    Csa 26 voltage monitoring update (#776)
    * [CSA-25] Implement voltage remedial actions in crac API
    
    * [CSA-25] Removed unused imports
    
    * [CSA-25] Fixed indentation level
    
    * [CSA-25] Fixed Unit Tests
    
    * [CSA-25] Fixed Linter Issues
    
    * [CSA-25] Fixed Linter Issues
    
    * [CSA-25] Fixed version rule for voltage constraint
    
    * [CSA-26] Added the new algorithm to include remedial action into the voltage monitoring
    
    * monitoring/voltage-monitoring/src/main/java/com/farao_community/farao/monitoring/voltage_monitoring/VoltageMonitoring.java
    
    * [CSA-26] Added unit tests
    
    * [CSA-26] Voltage monitoring update
    
    * [CSA-26] Fixed import
    
    * [CSA-26] Update test 1.2
    
    * CSA-26 Deleted unwanted result state
    
    * CSA-26 Added unit tests
    
    * CSA-26 All applied RA are now kept, fixed tests
    
    * CSA-26 Fixed test
    
    * [CSA-26] Added test 5
    
    * [CSA-26] Updated comment
    
    * [CSA-26] Fixed applied network action not being all kept
    
    * [CSA-26] Revert common-monitoring addition
    
    * [CSA-26] Reverted unwanted change inherited from CSA-25
    
    * [CSA-26] enhanced test
    
    * [CSA-26] Fixed test and specific behaviour
    
    * [CSA-26] removed useless test
    
    ---------
    
    Co-authored-by: Fabrice Buscaylet <fabrice.buscaylet@artelys.com>
    Co-authored-by: Gabriel Plante <gabriel.plante@artelys.com>
    Co-authored-by: PLANTE Gabriel <gabriel.plante_externe@rte-france.com>
    Co-authored-by: Peter <47452063+pet-mit@users.noreply.github.com>

--- monitoring/voltage-monitoring/src/main/java/com/farao_community/farao/monitoring/voltage_monitoring/VoltageMonitoringResult.java
+++ monitoring/voltage-monitoring/src/main/java/com/farao_community/farao/monitoring/voltage_monitoring/VoltageMonitoringResult.java
@@ -7,7 +7,10 @@
 
 package com.farao_community.farao.monitoring.voltage_monitoring;
 
+import com.farao_community.farao.commons.FaraoException;
+import com.farao_community.farao.data.crac_api.State;
 import com.farao_community.farao.data.crac_api.cnec.VoltageCnec;
+import com.farao_community.farao.data.crac_api.network_action.NetworkAction;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -23,7 +26,8 @@ public class VoltageMonitoringResult {
         SECURE,
         HIGH_VOLTAGE_CONSTRAINT,
         LOW_VOLTAGE_CONSTRAINT,
-        HIGH_AND_LOW_VOLTAGE_CONSTRAINTS;
+        HIGH_AND_LOW_VOLTAGE_CONSTRAINTS,
+        UNKNOWN;
 
         static Status fromConstraints(boolean highVoltageConstraints, boolean lowVoltageConstraints) {
             if (highVoltageConstraints && lowVoltageConstraints) {
@@ -41,31 +45,64 @@ public class VoltageMonitoringResult {
     private final Status status;
     private final Map<VoltageCnec, ExtremeVoltageValues> extremeVoltageValues;
     private final Set<VoltageCnec> constrainedElements;
+    private final Map<State, Set<NetworkAction>> appliedRas;
     private List<String> constraints;
 
-    public VoltageMonitoringResult(Map<VoltageCnec, ExtremeVoltageValues> extremeVoltageValues) {
+    public VoltageMonitoringResult(Map<VoltageCnec, ExtremeVoltageValues> extremeVoltageValues, Map<State, Set<NetworkAction>> appliedRas, Status status) {
         this.extremeVoltageValues = extremeVoltageValues;
         Set<VoltageCnec> tmpConstrainedElements = new HashSet<>();
+        this.constrainedElements = Collections.unmodifiableSet(tmpConstrainedElements);
+        for (Map.Entry<VoltageCnec, ExtremeVoltageValues> entry : extremeVoltageValues.entrySet()) {
+            if (entry.getKey().getThresholds().stream()
+                    .anyMatch(threshold -> threshold.limitsByMax() && entry.getValue().getMax() != null && entry.getValue().getMax() > threshold.max().orElseThrow())) {
+                tmpConstrainedElements.add(entry.getKey());
+            }
+            if (entry.getKey().getThresholds().stream()
+                    .anyMatch(threshold -> threshold.limitsByMin() && entry.getValue().getMin() != null && entry.getValue().getMin() < threshold.min().orElseThrow())) {
+                tmpConstrainedElements.add(entry.getKey());
+            }
+        }
+        this.appliedRas = appliedRas;
+        this.status = status;
+    }
+
+    public static Status getUnsecureStatus(Map<VoltageCnec, ExtremeVoltageValues> extremeVoltageValues) {
         boolean highVoltageConstraints = false;
         boolean lowVoltageConstraints = false;
         for (Map.Entry<VoltageCnec, ExtremeVoltageValues> entry : extremeVoltageValues.entrySet()) {
             if (entry.getKey().getThresholds().stream()
-                .anyMatch(threshold -> threshold.limitsByMax() && entry.getValue().getMax() != null && entry.getValue().getMax() > threshold.max().orElseThrow())) {
-                tmpConstrainedElements.add(entry.getKey());
+                    .anyMatch(threshold -> threshold.limitsByMax() && entry.getValue().getMax() != null && entry.getValue().getMax() > threshold.max().orElseThrow())) {
                 highVoltageConstraints = true;
             }
             if (entry.getKey().getThresholds().stream()
-                .anyMatch(threshold -> threshold.limitsByMin() && entry.getValue().getMin() != null && entry.getValue().getMin() < threshold.min().orElseThrow())) {
-                tmpConstrainedElements.add(entry.getKey());
+                    .anyMatch(threshold -> threshold.limitsByMin() && entry.getValue().getMin() != null && entry.getValue().getMin() < threshold.min().orElseThrow())) {
                 lowVoltageConstraints = true;
             }
         }
-        this.constrainedElements = Collections.unmodifiableSet(tmpConstrainedElements);
-        this.status = Status.fromConstraints(highVoltageConstraints, lowVoltageConstraints);
+        return Status.fromConstraints(highVoltageConstraints, lowVoltageConstraints);
     }
 
     public Set<VoltageCnec> getConstrainedElements() {
         return constrainedElements;
+    }
+
+    public Set<NetworkAction> getAppliedRas(State state) {
+        return appliedRas.getOrDefault(state, Collections.emptySet());
+    }
+
+    public Set<String> getAppliedRas(String stateId) {
+        Set<State> states = appliedRas.keySet().stream().filter(s -> s.getId().equals(stateId)).collect(Collectors.toSet());
+        if (states.isEmpty()) {
+            return Collections.emptySet();
+        } else if (states.size() > 1) {
+            throw new FaraoException(String.format("%s states share the same id : %s.", states.size(), stateId));
+        } else {
+            return appliedRas.get(states.iterator().next()).stream().map(NetworkAction::getId).collect(Collectors.toSet());
+        }
+    }
+
+    public Map<State, Set<NetworkAction>> getAppliedRas() {
+        return appliedRas;
     }
 
     public Status getStatus() {
