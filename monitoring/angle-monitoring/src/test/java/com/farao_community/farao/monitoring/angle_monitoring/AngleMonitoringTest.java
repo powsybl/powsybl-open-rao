60bcffe497c6e5d08411ad2d63affe95bda48309
GabrielPlante <43040976+GabrielPlante@users.noreply.github.com>
2023-09-27 15:54:56 +0200

    Csa 28 generalize angle monitoring (#808)
    * [CSA-28] Added bus handling for anglecnec
    
    * [CSA-28] Reorganized code
    
    * [CSA-28] Enhanced code, added unit test
    
    * Update monitoring/angle-monitoring/src/main/java/com/farao_community/farao/monitoring/angle_monitoring/AngleMonitoring.java
    
    Co-authored-by: Peter <47452063+pet-mit@users.noreply.github.com>
    
    * [CSA-24] Added check for angle cnec in unit test
    
    ---------
    
    Co-authored-by: PLANTE Gabriel <gabriel.plante_externe@rte-france.com>
    Co-authored-by: Peter <47452063+pet-mit@users.noreply.github.com>

--- monitoring/angle-monitoring/src/test/java/com/farao_community/farao/monitoring/angle_monitoring/AngleMonitoringTest.java
+++ monitoring/angle-monitoring/src/test/java/com/farao_community/farao/monitoring/angle_monitoring/AngleMonitoringTest.java
@@ -226,6 +226,18 @@ class AngleMonitoringTest {
     }
 
     @Test
+    void testAngleCnecOnBus() {
+        setUpCracFactory("network.xiidm");
+        crac.newContingency().withId("coL1").withNetworkElement("L2").add();
+        acCur1 = addAngleCnec("acCur1", Instant.CURATIVE, "coL1", network.getBusView().getBus("VL1_0").getId(), "VL2", -8., null);
+
+        runAngleMonitoring();
+        assertEquals(1, angleMonitoringResult.getAngleCnecsWithAngle().size());
+        assertEquals("acCur1", angleMonitoringResult.getAngleCnecsWithAngle().stream().findFirst().orElseThrow().getId());
+        assertTrue(angleMonitoringResult.isSecure());
+    }
+
+    @Test
     void testCracCim() {
         setUpCimCrac("/CIM_21_7_1_AngMon.xml", OffsetDateTime.parse("2021-04-02T05:00Z"), new CracCreationParameters());
         assertEquals(2, crac.getAngleCnecs().size());
