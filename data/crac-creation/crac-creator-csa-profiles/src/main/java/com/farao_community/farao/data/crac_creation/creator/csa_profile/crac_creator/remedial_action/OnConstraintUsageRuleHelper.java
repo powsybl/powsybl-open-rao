ced8e1b09ba50dc49f584c8927e84c35c2aeaaef
benrejebmoh <48012306+benrejebmoh@users.noreply.github.com>
2023-10-17 17:26:49 +0200

    [CSA-11] Import OnConstraint Remedial Actions (#790)
    * first working version of csa 10
    
    * add author and copyright for new classes
    
    * change the logic of reading static property ranges -> linked to subclass (tapposition, injectionsetpoint, ..) and not the parent gridstatealteration
    
    * minor: changing variable names
    
    * changes after review
    
    * WIP csa 11: fist draft version
    
    * update with master manual merge
    
    * working version of csa 11 OnFlowConstraint part only
    
    * merge with master manuel fixes
    
    * handling angle and voltage cnecs
    
    * add unit tests for angle and voltage cnecs
    
    * add headers for OnConstraintUsageRuleHelper.java and replace 2 TODO's by FIXME
    
    * enhance readability , throw exception for unsupported values
    
    * fix checkstyle after refacto
    
    * resolve discussions after review
    
    * fix unit test after merge with master (usage rules as set)
    
    * changes after second review

---
+++ data/crac-creation/crac-creator-csa-profiles/src/main/java/com/farao_community/farao/data/crac_creation/creator/csa_profile/crac_creator/remedial_action/OnConstraintUsageRuleHelper.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.data.crac_creation.creator.csa_profile.crac_creator.remedial_action;
+
+import com.farao_community.farao.commons.FaraoException;
+import com.farao_community.farao.data.crac_creation.creator.csa_profile.crac_creator.CsaProfileConstants;
+import com.farao_community.farao.data.crac_creation.creator.csa_profile.crac_creator.cnec.CsaProfileCnecCreationContext;
+import com.powsybl.triplestore.api.PropertyBag;
+import com.powsybl.triplestore.api.PropertyBags;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author Mohamed Ben-rejeb {@literal <mohamed.ben-rejeb at rte-france.com>}
+ */
+public class OnConstraintUsageRuleHelper {
+    private final Set<CsaProfileCnecCreationContext> csaProfileCnecCreationContexts;
+
+    private final Set<String> importedCnecsCombinableWithRas = new HashSet<>();
+    private final Map<String, Set<String>> excludedCnecsByRemedialAction = new HashMap<>();
+    private final Map<String, Set<String>> includedCnecsByRemedialAction = new HashMap<>();
+    private final Map<String, Set<String>> consideredCnecsElementsByRemedialAction = new HashMap<>();
+
+    public OnConstraintUsageRuleHelper(Set<CsaProfileCnecCreationContext> csaProfileCnecCreationContexts, PropertyBags assessedElements, PropertyBags assessedElementsWithRemedialAction) {
+        this.csaProfileCnecCreationContexts = csaProfileCnecCreationContexts;
+        readAssessedElementsCombinableWithRemedialActions(assessedElements);
+        readAssessedElementsWithRemedialAction(assessedElementsWithRemedialAction);
+    }
+
+    private void readAssessedElementsCombinableWithRemedialActions(PropertyBags assessedElements) {
+        List<String> nativeIdsOfCnecsCombinableWithRas = assessedElements.stream()
+                .filter(element -> element.getId(CsaProfileConstants.REQUEST_ASSESSED_ELEMENT_IS_COMBINABLE_WITH_REMEDIAL_ACTION) != null && Boolean.parseBoolean(element.getId(CsaProfileConstants.REQUEST_ASSESSED_ELEMENT_IS_COMBINABLE_WITH_REMEDIAL_ACTION)))
+                .map(element -> removePrefix(element.get(CsaProfileConstants.REQUEST_ASSESSED_ELEMENT))).collect(Collectors.toList());
+        Map<String, Set<String>> nativeToFaraoCnecIdsCombinableWithRas = getImportedCnecsNativeIdsToFaraoIds();
+        nativeToFaraoCnecIdsCombinableWithRas.keySet().retainAll(nativeIdsOfCnecsCombinableWithRas);
+        nativeToFaraoCnecIdsCombinableWithRas.values().stream().flatMap(Set::stream).forEach(importedCnecsCombinableWithRas::add);
+    }
+
+    private void readAssessedElementsWithRemedialAction(PropertyBags assessedElementsWithRemedialAction) {
+        assessedElementsWithRemedialAction.stream().filter(this::checkNormalEnabled).forEach(propertyBag -> {
+            String combinationConstraintKind = propertyBag.get(CsaProfileConstants.COMBINATION_CONSTRAINT_KIND);
+            String remedialActionId = removePrefix(propertyBag.get(CsaProfileConstants.REQUEST_REMEDIAL_ACTION));
+            String assessedElementId = removePrefix(propertyBag.get(CsaProfileConstants.REQUEST_ASSESSED_ELEMENT));
+
+            // TODO handle exception if getImportedCnecsNativeIdsToFaraoIds().get(assessedElementId) -> nullpointer
+            Set<String> faraoCnecIds = getImportedCnecsNativeIdsToFaraoIds().get(assessedElementId);
+
+            if (combinationConstraintKind.equals(CsaProfileConstants.ElementCombinationConstraintKind.EXCLUDED.toString())) {
+                excludedCnecsByRemedialAction
+                        .computeIfAbsent(remedialActionId, k -> new HashSet<>())
+                        .addAll(faraoCnecIds);
+            } else if (combinationConstraintKind.equals(CsaProfileConstants.ElementCombinationConstraintKind.INCLUDED.toString())) {
+                includedCnecsByRemedialAction
+                        .computeIfAbsent(remedialActionId, k -> new HashSet<>())
+                        .addAll(faraoCnecIds);
+            } else if (combinationConstraintKind.equals(CsaProfileConstants.ElementCombinationConstraintKind.CONSIDERED.toString())) {
+                consideredCnecsElementsByRemedialAction
+                        .computeIfAbsent(remedialActionId, k -> new HashSet<>())
+                        .addAll(faraoCnecIds);
+            } else {
+                throw new FaraoException(String.format("Unsupported combinationConstraintKind of AssessedElementsWithRemedialAction with id %s, only  ['INCLUDED', 'EXCLUDED', 'CONSIDERED'] are supported ", propertyBag.get(CsaProfileConstants.REQUEST_ASSESSED_ELEMENT_WITH_REMEDIAL_ACTION)));
+            }
+        });
+    }
+
+    private boolean checkNormalEnabled(PropertyBag propertyBag) {
+        Optional<String> normalEnabledOpt = Optional.ofNullable(propertyBag.get(CsaProfileConstants.NORMAL_ENABLED));
+        return normalEnabledOpt.isEmpty() || Boolean.parseBoolean(normalEnabledOpt.get());
+    }
+
+    private Map<String, Set<String>> getImportedCnecsNativeIdsToFaraoIds() {
+        return csaProfileCnecCreationContexts.stream()
+                .filter(CsaProfileCnecCreationContext::isImported)
+                .collect(Collectors.groupingBy(
+                        CsaProfileCnecCreationContext::getNativeId,
+                        Collectors.mapping(CsaProfileCnecCreationContext::getCnecId, Collectors.toSet())
+                ));
+    }
+
+    private String removePrefix(String mridWithPrefix) {
+        return mridWithPrefix.substring(mridWithPrefix.lastIndexOf("_") + 1);
+    }
+
+    public Set<String> getImportedCnecsCombinableWithRas() {
+        return importedCnecsCombinableWithRas;
+    }
+
+    public Map<String, Set<String>> getExcludedCnecsByRemedialAction() {
+        return this.excludedCnecsByRemedialAction;
+    }
+
+    public Map<String, Set<String>> getIncludedCnecsByRemedialAction() {
+        return this.includedCnecsByRemedialAction;
+    }
+
+    public Map<String, Set<String>> getConsideredCnecsElementsByRemedialAction() {
+        return this.consideredCnecsElementsByRemedialAction;
+    }
+}
