d3d4fdc303e0633c8b5fb455535da1a5aa875705
Hugo SCHINDLER <hugo.schindler@rte-france.com>
2023-10-27 15:25:46 +0200

    platfrom independant exception message
    Signed-off-by: Hugo SCHINDLER <hugo.schindler@rte-france.com>

--- data/refprog/refprog-xml-importer/src/test/java/com/farao_community/farao/data/refprog/refprog_xml_importer/RefProgImporterTest.java
+++ data/refprog/refprog-xml-importer/src/test/java/com/farao_community/farao/data/refprog/refprog_xml_importer/RefProgImporterTest.java
@@ -33,7 +33,8 @@ class RefProgImporterTest {
     void testUnexistantFile() {
         Path path = Paths.get("/refProg_12nodes_doesntexist.xml");
         FaraoException exception = assertThrows(FaraoException.class, () -> RefProgImporter.importRefProg(path, offsetDateTime));
-        assertTrue(exception.getMessage().contains("java.io.FileNotFoundException: /refProg_12nodes_doesntexist.xml")); // platform dependant exception message
+        assertTrue(exception.getMessage().contains("java.io.FileNotFoundException")); // platform dependant exception message
+        assertTrue(exception.getMessage().contains("refProg_12nodes_doesntexist.xml")); // platform dependant exception message
     }
 
     @Test
