242e29bf7dd2155b03d4c0b4fa112c5daa011829
Gabriel Plante <gabriel.plante@artelys.com>
2023-11-02 11:12:18 +0100

    [CSA-35] Added class documentation

--- data/crac/crac-impl/src/main/java/com/farao_community/farao/data/crac_impl/StandardRangeActionUtils.java
+++ data/crac/crac-impl/src/main/java/com/farao_community/farao/data/crac_impl/StandardRangeActionUtils.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 /**
  * @author Gabriel Plante {@literal <gabriel.plante_externe at rte-france.com}
+ * This class is here to avoid code duplication
+ * and because adding another base class to standard range actions would be too much class depth
  */
 public final class StandardRangeActionUtils {
 
