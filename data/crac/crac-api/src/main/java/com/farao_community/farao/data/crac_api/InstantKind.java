e02f4793ac1e32af5e7926926fbb8e7116d3640e
Hugo SCHINDLER <hugo.schindler@rte-france.com>
2023-11-02 13:03:44 +0100

    change authors
    Signed-off-by: Hugo SCHINDLER <hugo.schindler@rte-france.com>

--- data/crac/crac-api/src/main/java/com/farao_community/farao/data/crac_api/InstantKind.java
+++ data/crac/crac-api/src/main/java/com/farao_community/farao/data/crac_api/InstantKind.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -13,8 +13,8 @@ import com.farao_community.farao.data.crac_api.cnec.Cnec;
  * Enum representing the instants at which {@link Cnec} can be monitored and
  * {@link RemedialAction} applied.
  *
- * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
- * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
+ * @author Hugo Schindler {@literal <hugo.schindler at rte-france.com>}
+ * @author Philippe Edwards {@literal <philippe.edwards at rte-france.com>}
  */
 public enum InstantKind {
     PREVENTIVE,
--- data/crac/crac-impl/src/test/java/com/farao_community/farao/data/crac_impl/InstantImplTest.java
+++ data/crac/crac-impl/src/test/java/com/farao_community/farao/data/crac_impl/InstantImplTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -17,7 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
+ * @author Hugo Schindler {@literal <hugo.schindler at rte-france.com>}
+ * @author Philippe Edwards {@literal <philippe.edwards at rte-france.com>}
  */
 
 class InstantImplTest {
