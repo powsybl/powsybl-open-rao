9f869e09223407f2cadd9494bda73607f41f9d87
benrejebmoh <48012306+benrejebmoh@users.noreply.github.com>
2023-11-23 16:16:06 +0100

    Enhance RaoResult with Monitoring results (#841)
    * enhance RaoResult with Angle and Voltage Monitoring results
    
    * enhance RaoResult with Angle and Voltage Monitoring results
    
    * fix previous commit
    
    * enhance unit test of AbstractRaoResultClone.java
    
    * add test coverage for AbstractRaoResultClone.java
    
    * changes after review
    
    * changes after review
    
    * minor changes after second review

--- monitoring/voltage-monitoring/src/main/java/com/farao_community/farao/monitoring/voltage_monitoring/VoltageMonitoring.java
+++ monitoring/voltage-monitoring/src/main/java/com/farao_community/farao/monitoring/voltage_monitoring/VoltageMonitoring.java
@@ -50,8 +50,17 @@ public class VoltageMonitoring {
 
     /**
      * Main function : runs VoltageMonitoring computation on all VoltageCnecs defined in the CRAC.
+     * Returns an RaoResult enhanced with VoltageMonitoringResult
+     */
+    public RaoResult runAndUpdateRaoResult(String loadFlowProvider, LoadFlowParameters loadFlowParameters, int numberOfLoadFlowsInParallel) {
+        return new RaoResultWithVoltageMonitoring(raoResult, run(loadFlowProvider, loadFlowParameters, numberOfLoadFlowsInParallel));
+    }
+
+    /**
+     * Main function : runs VoltageMonitoring computation on all VoltageCnecs defined in the CRAC.
      * Returns an VoltageMonitoringResult
      */
+    @Deprecated
     public VoltageMonitoringResult run(String loadFlowProvider, LoadFlowParameters loadFlowParameters, int numberOfLoadFlowsInParallel) {
         BUSINESS_LOGS.info("----- Voltage monitoring [start]");
         stateSpecificResults = new ArrayList<>();
