94ba67100b308d84dae0b0c5982d2c4392f8e183
belthlemar <martin.belthle@rte-france.com>
2023-11-30 18:33:25 +0100

    add todo comment because the code does not work ...

--- data/crac-creation/crac-creator-csa-profiles/src/test/java/com/farao_community/farao/data/crac_creation/creator/csa_profile/crac_creator/CsaProfileCracFilteringTest.java
+++ data/crac-creation/crac-creator-csa-profiles/src/test/java/com/farao_community/farao/data/crac_creation/creator/csa_profile/crac_creator/CsaProfileCracFilteringTest.java
@@ -1,4 +1,15 @@
 package com.farao_community.farao.data.crac_creation.creator.csa_profile.crac_creator;
 
+import org.junit.jupiter.api.Test;
+
+import static com.farao_community.farao.data.crac_creation.creator.csa_profile.crac_creator.CsaProfileCracCreationTestUtil.getCsaCracCreationContext;
+
 public class CsaProfileCracFilteringTest {
+
+    @Test
+    void testCracCreatorFiltersOutBadTimeStamps() {
+        CsaProfileCracCreationContext cracCreationContext = getCsaCracCreationContext("/CSA_82_FilterBadTS.zip");
+        cracCreationContext.getCrac();
+        // todo: It doesn't work and I don't know why ...
+    }
 }
