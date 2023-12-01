7039cce4b8c7209cbfa44c7cf71d7b44a5e619ae
belthlemar <martin.belthle@rte-france.com>
2023-12-01 16:28:55 +0100

    checks crac content

--- data/crac-creation/crac-creator-csa-profiles/src/test/java/com/farao_community/farao/data/crac_creation/creator/csa_profile/crac_creator/CsaProfileCracFilteringTest.java
+++ data/crac-creation/crac-creator-csa-profiles/src/test/java/com/farao_community/farao/data/crac_creation/creator/csa_profile/crac_creator/CsaProfileCracFilteringTest.java
@@ -3,6 +3,7 @@ package com.farao_community.farao.data.crac_creation.creator.csa_profile.crac_cr
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
 import com.farao_community.farao.commons.logs.RaoBusinessWarns;
+import com.farao_community.farao.data.crac_api.Crac;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 import static com.farao_community.farao.data.crac_creation.creator.csa_profile.crac_creator.CsaProfileCracCreationTestUtil.getCsaCracCreationContext;
 import static com.farao_community.farao.data.crac_creation.creator.csa_profile.crac_creator.CsaProfileCracCreationTestUtil.getLogs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CsaProfileCracFilteringTest {
 
@@ -18,12 +20,22 @@ class CsaProfileCracFilteringTest {
         ListAppender<ILoggingEvent> listAppender = getLogs(RaoBusinessWarns.class);
         List<ILoggingEvent> logsList = listAppender.list;
 
-        CsaProfileCracCreationContext cracCreationContext = getCsaCracCreationContext("/CSA_82_FilterBadTS.zip");
-        assertEquals(5, logsList.size());
+        CsaProfileCracCreationContext context = getCsaCracCreationContext("/CSA_82_FilterBadTS.zip");
+        Crac crac = context.getCrac();
+
+        // checks files filtering
+        assertEquals(7, logsList.size());
         assert logsList.get(0).getFormattedMessage().contains("CSA_RA_outdated_remove.xml");
         assert logsList.get(1).getFormattedMessage().contains("CSA_RA_bad_end_date_remove.xml");
         assert logsList.get(2).getFormattedMessage().contains("CSA_RA_bad_start_date_remove.xml");
-        assert logsList.get(3).getFormattedMessage().contains("CSA_RA_start_date_after_end_date_remove.xml");
-        assert logsList.get(4).getFormattedMessage().contains("CSA_RA_not_yet_valid_remove.xml");
+        assert logsList.get(3).getFormattedMessage().contains("CSA_RA_missing_end_date_remove.xml");
+        assert logsList.get(4).getFormattedMessage().contains("CSA_RA_start_date_after_end_date_remove.xml");
+        assert logsList.get(5).getFormattedMessage().contains("CSA_RA_missing_start_date_remove.xml");
+        assert logsList.get(6).getFormattedMessage().contains("CSA_RA_not_yet_valid_remove.xml");
+
+        // checks crac content
+        assertTrue(crac.getRemedialActions().isEmpty());
+        assertEquals(1, crac.getContingencies().size());
+        assertEquals("RTE_co1_fr2_fr3_1", crac.getContingencies().stream().iterator().next().getName());
     }
 }
