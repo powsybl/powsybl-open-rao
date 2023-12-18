bcf57080231573ddf11d637e80193b99f6c12aac
Philippe Edwards <philippe.edwards@rte-france.com>
2023-12-18 13:44:27 +0100

    include virtual hubs

--- data/virtual-hubs/virtual-hubs-network-extension/src/main/java/com/farao_community/farao/virtual_hubs/network_extension/AssignedVirtualHubXmlSerializer.java
+++ data/virtual-hubs/virtual-hubs-network-extension/src/main/java/com/farao_community/farao/virtual_hubs/network_extension/AssignedVirtualHubXmlSerializer.java
@@ -7,50 +7,49 @@
 package com.farao_community.farao.virtual_hubs.network_extension;
 
 import com.google.auto.service.AutoService;
-import com.powsybl.commons.extensions.AbstractExtensionXmlSerializer;
-import com.powsybl.commons.extensions.ExtensionXmlSerializer;
-import com.powsybl.commons.xml.XmlReaderContext;
-import com.powsybl.commons.xml.XmlWriterContext;
+import com.powsybl.commons.extensions.AbstractExtensionSerDe;
+import com.powsybl.commons.extensions.ExtensionSerDe;
+import com.powsybl.commons.io.DeserializerContext;
+import com.powsybl.commons.io.SerializerContext;
 import com.powsybl.iidm.network.Injection;
 
-import javax.xml.stream.XMLStreamException;
 import java.util.Objects;
 
 /**
  * @author Baptiste Seguinot {@literal <baptiste.seguinot@rte-france.com>}
  */
-@AutoService(ExtensionXmlSerializer.class)
-public class AssignedVirtualHubXmlSerializer<T extends Injection<T>> extends AbstractExtensionXmlSerializer<T, AssignedVirtualHub<T>> {
+@AutoService(ExtensionSerDe.class)
+public class AssignedVirtualHubXmlSerializer<T extends Injection<T>> extends AbstractExtensionSerDe<T, AssignedVirtualHub<T>> {
 
     public AssignedVirtualHubXmlSerializer() {
-        super("assignedVirtualHub", "network", AssignedVirtualHub.class, false,
+        super("assignedVirtualHub", "network", AssignedVirtualHub.class,
             "assignedVirtualHub.xsd",
             "https://farao-community.github.io/schema/iidm/ext/virtual_hub_extension/1_0", "farao");
     }
 
     @Override
-    public void write(AssignedVirtualHub<T> assignedVirtualHub, XmlWriterContext context) throws XMLStreamException {
+    public void write(AssignedVirtualHub<T> assignedVirtualHub, SerializerContext context) {
         if (!Objects.isNull(assignedVirtualHub.getCode())) {
-            context.getExtensionsWriter().writeAttribute("code", assignedVirtualHub.getCode());
+            context.getWriter().writeStringAttribute("code", assignedVirtualHub.getCode());
         }
-        context.getExtensionsWriter().writeAttribute("eic", assignedVirtualHub.getEic());
-        context.getExtensionsWriter().writeAttribute("isMcParticipant", Boolean.toString(assignedVirtualHub.isMcParticipant()));
+        context.getWriter().writeStringAttribute("eic", assignedVirtualHub.getEic());
+        context.getWriter().writeStringAttribute("isMcParticipant", Boolean.toString(assignedVirtualHub.isMcParticipant()));
 
         if (!Objects.isNull(assignedVirtualHub.getNodeName())) {
-            context.getExtensionsWriter().writeAttribute("nodeName", assignedVirtualHub.getNodeName());
+            context.getWriter().writeStringAttribute("nodeName", assignedVirtualHub.getNodeName());
         }
         if (!Objects.isNull(assignedVirtualHub.getRelatedMa())) {
-            context.getExtensionsWriter().writeAttribute("relatedMa", assignedVirtualHub.getRelatedMa());
+            context.getWriter().writeStringAttribute("relatedMa", assignedVirtualHub.getRelatedMa());
         }
     }
 
     @Override
-    public AssignedVirtualHub<T> read(T extendable, XmlReaderContext context) {
-        String code = context.getReader().getAttributeValue(null, "code");
-        String eic = context.getReader().getAttributeValue(null, "eic");
-        String isMcParticipantAsString = context.getReader().getAttributeValue(null, "isMcParticipant");
-        String nodeName = context.getReader().getAttributeValue(null, "nodeName");
-        String relatedMa = context.getReader().getAttributeValue(null, "relatedMa");
+    public AssignedVirtualHub<T> read(T extendable, DeserializerContext context) {
+        String code = context.getReader().readStringAttribute("code");
+        String eic = context.getReader().readStringAttribute("eic");
+        String isMcParticipantAsString = context.getReader().readStringAttribute("isMcParticipant");
+        String nodeName = context.getReader().readStringAttribute("nodeName");
+        String relatedMa = context.getReader().readStringAttribute("relatedMa");
 
         boolean isMcParticipant = false;
         if (isMcParticipantAsString.equals(Boolean.toString(true))) {
