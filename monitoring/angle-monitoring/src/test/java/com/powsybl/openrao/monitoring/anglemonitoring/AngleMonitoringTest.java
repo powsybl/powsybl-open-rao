85fb5a4e00abba2820bc2eba04370f6ba070eda5
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2024-02-26 17:32:36 +0100

    Cleaning up, adding forgotten header in new class file
    Signed-off-by: SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>

--- monitoring/angle-monitoring/src/test/java/com/powsybl/openrao/monitoring/anglemonitoring/AngleMonitoringTest.java
+++ monitoring/angle-monitoring/src/test/java/com/powsybl/openrao/monitoring/anglemonitoring/AngleMonitoringTest.java
@@ -278,14 +278,4 @@ class AngleMonitoringTest {
             "AngleCnec AngleCnec1 (with importing network element _d77b61ef-61aa-4b22-95f6-b56ca080788d and exporting network element _8d8a82ba-b5b0-4e94-861a-192af055f2b8) at state Co-1 - curative has an angle of 5°."
         ));
     }
-
-    @Test
-    void testCracCimNoValidPositions() {
-        setUpCimCrac("/CIM_21_7_1_AngMon.xml", OffsetDateTime.parse("2021-04-01T22:00Z"), new CracCreationParameters());
-        assertEquals(0, crac.getContingencies().size());
-        assertEquals(0, crac.getFlowCnecs().size());
-        assertEquals(0, crac.getRemedialActions().size());
-        assertEquals(0, crac.getAngleCnecs().size());
-    }
-
 }
