0e63b45e3830496638f52613c3703fc04fc7e862
Thomas Bouquet <thomas.bouquet@rte-france.com>
2024-04-29 11:52:41 +0200

    Add author and copyright
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- data/crac/crac-impl/src/main/java/com/powsybl/openrao/data/cracimpl/AbstractOnConstraintUsageRule.java
+++ data/crac/crac-impl/src/main/java/com/powsybl/openrao/data/cracimpl/AbstractOnConstraintUsageRule.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package com.powsybl.openrao.data.cracimpl;
 
 import com.powsybl.openrao.data.cracapi.Instant;
@@ -5,6 +12,9 @@ import com.powsybl.openrao.data.cracapi.State;
 import com.powsybl.openrao.data.cracapi.cnec.Cnec;
 import com.powsybl.openrao.data.cracapi.usagerule.UsageMethod;
 
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
 public abstract class AbstractOnConstraintUsageRule<T extends Cnec<?>> extends AbstractUsageRule {
     protected Instant instant;
     protected T cnec;
