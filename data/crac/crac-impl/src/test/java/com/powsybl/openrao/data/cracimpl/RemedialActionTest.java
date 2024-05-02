powsybl/powsybl-open-rao

commit bcd6403c323b75ee1b1ede589d2ac19060fe058f

Accelerate state tree init (#979)
* precompute usage methods per state and instant in abstractRemedialAction

Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>

* cleaned up code

Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>

* revert changes to pom

Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>

* added tests

Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>

* fix tests

Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>

---------

Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>
Co-authored-by: Philippe Edwards <philippe.edwards@rte-france.com>

data/crac/crac-impl/src/main/java/com/powsybl/openrao/data/cracimpl/AbstractRemedialAction.java
@@ -8,6 +8,7 @@
 package com.powsybl.openrao.data.cracimpl;
 
 import com.powsybl.openrao.commons.OpenRaoException;
+import com.powsybl.openrao.data.cracapi.Instant;
 import com.powsybl.openrao.data.cracapi.RemedialAction;
 import com.powsybl.openrao.data.cracapi.State;
 import com.powsybl.openrao.data.cracapi.cnec.Cnec;
@@ -16,10 +17,7 @@ import com.powsybl.openrao.data.cracapi.usagerule.*;
 import com.powsybl.iidm.network.Country;
 import com.powsybl.iidm.network.Network;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -31,6 +29,9 @@ public abstract class AbstractRemedialAction<I extends RemedialAction<I>> extend
     protected String operator;
     protected Set<UsageRule> usageRules;
     protected Integer speed;
+    private boolean computedUsageMethods = false;
+    private Map<State, UsageMethod> usageMethodPerState;
+    private Map<Instant, UsageMethod> usageMethodPerInstant;
 
     protected AbstractRemedialAction(String id, String name, String operator, Set<UsageRule> usageRules, Integer speed) {
         super(id, name);
@@ -40,6 +41,7 @@ public abstract class AbstractRemedialAction<I extends RemedialAction<I>> extend
     }
 
     void addUsageRule(UsageRule usageRule) {
+        computedUsageMethods = false;
         this.usageRules.add(usageRule);
     }
 
@@ -65,10 +67,59 @@ public abstract class AbstractRemedialAction<I extends RemedialAction<I>> extend
 
     @Override
     public UsageMethod getUsageMethod(State state) {
-        Set<UsageMethod> usageMethods = usageRules.stream()
-            .map(usageRule -> usageRule.getUsageMethod(state))
-            .collect(Collectors.toSet());
-        return UsageMethod.getStrongestUsageMethod(usageMethods);
+        if (!computedUsageMethods) {
+            computeUsageMethodPerStateAndInstant();
+            computedUsageMethods = true;
+        }
+        if (usageMethodPerState.getOrDefault(state, UsageMethod.UNDEFINED).equals(usageMethodPerInstant.getOrDefault(state.getInstant(), UsageMethod.UNDEFINED))) {
+            return usageMethodPerInstant.getOrDefault(state.getInstant(), UsageMethod.UNDEFINED);
+        }
+        return UsageMethod.getStrongestUsageMethod(Set.of(
+            usageMethodPerState.getOrDefault(state, UsageMethod.UNDEFINED),
+            usageMethodPerInstant.getOrDefault(state.getInstant(), UsageMethod.UNDEFINED)));
+    }
+
+    private void computeUsageMethodPerStateAndInstant() {
+        usageMethodPerState = new HashMap<>();
+        usageMethodPerInstant = new HashMap<>();
+
+        for (UsageRule usageRule : usageRules) {
+            if (usageRule.getInstant().isPreventive()) {
+                updateMapWithValue(usageMethodPerInstant, usageRule.getInstant(), usageRule.getUsageMethod());
+            } else if (usageRule instanceof OnFlowConstraint ofc) {
+                State state = ofc.getFlowCnec().getState();
+                updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+            } else if (usageRule instanceof OnAngleConstraint oac) {
+                State state = oac.getAngleCnec().getState();
+                updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+            } else if (usageRule instanceof OnVoltageConstraint ovc) {
+                State state = ovc.getVoltageCnec().getState();
+                updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+            } else if (usageRule instanceof OnContingencyState ocs) {
+                State state = ocs.getState();
+                updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+            } else if (usageRule instanceof OnFlowConstraintInCountry || usageRule instanceof OnInstant) {
+                updateMapWithValue(usageMethodPerInstant, usageRule.getInstant(), usageRule.getUsageMethod());
+            } else {
+                throw new OpenRaoException(String.format("Usage rule of type %s is not implemented yet.", usageRule.getClass().getName()));
+            }
+        }
+    }
+
+    private void updateMapWithValue(Map<Instant, UsageMethod> map, Instant key, UsageMethod value) {
+        if (!map.containsKey(key)) {
+            map.put(key, value);
+        } else if (!value.equals(map.get(key))) {
+            map.put(key, UsageMethod.getStrongestUsageMethod(Set.of(map.get(key), value)));
+        }
+    }
+
+    private void updateMapWithValue(Map<State, UsageMethod> map, State key, UsageMethod value) {
+        if (!map.containsKey(key)) {
+            map.put(key, value);
+        } else if (!value.equals(map.get(key))) {
+            map.put(key, UsageMethod.getStrongestUsageMethod(Set.of(map.get(key), value)));
+        }
     }
 
     /**
@@ -107,9 +158,9 @@ public abstract class AbstractRemedialAction<I extends RemedialAction<I>> extend
 
     private static boolean isCnecInCountry(Cnec<?> cnec, Country country, Network network) {
         return cnec.getLocation(network).stream()
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .anyMatch(cnecCountry -> cnecCountry.equals(country));
+            .filter(Optional::isPresent)
+            .map(Optional::get)
+            .anyMatch(cnecCountry -> cnecCountry.equals(country));
     }
 
     @Override
@@ -122,8 +173,8 @@ public abstract class AbstractRemedialAction<I extends RemedialAction<I>> extend
         }
         AbstractRemedialAction<?> remedialAction = (AbstractRemedialAction<?>) o;
         return super.equals(remedialAction)
-                && new HashSet<>(usageRules).equals(new HashSet<>(remedialAction.getUsageRules()))
-                && (operator != null && operator.equals(remedialAction.operator) || operator == null && remedialAction.operator == null);
+            && new HashSet<>(usageRules).equals(new HashSet<>(remedialAction.getUsageRules()))
+            && (operator != null && operator.equals(remedialAction.operator) || operator == null && remedialAction.operator == null);
     }
 
     @Override

data/crac/crac-impl/src/test/java/com/powsybl/openrao/data/cracimpl/RemedialActionTest.java
@@ -1,0 +1,99 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.openrao.data.cracimpl;
+
+import com.powsybl.iidm.network.Country;
+import com.powsybl.openrao.data.cracapi.Instant;
+import com.powsybl.openrao.data.cracapi.State;
+import com.powsybl.openrao.data.cracapi.cnec.FlowCnec;
+import com.powsybl.openrao.data.cracapi.usagerule.UsageMethod;
+import com.powsybl.openrao.data.cracapi.usagerule.UsageRule;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Collections;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Philippe Edwards {@literal <philippe.edwards at rte-france.com>}
+ */
+class RemedialActionTest {
+    @Test
+    void testNoUsageRulesShouldReturnUndefined() {
+        State state = Mockito.mock(State.class);
+        Set<UsageRule> usageRules = Collections.emptySet();
+        AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
+        assertEquals(UsageMethod.UNDEFINED, ra.getUsageMethod(state));
+    }
+
+    @Test
+    void testOnlyOneOnInstantUsageRule() {
+        State state = Mockito.mock(State.class);
+        Instant instant = Mockito.mock(Instant.class);
+        Mockito.doReturn(instant).when(state).getInstant();
+        Set<UsageRule> usageRules = Set.of(new OnInstantImpl(UsageMethod.AVAILABLE, instant));
+        AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
+        assertEquals(UsageMethod.AVAILABLE, ra.getUsageMethod(state));
+    }
+
+    @Test
+    void testStrongestInstantUsageRule() {
+        State state = Mockito.mock(State.class);
+        Instant instant = Mockito.mock(Instant.class);
+        Mockito.doReturn(instant).when(state).getInstant();
+        Set<UsageRule> usageRules = Set.of(
+            new OnInstantImpl(UsageMethod.AVAILABLE, instant),
+            new OnFlowConstraintInCountryImpl(UsageMethod.FORCED, instant, Country.FR));
+        AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
+        assertEquals(UsageMethod.FORCED, ra.getUsageMethod(state));
+    }
+
+    @Test
+    void testOnlyOneOnContingencyStateUsageRule() {
+        State state = Mockito.mock(State.class);
+        Instant instant = Mockito.mock(Instant.class);
+        Mockito.doReturn(false).when(instant).isPreventive();
+        Mockito.doReturn(instant).when(state).getInstant();
+        Set<UsageRule> usageRules = Set.of(new OnContingencyStateImpl(UsageMethod.AVAILABLE, state));
+        AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
+        assertEquals(UsageMethod.AVAILABLE, ra.getUsageMethod(state));
+    }
+
+    @Test
+    void testOnlyStrongestStateUsageRule() {
+        State state = Mockito.mock(State.class);
+        Instant instant = Mockito.mock(Instant.class);
+        Mockito.doReturn(false).when(instant).isPreventive();
+        Mockito.doReturn(instant).when(state).getInstant();
+
+        FlowCnec flowCnec = Mockito.mock(FlowCnec.class);
+        Mockito.doReturn(state).when(flowCnec).getState();
+
+        Set<UsageRule> usageRules = Set.of(
+            new OnContingencyStateImpl(UsageMethod.AVAILABLE, state),
+            new OnFlowConstraintImpl(UsageMethod.FORCED, instant, flowCnec));
+        AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
+        assertEquals(UsageMethod.FORCED, ra.getUsageMethod(state));
+    }
+
+    @Test
+    void testStrongestStateAndInstantUsageRule() {
+        State state = Mockito.mock(State.class);
+        Instant instant = Mockito.mock(Instant.class);
+        Mockito.doReturn(false).when(instant).isPreventive();
+        Mockito.doReturn(instant).when(state).getInstant();
+
+        Set<UsageRule> usageRules = Set.of(
+            new OnContingencyStateImpl(UsageMethod.AVAILABLE, state),
+            new OnInstantImpl(UsageMethod.FORCED, instant));
+        AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
+        assertEquals(UsageMethod.FORCED, ra.getUsageMethod(state));
+    }
+}

commit 495303b3fe3c8768f8d9d699aa9d1956051b6752

Fix getUsageMethod bug introduced by #979 (#989)

data/crac/crac-impl/src/main/java/com/powsybl/openrao/data/cracimpl/AbstractRemedialAction.java
@@ -88,13 +88,19 @@ public abstract class AbstractRemedialAction<I extends RemedialAction<I>> extend
                 updateMapWithValue(usageMethodPerInstant, usageRule.getInstant(), usageRule.getUsageMethod());
             } else if (usageRule instanceof OnFlowConstraint ofc) {
                 State state = ofc.getFlowCnec().getState();
-                updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+                if (usageRule.getInstant().equals(state.getInstant())) {
+                    updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+                }
             } else if (usageRule instanceof OnAngleConstraint oac) {
                 State state = oac.getAngleCnec().getState();
-                updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+                if (usageRule.getInstant().equals(state.getInstant())) {
+                    updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+                }
             } else if (usageRule instanceof OnVoltageConstraint ovc) {
                 State state = ovc.getVoltageCnec().getState();
-                updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+                if (usageRule.getInstant().equals(state.getInstant())) {
+                    updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());
+                }
             } else if (usageRule instanceof OnContingencyState ocs) {
                 State state = ocs.getState();
                 updateMapWithValue(usageMethodPerState, state, usageRule.getUsageMethod());

data/crac/crac-impl/src/test/java/com/powsybl/openrao/data/cracimpl/RemedialActionTest.java
@@ -10,7 +10,9 @@ package com.powsybl.openrao.data.cracimpl;
 import com.powsybl.iidm.network.Country;
 import com.powsybl.openrao.data.cracapi.Instant;
 import com.powsybl.openrao.data.cracapi.State;
+import com.powsybl.openrao.data.cracapi.cnec.AngleCnec;
 import com.powsybl.openrao.data.cracapi.cnec.FlowCnec;
+import com.powsybl.openrao.data.cracapi.cnec.VoltageCnec;
 import com.powsybl.openrao.data.cracapi.usagerule.UsageMethod;
 import com.powsybl.openrao.data.cracapi.usagerule.UsageRule;
 import org.junit.jupiter.api.Test;
@@ -95,5 +97,86 @@ class RemedialActionTest {
             new OnInstantImpl(UsageMethod.FORCED, instant));
         AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
         assertEquals(UsageMethod.FORCED, ra.getUsageMethod(state));
+    }
+
+    @Test
+    void testDifferentInstantsBetweenOnFlowConstraintUsageRuleAndCnec() {
+        Instant autoInstant = Mockito.mock(Instant.class);
+        Mockito.when(autoInstant.isPreventive()).thenReturn(false);
+        Instant curativeInstant = Mockito.mock(Instant.class);
+        Mockito.when(curativeInstant.isPreventive()).thenReturn(false);
+
+        State autoState = Mockito.mock(State.class);
+        Mockito.when(autoState.getInstant()).thenReturn(autoInstant);
+        State curativeState = Mockito.mock(State.class);
+        Mockito.when(curativeState.getInstant()).thenReturn(curativeInstant);
+
+        FlowCnec autoFlowCnec = Mockito.mock(FlowCnec.class);
+        Mockito.when(autoFlowCnec.getState()).thenReturn(autoState);
+        FlowCnec curativeFlowCnec = Mockito.mock(FlowCnec.class);
+        Mockito.when(curativeFlowCnec.getState()).thenReturn(curativeState);
+
+        Set<UsageRule> usageRules = Set.of(
+            new OnFlowConstraintImpl(UsageMethod.FORCED, autoInstant, autoFlowCnec),
+            new OnFlowConstraintImpl(UsageMethod.FORCED, autoInstant, curativeFlowCnec)
+        );
+
+        AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
+        assertEquals(UsageMethod.FORCED, ra.getUsageMethod(autoState));
+        assertEquals(UsageMethod.UNDEFINED, ra.getUsageMethod(curativeState));
+    }
+
+    @Test
+    void testDifferentInstantsBetweenOnAngleConstraintUsageRuleAndCnec() {
+        Instant autoInstant = Mockito.mock(Instant.class);
+        Mockito.when(autoInstant.isPreventive()).thenReturn(false);
+        Instant curativeInstant = Mockito.mock(Instant.class);
+        Mockito.when(curativeInstant.isPreventive()).thenReturn(false);
+
+        State autoState = Mockito.mock(State.class);
+        Mockito.when(autoState.getInstant()).thenReturn(autoInstant);
+        State curativeState = Mockito.mock(State.class);
+        Mockito.when(curativeState.getInstant()).thenReturn(curativeInstant);
+
+        AngleCnec autoAngleCnec = Mockito.mock(AngleCnec.class);
+        Mockito.when(autoAngleCnec.getState()).thenReturn(autoState);
+        AngleCnec curativeAngleCnec = Mockito.mock(AngleCnec.class);
+        Mockito.when(curativeAngleCnec.getState()).thenReturn(curativeState);
+
+        Set<UsageRule> usageRules = Set.of(
+            new OnAngleConstraintImpl(UsageMethod.FORCED, autoInstant, autoAngleCnec),
+            new OnAngleConstraintImpl(UsageMethod.FORCED, autoInstant, curativeAngleCnec)
+        );
+
+        AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
+        assertEquals(UsageMethod.FORCED, ra.getUsageMethod(autoState));
+        assertEquals(UsageMethod.UNDEFINED, ra.getUsageMethod(curativeState));
+    }
+
+    @Test
+    void testDifferentInstantsBetweenOnVoltageConstraintUsageRuleAndCnec() {
+        Instant autoInstant = Mockito.mock(Instant.class);
+        Mockito.when(autoInstant.isPreventive()).thenReturn(false);
+        Instant curativeInstant = Mockito.mock(Instant.class);
+        Mockito.when(curativeInstant.isPreventive()).thenReturn(false);
+
+        State autoState = Mockito.mock(State.class);
+        Mockito.when(autoState.getInstant()).thenReturn(autoInstant);
+        State curativeState = Mockito.mock(State.class);
+        Mockito.when(curativeState.getInstant()).thenReturn(curativeInstant);
+
+        VoltageCnec autoVoltageCnec = Mockito.mock(VoltageCnec.class);
+        Mockito.when(autoVoltageCnec.getState()).thenReturn(autoState);
+        VoltageCnec curativeVoltageCnec = Mockito.mock(VoltageCnec.class);
+        Mockito.when(curativeVoltageCnec.getState()).thenReturn(curativeState);
+
+        Set<UsageRule> usageRules = Set.of(
+            new OnVoltageConstraintImpl(UsageMethod.FORCED, autoInstant, autoVoltageCnec),
+            new OnVoltageConstraintImpl(UsageMethod.FORCED, autoInstant, curativeVoltageCnec)
+        );
+
+        AbstractRemedialAction<?> ra = new NetworkActionImpl("id", "name", "operator", usageRules, Collections.emptySet(), 0);
+        assertEquals(UsageMethod.FORCED, ra.getUsageMethod(autoState));
+        assertEquals(UsageMethod.UNDEFINED, ra.getUsageMethod(curativeState));
     }
 }