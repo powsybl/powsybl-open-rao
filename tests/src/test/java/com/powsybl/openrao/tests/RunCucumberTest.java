36d9cf2306fc67541d306daf20146ea350638290
OpenSuze <40593901+OpenSuze@users.noreply.github.com>
2024-05-06 10:50:14 +0200

    Fix cucumber tests in "Run all tests" in IntelliJ IDE (#1000)

--- tests/src/test/java/com/powsybl/openrao/tests/RunCucumberTest.java
+++ tests/src/test/java/com/powsybl/openrao/tests/RunCucumberTest.java
@@ -8,15 +8,15 @@ package com.powsybl.openrao.tests;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.core.options.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("com.powsybl.openrao.tests")
+@SelectPackages("com.powsybl.openrao.tests")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.powsybl.openrao.tests")
 public class RunCucumberTest {
 }
