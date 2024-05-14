c66cec79a23cf4c535933a9de5573649086adfcd
Thomas Bouquet <63302082+bqth29@users.noreply.github.com>
2024-05-14 13:38:06 +0200

    Decrease remedial action usage limits among consecutive instants of the same kind (#904)
    Decrease remedial action usage limits among consecutive instants of the same kind
    
    ---------
    
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- data/crac/crac-api/src/main/java/com/powsybl/openrao/data/cracapi/RaUsageLimits.java
+++ data/crac/crac-api/src/main/java/com/powsybl/openrao/data/cracapi/RaUsageLimits.java
@@ -24,6 +24,7 @@ public class RaUsageLimits {
     private static final Map<String, Integer> DEFAULT_MAX_RA_PER_TSO = new HashMap<>();
     private int maxRa = DEFAULT_MAX_RA;
     private int maxTso = DEFAULT_MAX_TSO;
+    private final Set<String> maxTsoExclusion = new HashSet<>();
     private Map<String, Integer> maxTopoPerTso = DEFAULT_MAX_TOPO_PER_TSO;
     private Map<String, Integer> maxPstPerTso = DEFAULT_MAX_PST_PER_TSO;
     private Map<String, Integer> maxRaPerTso = DEFAULT_MAX_RA_PER_TSO;
@@ -96,6 +97,10 @@ public class RaUsageLimits {
         return maxRaPerTso;
     }
 
+    public Set<String> getMaxTsoExclusion() {
+        return maxTsoExclusion;
+    }
+
     private Map<String, Integer> replaceNegativeValues(Map<String, Integer> limitsPerTso) {
         limitsPerTso.forEach((tso, limit) -> {
             if (limit < 0) {
@@ -121,6 +126,10 @@ public class RaUsageLimits {
                 throw new OpenRaoException(String.format("TSO %s has a maximum number of allowed RAs smaller than the number of allowed PST RAs. This is not supported.", tso));
             }
         });
+    }
+
+    public void addTsoToExclude(String tso) {
+        maxTsoExclusion.add(tso);
     }
 
     @Override
