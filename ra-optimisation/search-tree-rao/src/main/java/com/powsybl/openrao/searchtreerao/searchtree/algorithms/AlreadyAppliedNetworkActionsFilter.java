f8eac6d8fad0a35c6784f63e5aa8e659881336b0
Thomas Bouquet <63302082+bqth29@users.noreply.github.com>
2024-05-15 13:42:50 +0200

    Harmonize bloomer filters (#963)

---
+++ ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/searchtree/algorithms/AlreadyAppliedNetworkActionsFilter.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.openrao.searchtreerao.searchtree.algorithms;
+
+import com.powsybl.openrao.searchtreerao.commons.NetworkActionCombination;
+import com.powsybl.openrao.searchtreerao.result.api.OptimizationResult;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
+public class AlreadyAppliedNetworkActionsFilter implements NetworkActionCombinationFilter {
+
+    public Set<NetworkActionCombination> filter(Set<NetworkActionCombination> naCombinations, OptimizationResult optimizationResult) {
+        return naCombinations.stream()
+            .filter(naCombination -> naCombination.getNetworkActionSet().stream().noneMatch(na -> optimizationResult.getActivatedNetworkActions().contains(na)))
+            .collect(Collectors.toSet());
+    }
+}
