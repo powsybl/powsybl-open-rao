defc521a3fa7142ab7708e945a193e3a985a4d2a
Philippe Edwards <philippe.edwards@rte-france.com>
2024-05-24 16:10:11 +0200

    reverted some unwanted changes from merge commit
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>

--- monitoring/angle-monitoring/src/main/java/com/powsybl/openrao/monitoring/anglemonitoring/RedispatchAction.java
+++ monitoring/angle-monitoring/src/main/java/com/powsybl/openrao/monitoring/anglemonitoring/RedispatchAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -34,7 +34,7 @@ public class RedispatchAction {
     }
 
     /**
-     * Scales powerToBeRedispatched on network.
+     * Scales power to redispatch (positive for generation, negative for load) on network.
      */
     public void apply(Network network) {
         ScalingParameters scalingParameters = new ScalingParameters().setIgnoredInjectionIds(networkElementsToBeExcluded);
