79db8d994e464cfe6e21ba118cdc30cbd7b6d74d
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2024-06-17 10:12:38 +0200

    changes after review
    Signed-off-by: benrejebmoh <mohamed.ben-rejeb@rte-france.com>

--- data/crac-creation/crac-creator-csa-profiles/src/main/java/com/powsybl/openrao/data/craccreation/creator/csaprofile/craccreator/NotEnabledException.java
+++ data/crac-creation/crac-creator-csa-profiles/src/main/java/com/powsybl/openrao/data/craccreation/creator/csaprofile/craccreator/NotEnabledException.java
@@ -1,8 +1,17 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.openrao.data.craccreation.creator.csaprofile.craccreator;
 
 import com.powsybl.openrao.data.craccreation.creator.api.ImportStatus;
 import com.powsybl.openrao.data.craccreation.util.OpenRaoImportException;
 
+/**
+ * @author Mohamed Ben-rejeb {@literal <mohamed.ben-rejeb at rte-france.com>}
+ */
 public class NotEnabledException extends OpenRaoImportException {
 
     public NotEnabledException(ImportStatus importStatus, String msg) {
