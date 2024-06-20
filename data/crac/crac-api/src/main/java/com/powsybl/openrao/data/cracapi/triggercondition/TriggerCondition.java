f19a87c24345e60343ac753bea1aab5176ca66fb
Thomas Bouquet <thomas.bouquet@rte-france.com>
2024-06-20 15:34:21 +0200

    TriggerCondition docstring
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- data/crac/crac-api/src/main/java/com/powsybl/openrao/data/cracapi/triggercondition/TriggerCondition.java
+++ data/crac/crac-api/src/main/java/com/powsybl/openrao/data/cracapi/triggercondition/TriggerCondition.java
@@ -16,6 +16,28 @@ import com.powsybl.openrao.data.cracapi.cnec.Cnec;
 import java.util.Optional;
 
 /**
+ * Condition that must be met for a remedial action to be triggered.
+ * A trigger condition is a global AND gate that gathers up to 4 sub-conditions:
+ *
+ * <ol>
+ *     <li>
+ *          (mandatory) an {@link Instant} at which the remedial action can be applied;
+ *     </li>
+ *     <li>
+ *          (optional) a {@link Contingency} after which the remedial action becomes applicable;
+ *     </li>
+ *     <li>
+ *          (optional) a {@link Cnec} which, if constrained, makes the remedial action applicable;
+ *     </li>
+ *     <li>
+ *          (optional) and a {@link Country} in which any overloaded FlowCNEC makes the remedial action applicable.
+ *     </li>
+ * </ol>
+ *
+ * A trigger condition also has a {@link UsageMethod} that describes how the remedial should be used if triggered.
+ * <p>
+ * A remedial action can have several trigger conditions, and they will be considered as a global OR gate among trigger conditions.
+ *
  * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
  */
 public interface TriggerCondition {
