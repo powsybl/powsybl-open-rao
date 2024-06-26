0b16145f64f2fa5c026ca88528c0973f18934053
Hugo SCHINDLER <hugo.schindler@rte-france.com>
2024-06-26 18:00:47 +0200

    Merge main
    Signed-off-by: Hugo SCHINDLER <hugo.schindler@rte-france.com>

--- ra-optimisation/search-tree-rao/src/test/java/com/powsybl/openrao/searchtreerao/castor/algorithm/CastorFullOptimizationTest.java
+++ ra-optimisation/search-tree-rao/src/test/java/com/powsybl/openrao/searchtreerao/castor/algorithm/CastorFullOptimizationTest.java
@@ -42,13 +42,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
