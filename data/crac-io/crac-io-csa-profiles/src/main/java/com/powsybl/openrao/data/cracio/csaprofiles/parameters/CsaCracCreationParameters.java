5e705a55225390027a7ea202be191f1412d9a67b
Thomas Bouquet <thomas.bouquet@rte-france.com>
2024-09-06 10:42:46 +0200

    Add borders in CSA CRAC creation parameters
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- data/crac-io/crac-io-csa-profiles/src/main/java/com/powsybl/openrao/data/cracio/csaprofiles/parameters/CsaCracCreationParameters.java
+++ data/crac-io/crac-io-csa-profiles/src/main/java/com/powsybl/openrao/data/cracio/csaprofiles/parameters/CsaCracCreationParameters.java
@@ -9,16 +9,25 @@ package com.powsybl.openrao.data.cracio.csaprofiles.parameters;
 import com.powsybl.commons.extensions.AbstractExtension;
 import com.powsybl.openrao.data.cracapi.parameters.CracCreationParameters;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Mohamed Ben-rejeb {@literal <mohamed.ben-rejeb at rte-france.com>}
  */
 public class CsaCracCreationParameters extends AbstractExtension<CracCreationParameters> {
+    private static final String REE = "REE";
+    private static final String REN = "REN";
+    private static final String RTE = "RTE";
+    private static final String ES_FR = "ES-FR";
+    private static final String ES_PT = "ES-PT";
     private String capacityCalculationRegionEicCode = "10Y1001C--00095L"; // swe as default
     private int spsMaxTimeToImplementThresholdInSeconds = 0;
-    private Map<String, Boolean> usePatlInFinalState = Map.of("REE", false, "REN", true, "RTE", true);
+    private Map<String, Boolean> usePatlInFinalState = Map.of(REE, false, REN, true, RTE, true);
     private Map<String, Integer> craApplicationWindow = Map.of("curative 1", 300, "curative 2", 600, "curative 3", 1200);
+    private Set<Border> borders = Set.of(new Border(ES_FR, "10YDOM--ES-FR--D", RTE), new Border(ES_PT, "10YDOM--ES-PT--T", REN));
 
     @Override
     public String getName() {
@@ -37,6 +46,10 @@ public class CsaCracCreationParameters extends AbstractExtension<CracCreationPar
         return craApplicationWindow;
     }
 
+    public Set<Border> getBorders() {
+        return borders;
+    }
+
     public void setCapacityCalculationRegionEicCode(String capacityCalculationRegionEicCode) {
         this.capacityCalculationRegionEicCode = capacityCalculationRegionEicCode;
     }
@@ -50,10 +63,14 @@ public class CsaCracCreationParameters extends AbstractExtension<CracCreationPar
     }
 
     public void setUsePatlInFinalState(Map<String, Boolean> usePatlInFinalState) {
-        this.usePatlInFinalState = usePatlInFinalState;
+        this.usePatlInFinalState = new HashMap<>(usePatlInFinalState);
     }
 
     public void setCraApplicationWindow(Map<String, Integer> craApplicationWindow) {
-        this.craApplicationWindow = craApplicationWindow;
+        this.craApplicationWindow = new HashMap<>(craApplicationWindow);
+    }
+
+    public void setBorders(Set<Border> borders) {
+        this.borders = new HashSet<>(borders);
     }
 }
