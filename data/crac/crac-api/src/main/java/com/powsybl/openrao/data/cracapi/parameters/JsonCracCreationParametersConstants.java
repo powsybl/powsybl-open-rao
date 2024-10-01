3bce586fdc9456a8522b775d2866c8850d3c25ac
Thomas Bouquet <thomas.bouquet@rte-france.com>
2024-10-01 15:02:46 +0200

    Mutualize RaUsageLimitsDeserializers
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- data/crac/crac-api/src/main/java/com/powsybl/openrao/data/cracapi/parameters/JsonCracCreationParametersConstants.java
+++ data/crac/crac-api/src/main/java/com/powsybl/openrao/data/cracapi/parameters/JsonCracCreationParametersConstants.java
@@ -101,7 +101,7 @@ public final class JsonCracCreationParametersConstants {
         return map;
     }
 
-    private static Pair<String, RaUsageLimits> deserializeRaUsageLimits(JsonParser jsonParser) throws IOException {
+    public static Pair<String, RaUsageLimits> deserializeRaUsageLimits(JsonParser jsonParser) throws IOException {
         RaUsageLimits raUsageLimits = new RaUsageLimits();
         String instant = null;
         while (!jsonParser.nextToken().isStructEnd()) {
