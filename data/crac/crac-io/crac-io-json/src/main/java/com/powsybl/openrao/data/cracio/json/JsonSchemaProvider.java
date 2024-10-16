6bf71634922b5384f0abf2eba84b6d2dc546d28f
Thomas Bouquet <thomas.bouquet@rte-france.com>
2024-10-16 17:43:40 +0200

    add json crac validator
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

---
+++ data/crac/crac-io/crac-io-json/src/main/java/com/powsybl/openrao/data/cracio/json/JsonSchemaProvider.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.openrao.data.cracio.json;
+
+import com.fasterxml.jackson.core.json.JsonReadFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.powsybl.openrao.commons.OpenRaoException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
+public final class JsonSchemaProvider {
+    private JsonSchemaProvider() {
+    }
+
+    private static final String SCHEMA_FILE_BASE_PATH = "/schemas/v%s/crac-v%s.%s-schema.json";
+    private static final JsonSchemaFactory SCHEMA_FACTORY = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V201909);
+    private static final ObjectMapper MAPPER = new ObjectMapper().configure(JsonReadFeature.ALLOW_NON_NUMERIC_NUMBERS.mappedFeature(), true);
+
+    public static boolean validateJsonCrac(String cracFile, int majorVersion, int minorVersion) throws IOException {
+        return getJsonCracSchema(majorVersion, minorVersion).validate(MAPPER.readTree(JsonSchemaProvider.class.getResourceAsStream(cracFile))).isEmpty();
+    }
+
+    private static JsonSchema getJsonCracSchema(int majorVersion, int minorVersion) {
+        InputStream schemaInputStream = JsonSchemaProvider.class.getResourceAsStream(SCHEMA_FILE_BASE_PATH.formatted(majorVersion, majorVersion, minorVersion));
+        if (schemaInputStream == null) {
+            throw new OpenRaoException("No JSON Schema found for CRAC v%s.%s.".formatted(majorVersion, minorVersion));
+        }
+        return SCHEMA_FACTORY.getSchema(schemaInputStream);
+    }
+}
---
+++ data/crac/crac-io/crac-io-json/src/test/java/com/powsybl/openrao/data/cracio/json/JsonSchemaProviderTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.openrao.data.cracio.json;
+
+import com.powsybl.openrao.commons.OpenRaoException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static com.powsybl.openrao.data.cracio.json.JsonSchemaProvider.validateJsonCrac;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
+class JsonSchemaProviderTest {
+    @Test
+    void missingVersion() {
+        OpenRaoException exception = assertThrows(OpenRaoException.class, () -> validateJsonCrac("/whatever/the/file/is", 0, 0));
+        assertEquals("No JSON Schema found for CRAC v0.0.", exception.getMessage());
+    }
+
+    @Test
+    void validateCrac2Point5() throws IOException {
+        assertTrue(validateJsonCrac("/retrocompatibility/v2/crac-v2.5.json", 2, 5));
+    }
+}
