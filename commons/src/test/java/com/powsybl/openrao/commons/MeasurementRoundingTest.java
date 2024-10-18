b40c093c19e2609c05e99fb40bcf5681b7aa9f5e
Thomas Bouquet <thomas.bouquet@rte-france.com>
2024-10-18 17:15:53 +0200

    rollback to bigdecimals in rounding tests
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- commons/src/test/java/com/powsybl/openrao/commons/MeasurementRoundingTest.java
+++ commons/src/test/java/com/powsybl/openrao/commons/MeasurementRoundingTest.java
@@ -9,6 +9,9 @@ package com.powsybl.openrao.commons;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import static com.powsybl.openrao.commons.MeasurementRounding.roundValueBasedOnMargin;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,27 +22,27 @@ class MeasurementRoundingTest {
     @Test
     void testRoundValueBasedOnMargin() {
         // light negative constraints (-1 <= constraint < 0)
-        assertEquals(40.0001, roundValueBasedOnMargin(40.0001, -0.0001, 2).doubleValue());
+        assertEquals(BigDecimal.valueOf(40.0001), roundValueBasedOnMargin(40.0001, -0.0001, 2));
         assertEquals(4, roundValueBasedOnMargin(40.0001, -0.0001, 2).scale());
 
-        assertEquals(40.0002, roundValueBasedOnMargin(40.0002, -0.0002, 2).doubleValue());
+        assertEquals(BigDecimal.valueOf(40.0002), roundValueBasedOnMargin(40.0002, -0.0002, 2));
         assertEquals(4, roundValueBasedOnMargin(40.0002, -0.0002, 2).scale());
 
         // strong negative constraints (constraint < -1)
-        assertEquals(50.000, roundValueBasedOnMargin(50.0001, -10.0001, 3).doubleValue());
+        assertEquals(BigDecimal.valueOf(50.000).setScale(3, RoundingMode.HALF_UP), roundValueBasedOnMargin(50.0001, -10.0001, 3));
         assertEquals(3, roundValueBasedOnMargin(50.0001, -10.0001, 3).scale());
 
-        assertEquals(50.0, roundValueBasedOnMargin(50.00345, -10.00345, 1).doubleValue());
+        assertEquals(BigDecimal.valueOf(50.0), roundValueBasedOnMargin(50.00345, -10.00345, 1));
         assertEquals(1, roundValueBasedOnMargin(50.00345, -10.0001, 1).scale());
 
         // positive constraints
-        assertEquals(40.00, roundValueBasedOnMargin(39.9999, 0.0001, 2).doubleValue());
+        assertEquals(BigDecimal.valueOf(40.0).setScale(2, RoundingMode.HALF_UP), roundValueBasedOnMargin(39.9999, 0.0001, 2));
         assertEquals(2, roundValueBasedOnMargin(39.9999, 0.0001, 2).scale());
 
-        assertEquals(30.000, roundValueBasedOnMargin(29.9999, -10.0001, 3).doubleValue());
+        assertEquals(BigDecimal.valueOf(30.000).setScale(3, RoundingMode.HALF_UP), roundValueBasedOnMargin(29.9999, -10.0001, 3));
         assertEquals(3, roundValueBasedOnMargin(29.9999, -10.0001, 3).scale());
 
-        assertEquals(30.00, roundValueBasedOnMargin(30.0, 0.0, 2).doubleValue());
+        assertEquals(BigDecimal.valueOf(30.00).setScale(2, RoundingMode.HALF_UP), roundValueBasedOnMargin(30.0, 0.0, 2));
         assertEquals(2, roundValueBasedOnMargin(30.0, 0.0, 2).scale());
     }
 }
