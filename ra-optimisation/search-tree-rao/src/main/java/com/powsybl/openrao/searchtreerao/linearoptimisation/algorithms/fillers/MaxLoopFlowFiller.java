cf3dc61cb213e7b4d39cbc8fed3e9a0b5ed3943c
Thomas Bouquet <63302082+bqth29@users.noreply.github.com>
2025-01-14 17:03:35 +0100

    timestamp in variables and constraints (#1259)
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/linearoptimisation/algorithms/fillers/MaxLoopFlowFiller.java
+++ ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/linearoptimisation/algorithms/fillers/MaxLoopFlowFiller.java
@@ -20,7 +20,9 @@ import com.powsybl.openrao.searchtreerao.result.api.FlowResult;
 import com.powsybl.openrao.searchtreerao.result.api.RangeActionActivationResult;
 import com.powsybl.openrao.searchtreerao.result.api.SensitivityResult;
 
+import java.time.OffsetDateTime;
 import java.util.Comparator;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -34,9 +36,10 @@ public class MaxLoopFlowFiller implements ProblemFiller {
     private final double loopFlowAcceptableAugmentation;
     private final double loopFlowViolationCost;
     private final double loopFlowConstraintAdjustmentCoefficient;
+    private final OffsetDateTime timestamp;
     private FlowResult preOptimFlowResult; // = flow result used in the first "fill" iteration
 
-    public MaxLoopFlowFiller(Set<FlowCnec> loopFlowCnecs, FlowResult initialFlowResult, LoopFlowParametersExtension loopFlowParameters) {
+    public MaxLoopFlowFiller(Set<FlowCnec> loopFlowCnecs, FlowResult initialFlowResult, LoopFlowParametersExtension loopFlowParameters, OffsetDateTime timestamp) {
         this.loopFlowCnecs = new TreeSet<>(Comparator.comparing(Identifiable::getId));
         this.loopFlowCnecs.addAll(FillersUtil.getFlowCnecsNotNaNFlow(loopFlowCnecs, initialFlowResult));
         this.initialFlowResult = initialFlowResult;
@@ -44,6 +47,7 @@ public class MaxLoopFlowFiller implements ProblemFiller {
         this.loopFlowAcceptableAugmentation = loopFlowParameters.getAcceptableIncrease();
         this.loopFlowViolationCost = loopFlowParameters.getViolationCost();
         this.loopFlowConstraintAdjustmentCoefficient = loopFlowParameters.getConstraintAdjustmentCoefficient();
+        this.timestamp = timestamp;
     }
 
     private Set<FlowCnec> getValidLoopFlowCnecs(SensitivityResult sensitivityResult) {
@@ -97,13 +101,14 @@ public class MaxLoopFlowFiller implements ProblemFiller {
                 }
 
                 // get loop-flow variable
-                OpenRaoMPVariable flowVariable = linearProblem.getFlowVariable(cnec, side);
+                OpenRaoMPVariable flowVariable = linearProblem.getFlowVariable(cnec, side, Optional.ofNullable(timestamp));
 
                 OpenRaoMPVariable loopflowViolationVariable = linearProblem.addLoopflowViolationVariable(
                     0,
                     linearProblem.infinity(),
                     cnec,
-                    side
+                    side,
+                    Optional.ofNullable(timestamp)
                 );
 
                 // build constraint which defines the loopFlow :
@@ -116,7 +121,8 @@ public class MaxLoopFlowFiller implements ProblemFiller {
                     linearProblem.infinity(),
                     cnec,
                     side,
-                    LinearProblem.BoundExtension.LOWER_BOUND
+                    LinearProblem.BoundExtension.LOWER_BOUND,
+                    Optional.ofNullable(timestamp)
                 );
                 positiveLoopflowViolationConstraint.setCoefficient(flowVariable, 1);
                 positiveLoopflowViolationConstraint.setCoefficient(loopflowViolationVariable, 1.0);
@@ -126,7 +132,8 @@ public class MaxLoopFlowFiller implements ProblemFiller {
                     loopFlowUpperBound + flowResult.getCommercialFlow(cnec, side, Unit.MEGAWATT),
                     cnec,
                     side,
-                    LinearProblem.BoundExtension.UPPER_BOUND
+                    LinearProblem.BoundExtension.UPPER_BOUND,
+                    Optional.ofNullable(timestamp)
                 );
                 negativeLoopflowViolationConstraint.setCoefficient(flowVariable, 1);
                 negativeLoopflowViolationConstraint.setCoefficient(loopflowViolationVariable, -1);
