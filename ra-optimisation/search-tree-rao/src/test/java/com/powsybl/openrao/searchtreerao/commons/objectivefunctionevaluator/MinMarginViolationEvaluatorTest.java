702649f6edf65eb4523e5bde01e8435ac239452e
Godelaine de Montmorillon <godelaine.demontmorillon@rte-france.com>
2025-03-24 10:17:32 +0100

    adapt tests to parameter change
    Signed-off-by: Godelaine de Montmorillon <godelaine.demontmorillon@rte-france.com>

--- ra-optimisation/search-tree-rao/src/test/java/com/powsybl/openrao/searchtreerao/commons/objectivefunctionevaluator/MinMarginViolationEvaluatorTest.java
+++ ra-optimisation/search-tree-rao/src/test/java/com/powsybl/openrao/searchtreerao/commons/objectivefunctionevaluator/MinMarginViolationEvaluatorTest.java
@@ -40,7 +40,7 @@ class MinMarginViolationEvaluatorTest {
 
         MinMarginViolationEvaluator evaluator = new MinMarginViolationEvaluator(Set.of(flowCnec), Unit.MEGAWATT, new BasicMarginEvaluator());
         CostEvaluatorResult result = evaluator.evaluate(flowResult, null);
-        assertEquals(10000.0, result.getCost(Set.of()));
+        assertEquals(1000.0, result.getCost(Set.of()));
         assertEquals(List.of(flowCnec), result.getCostlyElements(Set.of()));
     }
 
