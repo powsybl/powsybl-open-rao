b054d806cf5d646db35250363376c082240f3949
Thomas Bouquet <thomas.bouquet@rte-france.com>
2025-04-15 15:09:15 +0200

    optional GSK
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- tests/src/test/java/com/powsybl/openrao/tests/steps/InterTemporalRaoSteps.java
+++ tests/src/test/java/com/powsybl/openrao/tests/steps/InterTemporalRaoSteps.java
@@ -179,7 +179,7 @@ public final class InterTemporalRaoSteps {
         }
         interTemporalRaoInput = new InterTemporalRaoInputWithNetworkPaths(raoInputs, new HashSet<>());
         InputStream gskInputStream = icsGskPath == null ? null : new FileInputStream(getFile(icsGskPath));
-        if (icsGskPath != null && icsStaticPath != null && icsSeriesPath != null) {
+        if (icsStaticPath != null && icsSeriesPath != null) {
             IcsImporter.populateInputWithICS(interTemporalRaoInput, new FileInputStream(getFile(icsStaticPath)), new FileInputStream(getFile(icsSeriesPath)), gskInputStream, raoParameters.getExtension(OpenRaoSearchTreeParameters.class).getIcsImporterParameters().orElseThrow());
 
         }
