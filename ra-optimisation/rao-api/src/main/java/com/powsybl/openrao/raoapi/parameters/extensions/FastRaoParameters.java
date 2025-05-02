2d4ffad4b492725c8c6fb1e1da9effac8ceaeb2e
CHEN Roxane <roxane.chen@rte-france.com>
2025-05-02 15:15:10 +0200

    add config loader + make the extension optional
    Signed-off-by: CHEN Roxane <roxane.chen@rte-france.com>

--- ra-optimisation/rao-api/src/main/java/com/powsybl/openrao/raoapi/parameters/extensions/FastRaoParameters.java
+++ ra-optimisation/rao-api/src/main/java/com/powsybl/openrao/raoapi/parameters/extensions/FastRaoParameters.java
@@ -7,11 +7,9 @@
 
 package com.powsybl.openrao.raoapi.parameters.extensions;
 
-import com.powsybl.commons.config.PlatformConfig;
 import com.powsybl.commons.extensions.AbstractExtension;
 import com.powsybl.openrao.raoapi.parameters.RaoParameters;
 
-import java.util.Objects;
 
 import static com.powsybl.openrao.raoapi.RaoParametersCommons.*;
 
@@ -25,9 +23,9 @@ public class FastRaoParameters extends AbstractExtension<RaoParameters> {
         return FAST_RAO_PARAMETERS;
     }
 
-    private static final int DEFAULT_NUMBER_OF_CNECS_TO_ADD = 20;
-    private static final boolean DEFAULT_ADD_UNSECURE_CNECS = false;
-    private static final double DEFAULT_MARGIN_LIMIT = 5;
+    static final int DEFAULT_NUMBER_OF_CNECS_TO_ADD = 20;
+    static final boolean DEFAULT_ADD_UNSECURE_CNECS = false;
+    static final double DEFAULT_MARGIN_LIMIT = 5;
 
     private int getNumberOfCnecsToAdd = DEFAULT_NUMBER_OF_CNECS_TO_ADD;
     private boolean addUnsecureCnecs = DEFAULT_ADD_UNSECURE_CNECS;
@@ -56,18 +54,4 @@ public class FastRaoParameters extends AbstractExtension<RaoParameters> {
     public void setMarginLimit(double marginLimit) {
         this.marginLimit = marginLimit;
     }
-
-    public static FastRaoParameters load(PlatformConfig platformConfig) {
-        Objects.requireNonNull(platformConfig);
-        FastRaoParameters parameters = new FastRaoParameters();
-        platformConfig.getOptionalModuleConfig(FAST_RAO_PARAMETERS)
-            .ifPresent(config -> {
-                parameters.setNumberOfCnecsToAdd(config.getIntProperty(NUMBER_OF_CNECS_TO_ADD, DEFAULT_NUMBER_OF_CNECS_TO_ADD));
-                parameters.setAddUnsecureCnecs(config.getBooleanProperty(ADD_UNSECURE_CNECS, DEFAULT_ADD_UNSECURE_CNECS));
-                parameters.setMarginLimit(config.getDoubleProperty(MARGIN_LIMIT, DEFAULT_MARGIN_LIMIT));
-
-            });
-        return parameters;
-    }
-
 }
