dd595cad122055a2d7e8de2b1f81ab2a87d669cf
Thomas Bouquet <thomas.bouquet@rte-france.com>
2025-05-28 11:49:59 +0200

    add test (TODO)
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

---
+++ data/crac/crac-io/crac-io-commons/src/test/java/com/powsybl/openrao/data/crac/io/commons/FlowCnecAdderUtilTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.openrao.data.crac.io.commons;
+
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
+public class FlowCnecAdderUtilTest {
+    // TODO
+}
