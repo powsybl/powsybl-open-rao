3cc4f5eb1d84dfb7b8b623c788a08e1552a37351
Thomas Bouquet <thomas.bouquet@rte-france.com>
2025-08-21 11:03:40 +0200

    add missing line after copyright header
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/commons/optimizationperimeters/AbstractOptimizationPerimeter.java
+++ ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/commons/optimizationperimeters/AbstractOptimizationPerimeter.java
@@ -4,6 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 package com.powsybl.openrao.searchtreerao.commons.optimizationperimeters;
 
 import com.powsybl.openrao.data.crac.api.Identifiable;
