e1c115c71753f35bfafc44d68bf96fa005380343
phiedw <philippe.edwards@rte-france.com>
2025-10-16 15:40:29 +0200

    Accelr changes (#1495)
    * various changes for accelr
    
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>
    
    * few more changes
    
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>
    
    * avoid having all variables in full sensi for fast rao
    
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>
    
    * fixes for Marmot
    
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>
    
    * reverted unwanted changes
    
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>
    
    * fixed some code smells
    
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>
    
    * changes after review
    
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>
    
    * change the marginal term to variation
    
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>
    
    ---------
    
    Signed-off-by: Philippe Edwards <philippe.edwards@rte-france.com>

--- ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/searchtree/algorithms/FarFromMostLimitingElementFilter.java
+++ ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/searchtree/algorithms/FarFromMostLimitingElementFilter.java
@@ -26,13 +26,11 @@ import static com.powsybl.openrao.commons.logs.OpenRaoLoggerProvider.TECHNICAL_L
 public class FarFromMostLimitingElementFilter implements NetworkActionCombinationFilter {
     private final Network network;
     private final CountryGraph countryGraph;
-    private final boolean filterFarElements;
     private final int maxNumberOfBoundariesForSkippingNetworkActions;
 
-    public FarFromMostLimitingElementFilter(Network network, boolean filterFarElements, int maxNumberOfBoundariesForSkippingNetworkActions) {
+    public FarFromMostLimitingElementFilter(Network network, int maxNumberOfBoundariesForSkippingNetworkActions) {
         this.network = network;
         countryGraph = new CountryGraph(network);
-        this.filterFarElements = filterFarElements;
         this.maxNumberOfBoundariesForSkippingNetworkActions = maxNumberOfBoundariesForSkippingNetworkActions;
     }
 
@@ -42,10 +40,6 @@ public class FarFromMostLimitingElementFilter implements NetworkActionCombinatio
      * The most limiting elements are the most limiting functional cost element, and all elements with a non-zero virtual cost.
      */
     public Set<NetworkActionCombination> filter(Set<NetworkActionCombination> naCombinations, OptimizationResult optimizationResult) {
-        if (!filterFarElements) {
-            return naCombinations;
-        }
-
         Set<Country> worstCnecLocation = getOptimizedMostLimitingElementsLocation(optimizationResult);
 
         Set<NetworkActionCombination> filteredNaCombinations = naCombinations.stream()
