bf4279ca0e470b5abe6126e9786b8eeac8275cb7
Thomas Bouquet <thomas.bouquet@rte-france.com>
2025-10-30 16:26:32 +0100

    fix test
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- ra-optimisation/search-tree-rao/src/test/java/com/powsybl/openrao/searchtreerao/marmot/MarmotTest.java
+++ ra-optimisation/search-tree-rao/src/test/java/com/powsybl/openrao/searchtreerao/marmot/MarmotTest.java
@@ -315,7 +315,7 @@ class MarmotTest {
         inputPerTimestamp.put(timestamp10, RaoInputWithNetworkPaths.build(networkPath, networkPath, crac10).build());
 
         IntertemporalConstraints intertemporalConstraints = new IntertemporalConstraints();
-        intertemporalConstraints.addGeneratorConstraints(GeneratorConstraints.create().withGeneratorId("FFR1AA1 _generator").withLeadTime(0.0).withLagTime(0.0).withPMin(0.0).withPMax(5000.0).withUpwardPowerGradient(500.0).withDownwardPowerGradient(-500.0).build());
+        intertemporalConstraints.addGeneratorConstraints(GeneratorConstraints.create().withGeneratorId("FFR1AA1 _generator").withPMax(5000.0).withUpwardPowerGradient(500.0).withDownwardPowerGradient(-500.0).build());
 
         InterTemporalRaoInputWithNetworkPaths input = new InterTemporalRaoInputWithNetworkPaths(new TemporalDataImpl<>(inputPerTimestamp), intertemporalConstraints);
 
