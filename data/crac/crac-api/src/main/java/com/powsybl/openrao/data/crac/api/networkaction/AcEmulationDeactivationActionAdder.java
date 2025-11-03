9bc0e3c92aa45992dc3644a4ebe1cf6671bc7653
CHEN Roxane <roxane.chen@rte-france.com>
2025-11-03 11:23:05 +0100

    add AcEmulationDeactivationAction
    Signed-off-by: CHEN Roxane <roxane.chen@rte-france.com>

--- data/crac/crac-api/src/main/java/com/powsybl/openrao/data/crac/api/networkaction/AcEmulationDeactivationActionAdder.java
+++ data/crac/crac-api/src/main/java/com/powsybl/openrao/data/crac/api/networkaction/AcEmulationDeactivationActionAdder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, RTE (http://www.rte-france.com)
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -8,12 +8,7 @@
 package com.powsybl.openrao.data.crac.api.networkaction;
 
 /**
- * ActionType
- *
- * @author Viktor Terrier {@literal <viktor.terrier at rte-france.com>}
+ * @author Roxane Chen {@literal <roxane.chen at rte-france.com>}
  */
-public enum ActionType {
-    OPEN,
-    CLOSE,
-    DEACTIVATE
+public interface AcEmulationDeactivationActionAdder extends SingleNetworkElementActionAdder<AcEmulationDeactivationActionAdder> {
 }
