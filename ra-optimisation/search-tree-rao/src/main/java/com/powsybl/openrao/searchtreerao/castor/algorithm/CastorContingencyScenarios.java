71bfbf8fef1b90cbb51fab2c89de6235df401564
Thomas Bouquet <thomas.bouquet@rte-france.com>
2025-11-17 08:59:06 +0100

    rollback castor contingency scenarios
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/castor/algorithm/CastorContingencyScenarios.java
+++ ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/castor/algorithm/CastorContingencyScenarios.java
@@ -84,7 +84,6 @@ public class CastorContingencyScenarios {
         network.getVariantManager().setWorkingVariant(newVariant);
         // Create an automaton simulator
         AutomatonSimulator automatonSimulator = new AutomatonSimulator(crac, raoParameters, toolProvider, initialSensitivityOutput, prePerimeterSensitivityOutput, stateTree.getOperatorsNotSharingCras(), NUMBER_LOGGED_ELEMENTS_DURING_RAO);
-
         // Go through all contingency scenarios
         try (AbstractNetworkPool networkPool = AbstractNetworkPool.create(network, newVariant, getAvailableCPUs(raoParameters), true)) {
             AtomicInteger remainingScenarios = new AtomicInteger(stateTree.getContingencyScenarios().size());
@@ -102,7 +101,6 @@ public class CastorContingencyScenarios {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-
         return contingencyScenarioResults;
     }
 
