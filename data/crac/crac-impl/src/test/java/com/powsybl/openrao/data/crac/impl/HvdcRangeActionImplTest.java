9edf7187b74125c1b5072735dd0a4ec93635bb3f
CHEN Roxane <roxane.chen@rte-france.com>
2025-11-19 11:36:15 +0100

    Revert "throw an error if we try to change active setpoint in ac emulation mode"
    This reverts commit 0dfd924cc55b61cd124c458896a04eea7f867197.
    
    Signed-off-by: CHEN Roxane <roxane.chen@rte-france.com>

--- data/crac/crac-impl/src/test/java/com/powsybl/openrao/data/crac/impl/HvdcRangeActionImplTest.java
+++ data/crac/crac-impl/src/test/java/com/powsybl/openrao/data/crac/impl/HvdcRangeActionImplTest.java
@@ -64,12 +64,10 @@ class HvdcRangeActionImplTest {
         HvdcRangeAction hvdcRa = hvdcRangeActionAdder.newRange().withMin(-5).withMax(10).add()
                 .add();
         hvdcRa.apply(network, 5);
+        hvdcRa.apply(networkWithAngleDroop, 6);
         assertEquals(5, hvdcRa.getCurrentSetpoint(network), 1e-6);
-
-        // Not allowed to change HVDC line's active setpoint if the line is in AC emulation mode.
-        OpenRaoException exception = assertThrows(OpenRaoException.class, () -> hvdcRa.apply(networkWithAngleDroop, 6));
-        assertEquals("Unable to set an active power setpoint for HVDC line BBE2AA11 FFR3AA11 1 because it is operating in AC Emulation mode.", exception.getMessage());
-
+        // Not applied because the line is in ac emulation
+        assertEquals(0, hvdcRa.getCurrentSetpoint(networkWithAngleDroop), 1e-6);
     }
 
     @Test
@@ -77,11 +75,10 @@ class HvdcRangeActionImplTest {
         HvdcRangeAction hvdcRa = hvdcRangeActionAdder.newRange().withMin(-5).withMax(10).add()
                 .add();
         hvdcRa.apply(network, -3);
+        hvdcRa.apply(networkWithAngleDroop, -4);
         assertEquals(-3, hvdcRa.getCurrentSetpoint(network), 1e-6);
-
-        // Not allowed to change HVDC line's active setpoint if the line is in AC emulation mode.
-        OpenRaoException exception = assertThrows(OpenRaoException.class, () -> hvdcRa.apply(networkWithAngleDroop, -4));
-        assertEquals("Unable to set an active power setpoint for HVDC line BBE2AA11 FFR3AA11 1 because it is operating in AC Emulation mode.", exception.getMessage());
+        // Not applied because the line is in ac emulation
+        assertEquals(0, hvdcRa.getCurrentSetpoint(networkWithAngleDroop), 1e-6);
     }
 
     @Test
