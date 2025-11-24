766bde46357096ea80c73a2f0da118c6cc154839
Thomas Bouquet <thomas.bouquet@rte-france.com>
2025-11-24 17:15:42 +0100

    feat (PST regulation): Enable PST regulation at the end of RAO optimization when it's the limiting element (#1435)
    * Add PST regulation extension to RAO parameters
    * Create pst regulation specific classes
    * Only regulate if limiting element is a PST
    
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>
    Signed-off-by: Godelaine de Montmorillon <godelaine.demontmorillon@rte-france.com>
    Co-authored-by: Godelaine de Montmorillon <godelaine.demontmorillon@rte-france.com>
    Co-authored-by: danielthirion <152513244+danielthirion@users.noreply.github.com>

---
+++ ra-optimisation/search-tree-rao/src/main/java/com/powsybl/openrao/searchtreerao/castor/algorithm/pstregulation/PstRegulator.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.openrao.searchtreerao.castor.algorithm.pstregulation;
+
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.PhaseTapChanger;
+import com.powsybl.iidm.network.TwoWindingsTransformer;
+import com.powsybl.loadflow.LoadFlow;
+import com.powsybl.loadflow.LoadFlowParameters;
+import com.powsybl.openrao.commons.OpenRaoException;
+import com.powsybl.openrao.commons.logs.OpenRaoLoggerProvider;
+import com.powsybl.openrao.data.crac.api.rangeaction.PstRangeAction;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
+public final class PstRegulator {
+    private PstRegulator() {
+    }
+
+    public static Map<PstRangeAction, Integer> regulatePsts(Set<ElementaryPstRegulationInput> elementaryPstRegulationInputs, Network network, LoadFlowParameters loadFlowParameters) {
+        elementaryPstRegulationInputs.forEach(elementaryPstRegulationInput -> setRegulationForPst(network, elementaryPstRegulationInput));
+        LoadFlow.find("OpenLoadFlow").run(network, loadFlowParameters);
+        return elementaryPstRegulationInputs.stream().collect(Collectors.toMap(ElementaryPstRegulationInput::pstRangeAction, pstRegulationInput -> getRegulatedTap(network, pstRegulationInput.pstRangeAction())));
+    }
+
+    private static void setRegulationForPst(Network network, ElementaryPstRegulationInput elementaryPstRegulationInput) {
+        TwoWindingsTransformer twt = getTwoWindingsTransformer(network, elementaryPstRegulationInput.pstRangeAction());
+        PhaseTapChanger phaseTapChanger = twt.getPhaseTapChanger();
+        phaseTapChanger.setRegulationValue(elementaryPstRegulationInput.limitingThreshold());
+        setRegulationTerminal(twt, elementaryPstRegulationInput);
+        phaseTapChanger.setRegulationMode(PhaseTapChanger.RegulationMode.CURRENT_LIMITER);
+        setTargetDeadband(twt);
+        phaseTapChanger.setRegulating(true);
+    }
+
+    private static TwoWindingsTransformer getTwoWindingsTransformer(Network network, PstRangeAction pstRangeAction) {
+        String pstId = pstRangeAction.getNetworkElement().getId();
+        TwoWindingsTransformer twt = network.getTwoWindingsTransformer(pstId);
+        if (twt == null) {
+            throw new OpenRaoException("No two-windings transformer with id %s found in network.".formatted(pstId));
+        }
+        return twt;
+    }
+
+    private static void setRegulationTerminal(TwoWindingsTransformer twt, ElementaryPstRegulationInput elementaryPstRegulationInput) {
+        PhaseTapChanger phaseTapChanger = twt.getPhaseTapChanger();
+        if (phaseTapChanger.getRegulationTerminal() == null) {
+            OpenRaoLoggerProvider.TECHNICAL_LOGS.info("No default regulation terminal defined for phase tap changer of two-windings transformer %s, terminal on side %s will be used.".formatted(twt.getId(), elementaryPstRegulationInput.limitingSide()));
+            phaseTapChanger.setRegulationTerminal(twt.getTerminal(elementaryPstRegulationInput.limitingSide()));
+        }
+    }
+
+    private static void setTargetDeadband(TwoWindingsTransformer twt) {
+        PhaseTapChanger phaseTapChanger = twt.getPhaseTapChanger();
+        if (Double.isNaN(phaseTapChanger.getTargetDeadband())) {
+            OpenRaoLoggerProvider.TECHNICAL_LOGS.info("No default target deadband defined for phase tap changer of two-windings transformer %s, a value of 0.0 will be used.".formatted(twt.getId()));
+            phaseTapChanger.setTargetDeadband(0.0); // value is not used by OpenLoadFlow in CURRENT_LIMITER mode
+        }
+    }
+
+    private static int getRegulatedTap(Network network, PstRangeAction pstRangeAction) {
+        Integer tapPosition = getTwoWindingsTransformer(network, pstRangeAction).getPhaseTapChanger().getSolvedTapPosition();
+        if (tapPosition == null) {
+            throw new OpenRaoException("Could not retrieve regulated tap position for PST range action %s.".formatted(pstRangeAction.getName()));
+        }
+        return getTwoWindingsTransformer(network, pstRangeAction).getPhaseTapChanger().getSolvedTapPosition();
+    }
+}
---
+++ ra-optimisation/search-tree-rao/src/test/java/com/powsybl/openrao/searchtreerao/castor/algorithm/pstregulation/ElementaryPstRegulationInputTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.openrao.searchtreerao.castor.algorithm.pstregulation;
+
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.TwoSides;
+import com.powsybl.openrao.data.crac.api.Crac;
+import com.powsybl.openrao.data.crac.api.InstantKind;
+import com.powsybl.openrao.data.crac.api.State;
+import com.powsybl.openrao.data.crac.api.rangeaction.PstRangeAction;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * @author Thomas Bouquet {@literal <thomas.bouquet at rte-france.com>}
+ */
+class ElementaryPstRegulationInputTest {
+    @Test
+    void testRetrieveLimitingThresholdInCrac() throws IOException {
+        Network network = Network.read("2Nodes4ParallelLines3PSTs.uct", getClass().getResourceAsStream("/network/2Nodes4ParallelLines3PSTs.uct"));
+        Crac crac = Crac.read("crac-3-psts.json", getClass().getResourceAsStream("/crac/crac-3-psts.json"), network);
+        State state = crac.getState("Contingency BE1 FR1 1", crac.getInstant(InstantKind.CURATIVE));
+
+        // thresholds are properly defined on both sides
+        PstRangeAction pstBeFr2 = crac.getPstRangeAction("pstBeFr2");
+        ElementaryPstRegulationInput pst1RegulationInput = ElementaryPstRegulationInput.of(pstBeFr2, "BBE1AA1  FFR1AA1  2", state, crac, network);
+        assertNotNull(pst1RegulationInput);
+        assertEquals(TwoSides.TWO, pst1RegulationInput.limitingSide());
+        assertEquals(500.0, pst1RegulationInput.limitingThreshold());
+
+        // thresholds are defined only one side 1
+        PstRangeAction pstBeFr3 = crac.getPstRangeAction("pstBeFr3");
+        ElementaryPstRegulationInput pst2RegulationInput = ElementaryPstRegulationInput.of(pstBeFr3, "BBE1AA1  FFR1AA1  3", state, crac, network);
+        assertNotNull(pst2RegulationInput);
+        assertEquals(TwoSides.ONE, pst2RegulationInput.limitingSide());
+        assertEquals(250.0, pst2RegulationInput.limitingThreshold());
+
+        // no FlowCNEC defined
+        PstRangeAction pstBeFr4 = crac.getPstRangeAction("pstBeFr4");
+        assertNull(ElementaryPstRegulationInput.of(pstBeFr4, "BBE1AA1  FFR1AA1  4", state, crac, network));
+    }
+
+    @Test
+    void testRetrieveLimitingThresholdInCracForLineInSeries() throws IOException {
+        Network network = Network.read("2Nodes4ParallelLines3PSTs.uct", getClass().getResourceAsStream("/network/2Nodes4ParallelLines3PSTs.uct"));
+        Crac crac = Crac.read("crac-3-psts-monitored-line-in-series.json", getClass().getResourceAsStream("/crac/crac-3-psts-monitored-line-in-series.json"), network);
+        State state = crac.getState("Contingency BE1 FR1 2", crac.getInstant(InstantKind.CURATIVE));
+
+        // common terminal is on side 1 of both the line and the PST so only the thresholds on this side are used
+        PstRangeAction pstBeFr2 = crac.getPstRangeAction("pstBeFr2");
+        ElementaryPstRegulationInput pst1RegulationInput = ElementaryPstRegulationInput.of(pstBeFr2, "BBE1AA1  FFR1AA1  1", state, crac, network);
+        assertNotNull(pst1RegulationInput);
+        assertEquals(TwoSides.ONE, pst1RegulationInput.limitingSide());
+        assertEquals(800.0, pst1RegulationInput.limitingThreshold());
+
+        // common terminal is on side 1 of both the line and the PST so only the thresholds on this side are used
+        PstRangeAction pstBeFr3 = crac.getPstRangeAction("pstBeFr3");
+        ElementaryPstRegulationInput pst2RegulationInput = ElementaryPstRegulationInput.of(pstBeFr3, "BBE1AA1  FFR1AA1  1", state, crac, network);
+        assertNotNull(pst2RegulationInput);
+        assertEquals(TwoSides.ONE, pst2RegulationInput.limitingSide());
+        assertEquals(800.0, pst2RegulationInput.limitingThreshold());
+    }
+}
