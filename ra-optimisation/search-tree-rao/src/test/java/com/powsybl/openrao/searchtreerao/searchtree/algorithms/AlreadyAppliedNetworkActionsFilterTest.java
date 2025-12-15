8ccf5c22ed23c5cdcdd308630791383e420c6e46
vbochetRTE <vincent.bochet@rte-france.com>
2025-12-15 10:48:04 +0100

    Add AssertJ Core dependency and implement a test case with AssertJ assertions (#1571)
    Signed-off-by: Vincent BOCHET <vincent.bochet@rte-france.com>
    Signed-off-by: CHEN Roxane <roxane.chen@rte-france.com>

--- ra-optimisation/search-tree-rao/src/test/java/com/powsybl/openrao/searchtreerao/searchtree/algorithms/AlreadyAppliedNetworkActionsFilterTest.java
+++ ra-optimisation/search-tree-rao/src/test/java/com/powsybl/openrao/searchtreerao/searchtree/algorithms/AlreadyAppliedNetworkActionsFilterTest.java
@@ -8,6 +8,7 @@
 package com.powsybl.openrao.searchtreerao.searchtree.algorithms;
 
 import com.powsybl.openrao.searchtreerao.commons.NetworkActionCombination;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -23,8 +24,6 @@ import static com.powsybl.openrao.searchtreerao.searchtree.algorithms.NetworkAct
 import static com.powsybl.openrao.searchtreerao.searchtree.algorithms.NetworkActionCombinationsUtils.IND_FR_2;
 import static com.powsybl.openrao.searchtreerao.searchtree.algorithms.NetworkActionCombinationsUtils.IND_FR_DE;
 import static com.powsybl.openrao.searchtreerao.searchtree.algorithms.NetworkActionCombinationsUtils.NA_FR_1;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 
 /**
@@ -45,8 +44,8 @@ class AlreadyAppliedNetworkActionsFilterTest {
         AlreadyAppliedNetworkActionsFilter naFilter = new AlreadyAppliedNetworkActionsFilter();
         Set<NetworkActionCombination> filteredNaCombinations = naFilter.filter(naCombinations, previousLeaf);
 
-        assertEquals(5, filteredNaCombinations.size());
-        assertFalse(filteredNaCombinations.contains(IND_FR_1));
-        assertFalse(filteredNaCombinations.contains(COMB_3_FR));
+        Assertions.assertThat(filteredNaCombinations)
+            .hasSize(5)
+            .doesNotContain(IND_FR_1, COMB_3_FR);
     }
 }
