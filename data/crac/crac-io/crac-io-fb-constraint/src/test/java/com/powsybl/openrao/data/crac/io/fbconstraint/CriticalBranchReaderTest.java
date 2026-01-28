531ceeb84b51833ad1c54668e7a20c096a26788e
CHEN Roxane <roxane.chen@rte-france.com>
2026-01-28 16:09:37 +0100

    add ut
    Signed-off-by: CHEN Roxane <roxane.chen@rte-france.com>

--- data/crac/crac-io/crac-io-fb-constraint/src/test/java/com/powsybl/openrao/data/crac/io/fbconstraint/CriticalBranchReaderTest.java
+++ data/crac/crac-io/crac-io-fb-constraint/src/test/java/com/powsybl/openrao/data/crac/io/fbconstraint/CriticalBranchReaderTest.java
@@ -7,11 +7,19 @@
 
 package com.powsybl.openrao.data.crac.io.fbconstraint;
 
+import com.powsybl.iidm.network.Network;
+import com.powsybl.openrao.commons.Unit;
+import com.powsybl.openrao.data.crac.api.Crac;
+import com.powsybl.openrao.data.crac.api.CracFactory;
+import com.powsybl.openrao.data.crac.api.parameters.CracCreationParameters;
+import com.powsybl.openrao.data.crac.io.fbconstraint.parameters.FbConstraintCracCreationParameters;
 import com.powsybl.openrao.data.crac.io.fbconstraint.xsd.CriticalBranchType;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.time.OffsetDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Baptiste Seguinot {@literal <baptiste.seguinot at rte-france.com>}
@@ -52,5 +60,39 @@ class CriticalBranchReaderTest {
         // branch whose name does not finish with [XX]
         branch.setName("[PL-PL] My Branch Name [OPP]");
         assertFalse(CriticalBranchReader.isCrossZonal(branch));
+    }
+
+    @Test
+    void testAddThresholdWithZeroThreshold() throws IOException {
+        CracCreationParameters parameters = new CracCreationParameters();
+        parameters.setCracFactoryName(CracFactory.findDefault().getName());
+        parameters.addExtension(FbConstraintCracCreationParameters.class, new FbConstraintCracCreationParameters());
+
+        // Test that if a threshold is 0, it is not used
+        Network network = Network.read("TestCase12Nodes_with_Xnodes.uct", getClass().getResourceAsStream("/network/TestCase12Nodes_with_Xnodes.uct"));
+        OffsetDateTime timestamp = OffsetDateTime.parse("2019-01-08T10:30Z");
+        parameters.getExtension(FbConstraintCracCreationParameters.class).setTimestamp(timestamp);
+        FbConstraintCreationContext creationContext = (FbConstraintCreationContext) Crac.readWithContext("with_zero_limits.xml", getClass().getResourceAsStream("/merged_cb/with_zero_limits.xml"), network, parameters);
+        Crac crac = creationContext.getCrac();
+
+        // Only have imaxFactor value equal to 10
+        assertTrue(crac.getFlowCnec("BE_CBCO_000001 - preventive").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.max().get() == 10.0));
+        assertTrue(crac.getFlowCnec("BE_CBCO_000001 - preventive").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.getUnit() == Unit.PERCENT_IMAX));
+
+        // permanentImaxFactor is 0, have no value for ImaxFactor, no value for PermanentImaxA => should use imaxA value = 20
+        // Same temporaryImaxFactor is 0, have no value for ImaxFactor, no value for TemporaryImaxA => should use imaxA value = 20
+        assertTrue(crac.getFlowCnec("BE_CBCO_000003 - curative").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.max().get() == 20.0));
+        assertTrue(crac.getFlowCnec("BE_CBCO_000003 - curative").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.getUnit() == Unit.AMPERE));
+        assertTrue(crac.getFlowCnec("BE_CBCO_000003 - outage").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.max().get() == 20.0));
+        assertTrue(crac.getFlowCnec("BE_CBCO_000003 - outage").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.getUnit() == Unit.AMPERE));
+
+        // permanentImaxFactor is 0, but we have a value for imaxFactor = -40 (negative because of direction==opposite)
+        assertTrue(crac.getFlowCnec("BE_CBCO_000004 - curative").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.min().get() == -40));
+        assertTrue(crac.getFlowCnec("BE_CBCO_000004 - curative").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.getUnit() == Unit.PERCENT_IMAX));
+
+        // No thresholds info, the threshold should be set to -1 IMAX_percent (negative because of direction==opposite)
+        assertTrue(crac.getFlowCnec("BE_CBCO_000002 - preventive").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.min().get() == -1));
+        assertTrue(crac.getFlowCnec("BE_CBCO_000002 - preventive").getThresholds().stream().allMatch(branchThreshold -> branchThreshold.getUnit() == Unit.PERCENT_IMAX));
+
     }
 }
