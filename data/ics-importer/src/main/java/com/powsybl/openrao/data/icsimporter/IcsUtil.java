61c22171932d9ec95717fa1d0d9db02b1f9bd97a
CHEN Roxane <roxane.chen@rte-france.com>
2026-04-02 15:19:20 +0200

    oops
    Signed-off-by: CHEN Roxane <roxane.chen@rte-france.com>

--- data/ics-importer/src/main/java/com/powsybl/openrao/data/icsimporter/IcsUtil.java
+++ data/ics-importer/src/main/java/com/powsybl/openrao/data/icsimporter/IcsUtil.java
@@ -16,6 +16,8 @@ import java.time.OffsetDateTime;
 import java.util.Map;
 import java.util.Optional;
 
+import static com.powsybl.openrao.commons.logs.OpenRaoLoggerProvider.BUSINESS_WARNS;
+
 /**
  * @author Roxane Chen {@literal <roxane.chen at rte-france.com>}
  */
@@ -61,8 +63,11 @@ public final class IcsUtil {
         if (seriesPerType.containsKey(key)) {
             CSVRecord series = seriesPerType.get(key);
             String value = series.get(timestamp.getHour() + OFFSET);
-            if (value != null) {
+            if (value != null && !value.isEmpty()) {
                 return Optional.of(parseDoubleWithPossibleCommas(value) * shiftKey);
+            } else {
+                BUSINESS_WARNS.warn("Redispatching action {} is missing {} value for datetime {}", series.get(RA_RD_ID), key, timestamp);
+                return Optional.empty();
             }
         }
         return Optional.empty();
