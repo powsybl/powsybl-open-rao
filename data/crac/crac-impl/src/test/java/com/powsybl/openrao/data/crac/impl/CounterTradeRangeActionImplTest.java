8f5e9f7ec0ff1a57832bcf6da060bd57214a2864
Thomas Bouquet <thomas.bouquet@rte-france.com>
2026-06-08 10:52:50 +0200

    coverage
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- data/crac/crac-impl/src/test/java/com/powsybl/openrao/data/crac/impl/CounterTradeRangeActionImplTest.java
+++ data/crac/crac-impl/src/test/java/com/powsybl/openrao/data/crac/impl/CounterTradeRangeActionImplTest.java
@@ -11,12 +11,17 @@ import com.powsybl.iidm.network.Network;
 import com.powsybl.openrao.commons.OpenRaoException;
 import com.powsybl.openrao.data.crac.api.Crac;
 import com.powsybl.openrao.data.crac.api.InstantKind;
+import com.powsybl.openrao.data.crac.api.range.RangeType;
 import com.powsybl.openrao.data.crac.api.rangeaction.CounterTradeRangeAction;
 import com.powsybl.openrao.data.crac.impl.utils.NetworkImportsUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
@@ -75,5 +80,20 @@ class CounterTradeRangeActionImplTest {
                 .add();
         assertEquals(0, counterTradeRangeAction.getCurrentSetpoint(network));
 
+    }
+
+    @Test
+    void testEquals() {
+        CounterTradeRangeActionImpl ctFrEs1 = new CounterTradeRangeActionImpl("CT", "CT", "FR", null, Set.of(), List.of(new StandardRangeImpl(-1000.0, 1000.0, RangeType.ABSOLUTE)), 0.0, null, null, null, "FR", "ES");
+        CounterTradeRangeActionImpl ctFrEs2 = new CounterTradeRangeActionImpl("CT", "CT", "FR", null, Set.of(), List.of(new StandardRangeImpl(-1000.0, 1000.0, RangeType.ABSOLUTE)), 0.0, null, null, null, "FR", "ES");
+        CounterTradeRangeActionImpl ctFrEs3 = new CounterTradeRangeActionImpl("CT", "CT", "FR", null, Set.of(), List.of(new StandardRangeImpl(-2000.0, 2000.0, RangeType.ABSOLUTE)), 0.0, null, null, null, "FR", "ES");
+        CounterTradeRangeActionImpl ctPtEs = new CounterTradeRangeActionImpl("CT", "CT", "PT", null, Set.of(), List.of(new StandardRangeImpl(-1000.0, 1000.0, RangeType.ABSOLUTE)), 0.0, null, null, null, "PT", "ES");
+        CounterTradeRangeActionImpl ctFrNl = new CounterTradeRangeActionImpl("CT", "CT", "FR", null, Set.of(), List.of(new StandardRangeImpl(-1000.0, 1000.0, RangeType.ABSOLUTE)), 0.0, null, null, null, "FR", "NL");
+
+        assertEquals(ctFrEs1, ctFrEs1);
+        assertEquals(ctFrEs1, ctFrEs2);
+        assertNotEquals(ctFrEs1, ctFrEs3);
+        assertNotEquals(ctFrEs1, ctPtEs);
+        assertNotEquals(ctFrEs1, ctFrNl);
     }
 }
