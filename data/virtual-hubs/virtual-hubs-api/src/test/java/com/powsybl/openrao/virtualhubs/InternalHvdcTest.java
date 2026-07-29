a21c87d096e1825ca1394a12e6e5b83879976b4c
Antoine LIMOUZIN <antoine.limouzin@rte-france.com>
2026-07-29 13:51:55 +0200

    fix checkstyle
    Signed-off-by: Antoine LIMOUZIN <antoine.limouzin@rte-france.com>

--- data/virtual-hubs/virtual-hubs-api/src/test/java/com/powsybl/openrao/virtualhubs/InternalHvdcTest.java
+++ data/virtual-hubs/virtual-hubs-api/src/test/java/com/powsybl/openrao/virtualhubs/InternalHvdcTest.java
@@ -20,10 +20,10 @@ class InternalHvdcTest {
     private static final String HVDC_CODE = "CODE_OF_HVDC";
     private static final String POLE_ID = "EIC_OF_POLE";
 
-    final static List<ThrowableAssert.ThrowingCallable> FAILING_CONSTRUCTORS = List.of(() -> new InternalHvdc(HVDC_EIC, HVDC_CODE, null),
-                                                                                       () -> new InternalHvdc(HVDC_EIC, null, createPoles()),
-                                                                                       () -> new InternalHvdc(null, HVDC_CODE, createPoles()));
-    
+    private static final List<ThrowableAssert.ThrowingCallable> FAILING_CONSTRUCTORS = List.of(() -> new InternalHvdc(HVDC_EIC, HVDC_CODE, null),
+                                                                                               () -> new InternalHvdc(HVDC_EIC, null, createPoles()),
+                                                                                               () -> new InternalHvdc(null, HVDC_CODE, createPoles()));
+
     @ParameterizedTest
     @FieldSource("FAILING_CONSTRUCTORS")
     void checkThatCreationThrowsWhenNullArg(final ThrowableAssert.ThrowingCallable callable) {
