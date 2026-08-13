62019dab1188083646e7289bba641555999e5a95
Thomas Bouquet <thomas.bouquet@rte-france.com>
2026-08-13 14:53:04 +0200

    fix after comments 2
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- tests/src/test/java/com/powsybl/openrao/tests/steps/CommonTestData.java
+++ tests/src/test/java/com/powsybl/openrao/tests/steps/CommonTestData.java
@@ -23,10 +23,11 @@ import com.powsybl.openrao.data.crac.io.nc.parameters.NcCracCreationParameters;
 import com.powsybl.openrao.data.glsk.virtual.hubs.GlskVirtualHubs;
 import com.powsybl.openrao.data.raoresult.api.RaoResult;
 import com.powsybl.openrao.data.raoresult.api.extension.AngleResult;
+import com.powsybl.openrao.data.raoresult.api.extension.VoltageResult;
 import com.powsybl.openrao.data.refprog.referenceprogram.ReferenceProgram;
 import com.powsybl.openrao.monitoring.results.AngleMonitoringResultAdapter;
 import com.powsybl.openrao.monitoring.results.MonitoringResult;
-import com.powsybl.openrao.monitoring.results.RaoResultWithVoltageMonitoring;
+import com.powsybl.openrao.monitoring.results.VoltageMonitoringResultAdapter;
 import com.powsybl.openrao.raoapi.TimeCoupledRaoInput;
 import com.powsybl.openrao.raoapi.json.JsonRaoParameters;
 import com.powsybl.openrao.raoapi.parameters.RaoParameters;
@@ -131,7 +132,7 @@ public final class CommonTestData {
         CommonTestData.monitoringResult = result;
         if (CommonTestData.raoResult != null) {
             // update RAO result with angle values
-            CommonTestData.raoResult = new RaoResultWithVoltageMonitoring(CommonTestData.raoResult, CommonTestData.monitoringResult);
+            CommonTestData.raoResult.addExtension(VoltageResult.class, VoltageMonitoringResultAdapter.convertToVoltageExtension(CommonTestData.monitoringResult));
         }
     }
 
