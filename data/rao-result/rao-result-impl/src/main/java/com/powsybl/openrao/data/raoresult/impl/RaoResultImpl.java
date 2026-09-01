aee648eb1d96f476eaf1e6ff01596adef397b01f
Thomas Bouquet <thomas.bouquet@rte-france.com>
2026-09-01 10:18:08 +0200

    merge flow extension
    Signed-off-by: Thomas Bouquet <thomas.bouquet@rte-france.com>

--- data/rao-result/rao-result-impl/src/main/java/com/powsybl/openrao/data/raoresult/impl/RaoResultImpl.java
+++ data/rao-result/rao-result-impl/src/main/java/com/powsybl/openrao/data/raoresult/impl/RaoResultImpl.java
@@ -8,9 +8,7 @@
 package com.powsybl.openrao.data.raoresult.impl;
 
 import com.powsybl.commons.extensions.AbstractExtendable;
-import com.powsybl.iidm.network.TwoSides;
 import com.powsybl.openrao.commons.OpenRaoException;
-import com.powsybl.openrao.commons.Unit;
 import com.powsybl.openrao.data.crac.api.Crac;
 import com.powsybl.openrao.data.crac.api.Instant;
 import com.powsybl.openrao.data.crac.api.State;
